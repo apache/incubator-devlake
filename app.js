f563d5ccdfb4912fcd52f74a4a1eb1fe4ce14ece
Jonathan O'Donnell <joncodo@meri.co>
2021-07-19 14:14:13 -0300

    notes in app.js for version 0

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const { JiraUser } = require('@db/postgres')
 
 let main = async ()=>{
   // get data from Jira
+    // get users
+    // get issues
+    // get changelogs
 
-
-  // store data in mongodb
-
-
+  // store all raw data in mongodb
 
   // store data in postgress
   let jiraUser = await JiraUser.create({})
