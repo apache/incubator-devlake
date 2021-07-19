d81fa3a035b73fcbfcda23efa4bbe85f983acd5e
Jonathan O'Donnell <joncodo@meri.co>
2021-07-19 14:05:05 -0300

    jira users are being saved to pg db

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const { JiraUser } = require('@db/postgres')
 
 let main = async ()=>{
   // get data from Jira
+
+
   // store data in mongodb
+
+
 
   // store data in postgress
   let jiraUser = await JiraUser.create({})
 
   // enhance data in postgres db
-  
+
 }
 
 main()
---
+++ db/README.md
@@ -0,0 +1,10 @@
+# DB
+
+## How to make a migration
+
+`npx sequelize-cli migration:generate --name my-script-name`
+
+## How to migrate your DB
+
+`npx sequelize-cli db:migrate`
+
