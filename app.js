a27888ed3181baddcfb710a07d0c7c6ca5984272
Justin <justinb@live.ca>
2021-07-19 17:04:19 -0400

    add db support for jira issues taken from api

--- app.js
+++ app.js
@@ -2,25 +2,65 @@ require('module-alias/register')
 
 const jira = require('@collectors/jira')
 const { JiraIssue } = require('@db/postgres')
-
-const { MongoRaw } = require('@db/mongodb/connection')
+const { MongoClient } = require('mongodb')
+const connection = require('@config/resolveConfig').mongo.connectionString
+const client = new MongoClient(connection)
 
 const main = async () => {
   // get data from Jira
   // get users
-  // get issues
-  const issues = await jira.issues.collectIssues('test-api')
-  console.log(issues)
-  // get changelogs
 
-  // store data in mongodb
-  const { mongoClient, mongoDb } = await MongoRaw.connect()
-  const issuesCollection = MongoRaw.createCollection(mongodb, 'issues')
-	// issuesCollection.insertMany([issues])
-	// MongoRaw.disconnect(mongoClient)  
+  // get issues and store in mongodb
+  try {
+    const issues = await jira.issues.collectIssues('test-api')
+    // console.log('Issues from Jira API: ', issues)
 
-  // store data in postgress
-  await JiraIssue.create({})
+    // store data in mongodb
+    await client.connect()
+
+    let issueCollection
+    let foundCollectionsCursor = await client.db().listCollections()
+    let foundCollections = await foundCollectionsCursor.toArray()
+    const collectionName = 'jira_issues'
+
+    // check if Jira collection exists
+    const collectionExists = foundCollections
+      .some(collection => collection.name === collectionName)
+
+    if (collectionExists === true) {
+      issueCollection = client.db().collection(collectionName)
+    }
+    else {
+      issueCollection = client.db().createCollection(collectionName)
+    }
+
+    // Insert issues into mongodb
+    await issueCollection.insertMany(issues)
+    let foundIssuesCursor = await issueCollection.find()
+    let foundIssues = await foundIssuesCursor.toArray()
+
+    // Insert data in postgress
+    foundIssues.forEach(issue => {
+
+      await JiraIssue.create({
+        id: issue.id,
+        url: issue.self,
+        title: issue.fields.summary,
+        projectId: issue.fields.project.id,
+        description: issue.fields.description
+
+        // TODO: additional jira issue fields
+        // leadTime: issue.fields.timespent
+      })
+    })
+
+  }
+  catch (e) {
+    console.error(e)
+  }
+  finally {
+    await client.close()
+  }
 
   // enhance data in postgres db
 }
