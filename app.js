df901a468166465a541706819600c8ad15bb90f2
Julien Chinapen <julien.chinapen@merico.dev>
2021-07-19 14:23:49 -0400

    Setup MongoDB Connection

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require('module-alias/register')
 const jira = require('@collectors/jira')
 const { JiraIssue } = require('@db/postgres')
 
+const { MongoRaw } = require('@db/mongodb/connection')
+
 const main = async () => {
   // get data from Jira
   // get users
@@ -10,6 +12,12 @@ const main = async () => {
   const issues = await jira.issues.collectIssues('test-api')
   console.log(issues)
   // get changelogs
+
+  // store data in mongodb
+  const { mongoClient, mongoDb } = await MongoRaw.connect()
+  const issuesCollection = MongoRaw.createCollection(mongodb, 'issues')
+	// issuesCollection.insertMany([issues])
+	// MongoRaw.disconnect(mongoClient)  
 
   // store data in postgress
   await JiraIssue.create({})
