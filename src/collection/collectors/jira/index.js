131bafafe2901f2e9762d3bcda14a01e9020b568
Justin Brazeau <justin.brazeau@merico.dev>
2021-07-22 00:28:56 -0400

    new setup for calling mongo collector services

--- src/collection/collectors/jira/index.js
+++ src/collection/collectors/jira/index.js
@@ -1,13 +1,30 @@
+const dbConnector = require('@mongo/connection')
 const issues = require('./issues')
-const changelogs = require('./changelogs')
+// const changelogs = require('./changelogs')
 
 module.exports = {
   async collect ({ projectId }) {
     console.log('Jira Collection, projectId:', projectId)
 
-    await issues.collectIssues(projectId)
-    await changelogs.collectChangelogs(projectId)
-  },
+    const { db } = await dbConnector.connect()
+    const jiraCollectors = []
 
-  issues
+    // ^ Issues
+    const jiraIssues = await issues.collectIssues(projectId)
+
+    const setJiraIssues = new Promise(() => {
+      jiraIssues.forEach(issue => {
+        const id = Number(issue.id)
+        const collectionName = 'jira_issues'
+        const doc = db.collection(collectionName).findOneAndUpdate({ id }, { $set: issue }, { upsert: true })
+        jiraCollectors.push(doc)
+      })
+    })
+
+    // ^ Changelogs
+    // await changelogs.collectChangelogs(projectId)
+
+    // ^ All Jira
+    Promise.all(setJiraIssues)
+  }
 }
