50d4518d7b6e254c5d988061c9cd536339b99242
Jonathan O'Donnell <joncodo@meri.co>
2021-07-22 11:36:08 -0300

    refactor to have one db connection

--- src/collection/collectors/jira/issues.js
+++ src/collection/collectors/jira/issues.js
@@ -6,44 +6,45 @@ const fetcher = require('./fetcher')
 const collectionName = 'jira_issues'
 
 module.exports = {
-  async collectIssues (projectId) {
-    // const { client, db } = await dbConnector.connect()
-
+  async collect(options) {
     try {
-      const { issues } = await module.exports.fetchIssues(projectId)
-
-      // const issueCollection = await dbConnector.findOrCreateCollection(db, collectionName)
-
-      // // Insert issues into mongodb
-      // await issueCollection.insertMany(issues)
-      return issues
+      const issuesResponse = await module.exports.fetchIssues(options.projectId)
+      await module.exports.save({issuesResponse, db: options.db})
+    } catch (error) {
+      console.log(error)      
+    }
+  },
+  
+  async save (options) {
+    try {
+      let promises = []
+      options.issuesResponse.issues.forEach(issue => {
+        const id = Number(issue.id)
+        promises.push(options.db.collection(collectionName).findOneAndUpdate({
+          id
+        }, {
+          $set: issue
+        }, {
+          upsert: true
+        }))
+      })
+      await Promise.all(promises)
     } catch (error) {
       console.error(error)
-    } finally {
-      // dbConnector.disconnect(client)
     }
   },
 
-  async fetchIssues (project) {
+
+  async fetchIssues(project) {
     const requestUri = `search?jql=project="${project}"`
 
     return fetcher.fetch(requestUri)
   },
 
-  async findIssues (where, limit = 99999999) {
-    const { client, db } = await dbConnector.connect()
-
-    let issues = []
-
-    try {
-      const issueCollection = await dbConnector.findOrCreateCollection(db, collectionName)
-      const foundIssuesCursor = await issueCollection.find(where).limit(limit)
-
-      issues = await foundIssuesCursor.toArray()
-    } finally {
-      dbConnector.disconnect(client)
-    }
-
-    return issues
+  async findIssues(where, db, limit = 99999999) {
+    console.log('INFO >>> findIssues where', where)
+    const issueCollection = await dbConnector.findOrCreateCollection(db, collectionName)
+    const foundIssuesCursor = await issueCollection.find(where).limit(limit)
+    return await foundIssuesCursor.toArray()
   }
 }
