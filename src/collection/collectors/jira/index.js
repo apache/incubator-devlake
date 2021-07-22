9039df5d8013edad6992505f3fb8fa12de0ac8a4
Jonathan O'Donnell <joncodo@meri.co>
2021-07-22 11:36:08 -0300

    refactor to have one db connection

--- src/collection/collectors/jira/index.js
+++ src/collection/collectors/jira/index.js
@@ -1,30 +1,24 @@
 const dbConnector = require('@mongo/connection')
 const issues = require('./issues')
-// const changelogs = require('./changelogs')
+const changelogs = require('./changelogs')
 
 module.exports = {
-  async collect ({ projectId }) {
+  async collect({
+    projectId
+  }) {
     console.log('Jira Collection, projectId:', projectId)
-
-    const { db } = await dbConnector.connect()
-    const jiraCollectors = []
-
-    // ^ Issues
-    const jiraIssues = await issues.collectIssues(projectId)
-
-    const setJiraIssues = new Promise(() => {
-      jiraIssues.forEach(issue => {
-        const id = Number(issue.id)
-        const collectionName = 'jira_issues'
-        const doc = db.collection(collectionName).findOneAndUpdate({ id }, { $set: issue }, { upsert: true })
-        jiraCollectors.push(doc)
-      })
-    })
-
-    // ^ Changelogs
-    // await changelogs.collectChangelogs(projectId)
-
-    // ^ All Jira
-    Promise.all(setJiraIssues)
+    const {
+      db, client
+    } = await dbConnector.connect()
+    
+    try {
+      await issues.collect({db, projectId})
+      await changelogs.collect({db, projectId})
+      console.log('INFO >>> done collecting')
+    } catch (error) {
+      console.log('>>> error', error)
+    } finally {
+      dbConnector.disconnect(client)
+    }
   }
 }
