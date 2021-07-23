aecc741821fbaef39050e76d29321f8e6b75889e
Kevin Kline <kevinkline@Kevins-MacBook-Pro.local>
2021-07-23 10:44:31 -0300

    added private token header, added fetch merge requests, added get all project repo commits

--- src/collection/collectors/gitlab/projects.js
+++ src/collection/collectors/gitlab/projects.js
@@ -2,7 +2,8 @@ require('module-alias/register')
 
 const fetcher = require('../util/fetcher')
 const modelName = 'projects'
-const { gitlab: { host, apiPath } } = require('../../../../config/resolveConfig')
+const { gitlab: { host, apiPath, token } } = require('../../../../config/resolveConfig')
+const privateTokenHeader = {"PRIVATE-TOKEN": token}
 
 module.exports = {
   async fetchProject (projectId) {
@@ -12,24 +13,27 @@ module.exports = {
     return fetcher.fetchAll(host, apiPath, modelName)
   },
   async fetchProjectRepoCommits (projectId) {
-    const routeName = 'repository/commits'
-    return fetcher.fetch(`${host}/${apiPath}/${modelName}/${projectId}/${routeName}`)
+    const routeName = 'repository/commits?all=true&with_stats=true'
+    return fetcher.fetch(`${host}/${apiPath}/${modelName}/${projectId}/${routeName}`, privateTokenHeader)
   },
   async fetchProjectRepoTree (projectId) {
     const routeName = 'repository/tree'
     return fetcher.fetch(`${host}/${apiPath}/${modelName}/${projectId}/${routeName}`)
   },
-  async fetchProjectFiles (projectId, tree, defaultBranch) {
+  async fetchProjectFiles (projectId, tree) {
     const routeName = 'repository/files'
     const files = []
     for (const treeNode of tree) {
       const path = treeNode.path.replace('.', '%2E').replace('/', '%2F')
-      console.log('path', path)
       const url = `${host}/${apiPath}/${modelName}/${projectId}/${routeName}/${path}/raw`
-      console.log('url', url)
       const file = await fetcher.fetch(url)
       files.push(file)
     }
     return files
+  },
+  async fetchMergeRequests (projectId) {
+    const routeName = 'merge_requests'
+    const url = `${host}/${apiPath}/${modelName}/${projectId}/${routeName}`
+    return fetcher.fetch(url, privateTokenHeader) 
   }
 }
