08ab0369b25f269253b20ae534599925e6765df4
Jonathan O'Donnell <joncodo@meri.co>
2021-07-23 11:13:23 -0300

    no more duplicate rows

--- src/plugins/jira-pond/src/collector/changelogs.js
+++ src/plugins/jira-pond/src/collector/changelogs.js
@@ -1,4 +1,6 @@
-const { findOrCreateCollection } = require('commondb')
+const {
+  findOrCreateCollection
+} = require('commondb')
 
 const issueUtil = require('./issues')
 
@@ -7,35 +9,51 @@ const fetcher = require('./fetcher')
 const collectionName = 'jira_issue_changelogs'
 
 module.exports = {
-  async collect (options) {
-    const issues = await issueUtil.findIssues({ 'fields.project.id': options.projectId }, options.db)
+  async collect(options) {
+    try {
+      console.log('INFO >>> collecting changelogs...', options.projectId)
+      const issues = await issueUtil.findIssues({
+        'fields.project.id': `${options.projectId}`
+      }, options.db)
+      console.info('INFO: issues length', issues.length)
 
-    const changelogCollection = await findOrCreateCollection(options.db, collectionName)
+      const changelogCollection = await findOrCreateCollection(options.db, collectionName)
 
-    const promises = []
-    for (const issue of issues) {
-      // todo we cant have this line. It needs to be a promise.all async
-      const changelog = await module.exports.fetchChangelogForIssue(issue.id)
+      const promises = []
+      for (const issue of issues) {
+        // todo we cant have this line. It needs to be a promise.all async
+        const changelog = await module.exports.fetchChangelogForIssue(issue.id)
+        for (const change of changelog.values) {
+          let primaryKey = changelog.id
 
-      for (const change of changelog.values) {
-        // todo we need to add our own primary key
-        // todo only update based on primary key
-        promises.push(changelogCollection.insertOne({
-          issueId: issue.id,
-          ...change
-        }))
+          promises.push(changelogCollection.findOneAndUpdate({
+            primaryKey
+          }, {
+            $set: {
+              issueId: issue.id,
+              ...change
+            }
+          }, {
+            upsert: true
+          }))
+        }
       }
+      await Promise.all(promises)
+    } catch (error) {
+      console.log(error)
     }
-    await Promise.all(promises)
+
   },
 
-  async fetchChangelogForIssue (issueId) {
+  async fetchChangelogForIssue(issueId) {
     const requestUri = `issue/${issueId}/changelog`
 
     return fetcher.fetch(requestUri)
   },
 
-  async findChangelogs (where, db, limit = 999999, sort = { createdAt: 1 }) {
+  async findChangelogs(where, db, limit = 999999, sort = {
+    createdAt: 1
+  }) {
     const changelogCollection = await findOrCreateCollection(db, collectionName)
     const changelogsCursor = await changelogCollection.find(where).limit(limit).sort(sort)
 
