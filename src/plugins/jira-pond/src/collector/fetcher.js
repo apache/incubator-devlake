7746e3ab63c1c4b28051c70cec2301e6aad502b6
Julien Chinapen <j.c@shogun.io>
2021-07-24 11:49:26 -0400

    Remove optional check operator

--- src/plugins/jira-pond/src/collector/fetcher.js
+++ src/plugins/jira-pond/src/collector/fetcher.js
@@ -5,7 +5,7 @@ const config = require('@config/resolveConfig').jira
 module.exports = {
   async fetch (resourceUri) {
     try {
-      const Authorization = config.restAuth?.enabled
+      const Authorization = config.restAuth.enabled
         ? `Basic ${new Buffer(`${config.restAuth.username}:${config.restAuth.apiToken}`).toString('base64')}`
         : `Basic ${config.basicAuth}`
 
