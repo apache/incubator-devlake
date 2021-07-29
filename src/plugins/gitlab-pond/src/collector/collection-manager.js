4c265a4a633a95f85ae1a6fc1165d346b20adee3
Kevin Kline <kevin.kline@merico.dev>
2021-07-29 15:07:27 -0300

    feat: adds merge requests collection method

--- src/plugins/gitlab-pond/src/collector/collection-manager.js
+++ src/plugins/gitlab-pond/src/collector/collection-manager.js
@@ -7,6 +7,20 @@ const {
 const fetcher = require('./fetcher')
 
 module.exports = {
+  async collectProjectMergeRequests (options, db) {
+    for (let index = 0; index < options.projectIds.length; index++) {
+      const projectId = options.projectIds[index];
+      
+      let response = await module.exports.fetchCollectionData('projects', projectId, 'merge_requests')
+      response = response.map(res => {
+        return {
+          projectId,
+          ...res
+        }
+      })
+      await module.exports.saveMany(response, db, 'gitlab_merge_requests')
+    }
+  },
   async collectProjectCommits (options, db) {
     for (let index = 0; index < options.projectIds.length; index++) {
       const projectId = options.projectIds[index]
