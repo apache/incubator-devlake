c390b7d9a606d7d988f276b84d27f1808db5a537
Klesh Wong <zhenmian.huang@merico.dev>
2021-07-29 20:22:38 +0800

    feat: update LeadTime calculation according to standard
    1. Lead-time calculation
       According to @Yi.Zheng, Lead time is the days between creation and
       resolution, simple subtraction, no other factor. The resolution date
       can be obtained from jira_issue.fields.resolutiondate according to
       @Jax. so, we don't need changelog at this point, deleting it can
       simplify our codebase and lessen performance concern.
    2. Collect JIRA data by `boardId`
       In reality, a JIRA Project can be a Product(EE) or Team(EE Backend),
       and a JIRA board can covers issues among single or multiple projects,
       collecting issues by boardId can help dealing with these situations.
    3. Collect JIRA data page by page to lower down memory usage footprint
       User may run our product on some huge JIRA database, load all records
       in memory before flushing them to database could cause OOM
    4. Collecting/Enriching changed data only
       By filtering out unneeded data we could reduce subsequent collection
       time drastically, by fetching the max updated time from local db and
       send it to JIRA through jql we can gain much faster collection time
       in future collection. Same technique applied to enricher by
       maintaining a `enriched` timestamp, whenever issue got
       enriched, the timestamp got synchronized to `fields.updated`, so we
       can filter out records that don't need to be re-enriched.
    5. Refactor pagination fetching to fetcher as a common function
       `fetcher.fetch` should be responsible for retrying logic due to it
       was designed for a specific request. Pagination for JIRA APIs are
       uniform, a shared function for pages iteration could be used for
       different APIs.

--- src/plugins/jira-pond/src/collector/fetcher.js
+++ src/plugins/jira-pond/src/collector/fetcher.js
@@ -4,21 +4,44 @@ const ProxyAgent = require('proxy-agent')
 const config = require('@config/resolveConfig').jira
 
 module.exports = {
-  async fetch (resourceUri) {
-    console.log('[jira] Fetching data from ', resourceUri)
-    try {
-      const response = await axios.get(`${config.host}/rest/api/3/${resourceUri}`, {
-        headers: {
-          Accept: 'application/json',
-          Authorization: `Basic ${config.basicAuth}`
-        },
-        agent: config.proxy && new ProxyAgent(config.proxy),
-        timeout: config.timeout
-      })
+  async fetch (resourceUri, maxRetry = 3) {
+    let retry = 0
+    let res
+    while (retry < maxRetry) {
+      console.log(`[jira] Fetching data from ${resourceUri} #${retry}`)
+      try {
+        res = await axios.get(`${config.host}/rest/${resourceUri}`, {
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Basic ${config.basicAuth}`
+          },
+          agent: config.proxy && new ProxyAgent(config.proxy),
+          timeout: config.timeout
+        })
+        break
+      } catch (error) {
+        retry++
+      }
+    }
+    if (res.data && res.data.message) {
+      throw new Error(`status: ${res.status} messgae: ${res.data.message}`)
+    }
+    return res
+  },
 
-      return response.data
-    } catch (error) {
-      console.error(error)
+  async * fetchPaged (resourceUri, prop = 'values', maxRetry = 3, startAt = 0, pageSize = 100) {
+    resourceUri = `${resourceUri}${resourceUri.includes('?') ? '&' : '?'}`
+
+    let total = Number.MAX_VALUE
+
+    while (startAt < total) {
+      const res = await module.exports.fetch(`${resourceUri}maxResults=${pageSize}&startAt=${startAt}`, maxRetry)
+      total = res.data.total || 0
+      const list = res.data[prop]
+      startAt += list.length
+      for (const item of list) {
+        yield item
+      }
     }
   }
 }
