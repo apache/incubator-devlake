20cf5ddff8a52f59a8c84dded884fec73fc3551a
Klesh Wong <zhenmian.huang@merico.dev>
2021-07-29 22:28:56 +0800

    feat: gitlab commit/mr data collecting

--- src/plugins/gitlab-pond/index.js
+++ src/plugins/gitlab-pond/index.js
@@ -1,15 +1,19 @@
-const collectionManager = require('./src/collector/collection-manager')
-const enrichment = require('gitlab-pond/src/enricher')
+const commits = require('./src/collector/commits')
+const mergeRequests = require('./src/collector/merge-requests')
+const projects = require('./src/collector/projects')
+const enrichment = require('./src/enricher')
 
 module.exports = {
   collector: {
     name: 'gitlabCollector',
     exec: async function (rawDb, options) {
-      console.log('INFO >>> gitlab collecting')
-      await collectionManager.collectProjectsDetails(options, rawDb)
-      await collectionManager.collectProjectCommits(options, rawDb)
-      await collectionManager.collectProjectMergeRequests(options, rawDb)
-      console.log('INFO >>> done collecting')
+      const args = { db: rawDb, ...options }
+
+      console.info('INFO >>> gitlab collecting', options)
+      await commits.collect(args)
+      await mergeRequests.collect(args)
+      await projects.collect(args)
+      console.info('INFO >>> gitlab collecting done!')
 
       return {
         ...options,
@@ -25,4 +29,20 @@ module.exports = {
       return []
     }
   }
+}
+
+if (require.main === module) {
+  require('module-alias/register')
+  const dbConnector = require('@mongo/connection')
+  const enrichedDb = require('@db/postgres');
+
+  (async function() {
+    const { db, client } = await dbConnector.connect()
+    try {
+      await module.exports.collector.exec(db, { projectId: process.argv[2] })
+      // await module.exports.enricher.exec(db, enrichedDb, { projectId: process.argv[2] })
+    } finally {
+      dbConnector.disconnect(client)
+    }
+  })()
 }
