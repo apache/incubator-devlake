c9d4ca6216424e14ff49cf2220ad54358bb20efd
Jonathan O'Donnell <joncodo@meri.co>
2021-07-30 16:05:54 -0300

    feat(gitlab reviewers): save the reviewers in enriched psql and show in grafana
    migrations, enrichment and grafana in home dashboard

---
+++ db/migrations/20210730185809-reviewer-as-array.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('gitlab_merge_requests', 'reviewers', {
+      type: Sequelize.ARRAY(Sequelize.STRING),
+      allowNull: true,
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    
+  }
+};
