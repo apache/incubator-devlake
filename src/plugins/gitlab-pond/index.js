270bfc0460cfe9dfa1010989483737aee1d94549
Hezheng Yin <hezheng@merico.dev>
2021-07-29 20:30:23 -0700

    refactor: switch the implementation of gitlab commits and merge requests collectors

--- src/plugins/gitlab-pond/index.js
+++ src/plugins/gitlab-pond/index.js
@@ -1,14 +1,23 @@
 const collectionManager = require('./src/collector/collection-manager')
 const enrichment = require('gitlab-pond/src/enricher')
+const commits = require('./src/collector/commits')
+const mergeRequests = require('./src/collector/merge-requests')
 
 module.exports = {
   collector: {
     name: 'gitlabCollector',
     exec: async function (rawDb, options) {
       console.log('INFO >>> gitlab collecting')
+      console.log(options)
       await collectionManager.collectProjectsDetails(options, rawDb)
-      await collectionManager.collectProjectCommits(options, rawDb)
-      await collectionManager.collectProjectMergeRequests(options, rawDb)
+      for (const projectId of options.projectIds) {
+        await commits.collect({ rawDb, projectId })
+      }
+      for (const projectId of options.projectIds) {
+        await mergeRequests.collect({ rawDb, projectId })
+      }
+      // await collectionManager.collectProjectCommits(options, rawDb)
+      // await collectionManager.collectProjectMergeRequests(options, rawDb)
       console.log('INFO >>> done collecting')
 
       return {
