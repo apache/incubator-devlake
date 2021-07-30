e0ab17de1fdaf56abb6c3f6bb19eb1dbf1a63b09
Jonathan O'Donnell <joncodo@meri.co>
2021-07-30 09:57:02 -0300

    fix: fixing breaking main branch

--- src/plugins/gitlab-pond/src/util/mongo.js
+++ src/plugins/gitlab-pond/src/util/mongo.js
@@ -1,17 +1,14 @@
-// const dbConnector = require('@mongo/connection')
-
-// const mongo = {
-//   async storeRawData (data, collectionName) {
-//     const { client, db } = await dbConnector.connect()
-//     try {
-//       const collection = await dbConnector.findOrCreateCollection(db, collectionName)
-//       await collection.insert(data)
-//     } catch (error) {
-//       console.error(error)
-//     } finally {
-//       dbConnector.disconnect(client)
-//     }
-//   }
-// }
-
-// module.exports = mongo
+module.exports = {
+  async fetchCollectionData (modelName, id, uriComponent = '') {
+    const requestUri = `${modelName}/${id}${uriComponent && '/' + uriComponent}`
+    console.log('INFO: requestUri', requestUri)
+    return fetcher.fetch(requestUri)
+  },
+  
+  async findCollection (collectionName, where, db, limit = 99999999) {
+    console.log(`INFO >>> ${collectionName} where`, where)
+    const collection = await findOrCreateCollection(db, collectionName)
+    const collectionDataCursor = await collection.find(where).limit(limit)
+    return await collectionDataCursor.toArray()
+  }
+}
