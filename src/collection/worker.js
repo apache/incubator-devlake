0728bc9712aff6ad202aabe27ffd01f8142a55e4
Klesh Wong <zhenmian.huang@merico.dev>
2021-08-01 01:22:15 +0800

    feat: kanban-repo mapping and compound figures
    1. Add plugin initialization mechanism so plugin can do some preparation work
    2. Kanban-Repo mapping through JiraBaordGitlabProject model
    3. Add CompoundFigure enricher plugin to copy `jiraBoardGitlabProject` configuration to database

--- src/collection/worker.js
+++ src/collection/worker.js
@@ -5,13 +5,14 @@ const axios = require('axios')
 const _has = require('lodash/has')
 
 const dbConnector = require('@mongo/connection')
-const { collection } = require('../plugins')
+const { buildPluginRegistry } = require('../plugins')
 const consumer = require('../queue/consumer')
 const enrichmentApiUrl = require('@config/resolveConfig').enrichment.connectionString
 
 const queue = 'collection'
 
 const jobHandler = async (job) => {
+  const collection = await buildPluginRegistry('collector')
   const {
     db, client
   } = await dbConnector.connect()
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,11 +1,14 @@
+require('module-alias/register')
 const registryConfig = require('../../config/plugins')
+const dbConnector = require('@mongo/connection')
+const enrichedDb = require('@db/postgres')
 
 module.exports = {
   collection: buildPluginRegistry('collector'),
   enrichment: buildPluginRegistry('enricher')
 }
 
-function buildPluginRegistry (type) {
+async function buildPluginRegistry (type) {
   const pluginRegistry = pluginRegistryFactory()
 
   for (const pluginModule of registryConfig) {
@@ -18,6 +21,8 @@ function buildPluginRegistry (type) {
     pluginRegistry.register(plugin[pluginModule.name])
   }
 
+  await pluginRegistry.initialize()
+
   return pluginRegistry
 }
 
@@ -28,6 +33,28 @@ function pluginRegistryFactory () {
     register (plugin) {
       const { name, exec } = plugin
       this.plugins[name] = exec
+    },
+
+    // initialize all plugins due to some of them need to do some preparation
+    // all database migration should be moved to plugin initialization in the
+    // future
+    async initialize () {
+      const {
+        db: rawDb, client
+      } = await dbConnector.connect()
+
+      try {
+        for (const key of Object.keys(this.plugins)) {
+          const plugin = this.plugins[key]
+          if (typeof plugin.initialize === 'function') {
+            plugin.initialize(rawDb, enrichedDb, this.plugins)
+          }
+        }
+      } finally {
+        dbConnector.disconnect(client)
+      }
     }
   }
 }
+
+module.exports = { buildPluginRegistry }
