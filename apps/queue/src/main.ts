f7ff485bfa767153bba0ca0ca69e8e77494a6046
Jinxiu Zhou <jinxiu.zhou@merico.dev>
2021-08-02 15:20:34 +0800

    chore: change queue service to microservice

--- apps/queue/src/main.ts
+++ apps/queue/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { QueueModule } from './queue.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(QueueModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(QueueModule);
+  await app.listen();
 }
 bootstrap();
