2ef3219abff5cdaee4c194582a0dab1181915c6c
Hezheng Yin <hezheng@merico.dev>
2021-08-03 16:35:20 -0700

    refactor: modify skip flags to only affect collection phase

--- src/plugins/gitlab-pond/src/collector/index.js
+++ src/plugins/gitlab-pond/src/collector/index.js
@@ -6,10 +6,18 @@ const { gitlab } = require('@config/resolveConfig')
 
 async function collect (db, { projectId, forceAll }) {
   const args = { db, projectId: Number(projectId), forceAll }
-  !gitlab.skip.projects && await projects.collect(args)
-  !gitlab.skip.commits && await commits.collect(args)
-  !gitlab.skip.mergeRequests && await mergeRequests.collect(args)
-  !gitlab.skip.notes && await notes.collect(args)
+  const skipFlags = gitlab.skip
+  if (skipFlags) {
+    !skipFlags.projects && await projects.collect(args)
+    !skipFlags.commits && await commits.collect(args)
+    !skipFlags.mergeRequests && await mergeRequests.collect(args)
+    !skipFlags.notes && await notes.collect(args)
+  } else {
+    await projects.collect(args)
+    await commits.collect(args)
+    await mergeRequests.collect(args)
+    await notes.collect(args)
+  }
 }
 
 module.exports = { collect }
