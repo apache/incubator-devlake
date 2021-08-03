1796c681d9fd2bdb234cd770d397fb65dc7a6946
Hezheng Yin <hezheng@merico.dev>
2021-08-03 16:35:20 -0700

    refactor: modify skip flags to only affect collection phase

--- src/plugins/gitlab-pond/src/enricher/index.js
+++ src/plugins/gitlab-pond/src/enricher/index.js
@@ -2,14 +2,13 @@ const projects = require('./projects')
 const commits = require('./commits')
 const mergeRequests = require('./merge-requests')
 const notes = require('./notes')
-const { gitlab } = require('@config/resolveConfig')
 
 async function enrich (rawDb, enrichedDb, { projectId }) {
   const args = { rawDb, enrichedDb, projectId: Number(projectId) }
-  !gitlab.skip.projects && await projects.enrich(args)
-  !gitlab.skip.commits && await commits.enrich(args)
-  !gitlab.skip.mergeRequests && await mergeRequests.enrich(args)
-  !gitlab.skip.notes && await notes.enrich(args)
+  await projects.enrich(args)
+  await commits.enrich(args)
+  await mergeRequests.enrich(args)
+  await notes.enrich(args)
 }
 
 module.exports = { enrich }
