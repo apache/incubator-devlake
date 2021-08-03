f8bf5c793e128e7a37dff9facb6bb0f4b5638dd9
Jonathan O'Donnell <joncodo@meri.co>
2021-08-03 12:57:51 -0300

    feat(first commit time): adding in first commit time to merge requests

---
+++ db/migrations/20210803155513-add-first-note-time.js
@@ -0,0 +1,27 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const transaction = await queryInterface.sequelize.transaction()
+    try {
+      await queryInterface.addColumn('gitlab_merge_requests', 'first_comment_time', {
+        type: Sequelize.DataTypes.DATE
+      }, { transaction })
+      await transaction.commit()
+    } catch (err) {
+      await transaction.rollback()
+      throw err
+    }
+  },
+
+  down: async (queryInterface) => {
+    const transaction = await queryInterface.sequelize.transaction()
+    try {
+      await queryInterface.removeColumn('gitlab_merge_requests', 'projfirst_comment_timeect_id', { transaction })
+      await transaction.commit()
+    } catch (err) {
+      await transaction.rollback()
+      throw err
+    }
+  }
+}
