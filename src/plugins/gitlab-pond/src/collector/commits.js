0bf076c8f4ca3ef7a01b79d4e3ec8b85a67cb448
Jonathan O'Donnell <joncodo@meri.co>
2021-08-04 09:34:51 -0300

    feat(gitlab branch): this allows you to set the branch you want in the post request
    You can now analyze a branch other than the main branch of the repo

--- src/plugins/gitlab-pond/src/collector/commits.js
+++ src/plugins/gitlab-pond/src/collector/commits.js
@@ -2,18 +2,25 @@ require('module-alias/register')
 const { findOrCreateCollection } = require('../../../commondb')
 const fetcher = require('./fetcher')
 
-async function collect ({ db, projectId, forceAll }) {
+async function collect ({ db, projectId, branch, forceAll }) {
   if (!projectId) {
     throw new Error('Failed to collect gitlab data, projectId is required')
   }
 
-  await collectByProjectId(db, projectId, forceAll)
+  await collectByProjectId(db, projectId, branch, forceAll)
 }
 
-async function collectByProjectId (db, projectId, forceAll) {
+async function collectByProjectId (db, projectId, branch, forceAll) {
   console.info('INFO >>> gitlab collecting commits for project', projectId)
   const commitsCollection = await getCollection(db)
-  for await (const commit of fetcher.fetchPaged(`projects/${projectId}/repository/commits?with_stats=true`)) {
+
+  let queryParams = 'withStats=true'
+  // in some cases, the user does not want to pull commits from the default branch.
+  if (branch) {
+    queryParams += `&ref_name=${branch}`
+  }
+
+  for await (const commit of fetcher.fetchPaged(`projects/${projectId}/repository/commits?${queryParams}`)) {
     commit.projectId = projectId
     await commitsCollection.findOneAndUpdate(
       { id: commit.id },
