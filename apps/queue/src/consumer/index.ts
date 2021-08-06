64307179479ed3e7ab27a2a3a0beb83ed4f3041d
Jinxiu Zhou <jinxiu.zhou@merico.dev>
2021-08-06 09:59:13 +0800

    feat: change provider in consumer to plugins

--- apps/queue/src/consumer/index.ts
+++ apps/queue/src/consumer/index.ts
@@ -1,14 +1,18 @@
 import { DynamicModule } from '@nestjs/common';
 import { BullQueueModule } from '../bull/queue.module';
-import { providers } from './providers';
+import { PluginModule } from './plugins';
 import { ConsumerService } from './service';
 
 export class ConsumerModule {
   static forRoot(queue = 'default'): DynamicModule {
     return {
       module: ConsumerModule,
-      imports: [BullQueueModule.forRoot(queue), ConsumerService],
-      providers,
+      imports: [
+        BullQueueModule.forRoot(queue),
+        PluginModule.forRootAsync(),
+        ConsumerService,
+      ],
+      providers: [],
       exports: [ConsumerService],
     };
   }
--- apps/queue/src/consumer/providers.ts
+++
@@ -1,9 +0,0 @@
-import { ClassProvider } from '@nestjs/common';
-import Jira from 'plugins/jira/src';
-
-const JiraProvider: ClassProvider<Jira> = {
-  provide: 'Jira',
-  useClass: Jira,
-};
-
-export const providers = [JiraProvider];
