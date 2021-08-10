e24aaf4ab6331a1758b38454fe2ebdc8d6cebc22
Klesh Wong <zhenmian.huang@merico.dev>
2021-08-10 15:35:54 +0800

    feat: trigger enrichment only when data was collected successfully 1. Enrichment should not be triggered if collection failed, especially for first time setup 2. User might trigger collection process by calling api, we should check if data exists beforehand    and print out a meaningful error message to help user understand the situation.

--- src/plugins/jira-pond/src/enricher/index.js
+++ src/plugins/jira-pond/src/enricher/index.js
@@ -1,7 +1,15 @@
 const boards = require('./boards')
 const issues = require('./issues')
+const boardsCollector = require('../collector/boards')
 
 async function enrich (rawDb, enrichedDb, { boardId, forceAll }) {
+  // verify collected data existence
+  const boardsCollection = await boardsCollector.getCollection(rawDb)
+  const board = await boardsCollection.findOne({ id: boardId })
+  if (!board) {
+    throw new Error(`jiraEnricher: unable to find collected data for board ${boardId}`)
+  }
+
   const args = { rawDb, enrichedDb, boardId: Number(boardId), forceAll }
   await boards.enrich(args)
   await issues.enrich(args)
