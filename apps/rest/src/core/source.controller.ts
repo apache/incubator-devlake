fdfc832c6f9b27545061a8cdfcfd533b067085fe
narro <cong.wang@merico.dev>
2021-08-12 17:57:16 +0800

    feat: add source api definition

--- apps/rest/src/core/source.controller.ts
+++ apps/rest/src/core/source.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import Source from './source.model';
 import { SourceService } from './source.service';
 import { CreateSource } from './source.type';
@@ -10,5 +10,25 @@ export class SourceController {
   @Post()
   async create(@Body() source: CreateSource): Promise<Source> {
     return await this.sourceService.create(source);
+  }
+
+  @Get()
+  async list(): Promise<Source[]> {
+    return [];
+  }
+
+  @Get()
+  async get(): Promise<Source> {
+    return;
+  }
+
+  @Put()
+  async update(): Promise<Source> {
+    return;
+  }
+
+  @Delete()
+  async delete(): Promise<Source> {
+    return;
   }
 }
