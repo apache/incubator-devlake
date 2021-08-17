b32c251ef8f27eb75771eb3de8eb3f15ec036b43
narro <cong.wang@merico.dev>
2021-08-17 15:11:29 +0800

    refactor: reorg rest-api folders

--- apps/rest/src/app.module.ts
+++ apps/rest/src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService } from './services/app';
 import CustomTypeOrmModule from './providers/typeorm.module';
 import entities from './models';
+import { AppController } from './controllers/app';
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+    }),
     CustomTypeOrmModule.forRootAsync(null, {
       entities,
       // FIXME: using db migration instead of synchronize
--- apps/rest/src/controllers/app.spec.ts
+++ apps/rest/src/controllers/app.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './app';
+import { AppService } from '../services/app';
 
 describe('AppController', () => {
   let appController: AppController;
