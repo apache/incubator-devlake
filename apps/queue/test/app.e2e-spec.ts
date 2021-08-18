3a1a60248c4033e0fc48cd08539d04a5971bb921
Jinxiu Zhou <jinxiu.zhou@merico.dev>
2021-08-18 09:42:02 +0800

    test(apps:e2e): fix the e2e tests error

--- apps/queue/test/app.e2e-spec.ts
+++ apps/queue/test/app.e2e-spec.ts
@@ -1,24 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, INestMicroservice } from '@nestjs/common';
 import * as request from 'supertest';
 import { QueueModule } from './../src/queue.module';
+import { ConsumerService } from '../src/consumer/service';
+import { ProducerService } from '../src/producer/service';
 
 describe('QueueController (e2e)', () => {
-  let app: INestApplication;
+  let app: INestMicroservice;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [QueueModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestMicroservice({});
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('Consumer', () => {
+    const consumer = app.get(ConsumerService);
+    expect(consumer).toBeDefined();
+  });
+
+  it('Producer', () => {
+    const producer = app.get(ProducerService);
+    expect(producer).toBeDefined();
   });
 });
