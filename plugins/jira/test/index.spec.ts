37f05af3a32a7ffe9ac21401a680affb7fd495a8
Jinxiu Zhou <jinxiu.zhou@merico.dev>
2021-08-19 09:48:54 +0800

    test: add plugin tests

--- plugins/jira/test/index.spec.ts
+++ plugins/jira/test/index.spec.ts
@@ -1,8 +1,19 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import PluginModule from 'plugins/core/src/plugins.module';
+import Task from 'plugins/core/src/task.interface';
 import Jira, { JiraOptions } from '../src';
 
 describe('Jira Plugin', () => {
+  let app: TestingModule;
+
+  beforeAll(async () => {
+    app = await Test.createTestingModule({
+      imports: [PluginModule.forRootAsync([Jira])],
+    }).compile();
+  });
+
   it('Jira', async () => {
-    const jira = new Jira();
+    const jira = await app.resolve<Task>('Jira');
     expect(jira).toBeDefined();
     expect(jira.execute).toBeDefined();
   });
