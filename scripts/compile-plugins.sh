6543136ac99a0da6afbf7fd740157620c3f5bdf3
linyh <yanghui@meri.co>
2021-08-26 22:43:31 +0800

    fix: fix for ci

--- scripts/compile-plugins.sh
+++ scripts/compile-plugins.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-for PLUG in $(find plugins/* -type d -not -name core); do
+for PLUG in $(find plugins/* -maxdepth 0 -type d -not -name core -not -empty); do
   NAME=$(basename $PLUG)
+  echo $PLUG/$NAME
   go build -buildmode=plugin -o $PLUG/$NAME.so $PLUG/*.go
 done
