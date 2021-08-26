418997032ca947fbb52ede1ba68f1a9d2ae1e967
Cong Wang <cong.wang@merico.dev>
2021-08-26 18:01:29 +0800

    feat: trigger plugins when post task

--- api/controllers/task/task.go
+++ api/controllers/task/task.go
@@ -1,6 +1,7 @@
 package task
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -8,6 +9,7 @@ import (
 	"github.com/merico-dev/lake/api/services"
 	"github.com/merico-dev/lake/api/types"
 	"github.com/merico-dev/lake/logger"
+	"github.com/merico-dev/lake/plugins"
 )
 
 func Post(ctx *gin.Context) {
@@ -23,6 +25,17 @@ func Post(ctx *gin.Context) {
 		_ = ctx.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
+
+	// trigger plugins
+	data.Options["ID"] = task.ID
+
+	go func() {
+		progress := make(chan float32)
+		_ = plugins.RunPlugin(task.Plugin, data.Options, progress)
+		for p := range progress {
+			fmt.Printf("running plugin %v, progress: %v\n", task.Plugin, p*100)
+		}
+	}()
+
 	ctx.JSON(http.StatusCreated, task)
-	// TODO: trigger plugin task
 }
