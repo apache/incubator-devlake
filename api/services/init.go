9ab89e63c6f4fc02f4ef1d1676943b8b05dd0cdc
linyh <yanghui@meri.co>
2021-08-26 14:32:08 +0800

    feat: add entity and init() in plugin
    #285 #286 #288

--- api/services/init.go
+++ api/services/init.go
@@ -7,12 +7,12 @@ import (
 	"gorm.io/gorm"
 )
 
-var db *gorm.DB
+var Db *gorm.DB
 
 func init() {
 	var connectionString = config.V.GetString("DB_URL")
 	var err error
-	db, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+	Db, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
@@ -20,7 +20,7 @@ func init() {
 }
 
 func migrateDB() {
-	err := db.AutoMigrate(&models.Source{})
+	err := Db.AutoMigrate(&models.Source{})
 	if err != nil {
 		panic(err)
 	}
