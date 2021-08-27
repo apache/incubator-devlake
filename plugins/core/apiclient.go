1dcfe4fb1ef84e68dee7cc73327e70170f9c91f1
Klesh Wong <zhenmian.huang@merico.dev>
2021-08-27 19:54:38 +0800

    feat: add jira board collection

---
+++ plugins/core/apiclient.go
@@ -0,0 +1,137 @@
+package core
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type ApiClientBeforeRequest func(req *http.Request) error
+type ApiClientAfterResponse func(res *http.Response) error
+
+type ApiClient struct {
+	client        *http.Client
+	endpoint      string
+	headers       map[string]string
+	maxRetry      int
+	beforeRequest ApiClientBeforeRequest
+	afterReponse  ApiClientAfterResponse
+}
+
+func NewApiClient(
+	endpoint string,
+	headers map[string]string,
+	timeout time.Duration,
+	maxRetry int,
+) *ApiClient {
+	return &ApiClient{
+		client:        &http.Client{Timeout: timeout},
+		endpoint:      endpoint,
+		headers:       headers,
+		maxRetry:      maxRetry,
+		beforeRequest: nil,
+		afterReponse:  nil,
+	}
+}
+
+func (apiClient *ApiClient) SetHeaders(headers map[string]string) {
+	apiClient.headers = headers
+}
+
+func (apiClient *ApiClient) SetProxy(proxyUrl string) error {
+	pu, err := url.Parse(proxyUrl)
+	if err != nil {
+		return err
+	}
+	apiClient.client.Transport = &http.Transport{Proxy: http.ProxyURL(pu)}
+	return nil
+}
+
+func (apiClient *ApiClient) Do(
+	method string,
+	path string,
+	query *url.Values,
+	body *map[string]interface{},
+	headers *map[string]string,
+) (*http.Response, error) {
+	uri := apiClient.endpoint + path
+
+	// append query
+	if query != nil {
+		queryString := query.Encode()
+		if queryString != "" {
+			uri += "?" + queryString
+		}
+	}
+
+	// process body
+	var reqBody io.Reader
+	if body != nil {
+		reqJson, err := json.Marshal(body)
+		if err != nil {
+			return nil, err
+		}
+		reqBody = bytes.NewBuffer(reqJson)
+	}
+	req, err := http.NewRequest(method, uri, reqBody)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	// populate headers
+	if apiClient.headers != nil {
+		for name, value := range apiClient.headers {
+			req.Header.Set(name, value)
+		}
+	}
+	if headers != nil {
+		for name, value := range *headers {
+			req.Header.Set(name, value)
+		}
+	}
+
+	// before send
+	if apiClient.beforeRequest != nil {
+		err = apiClient.beforeRequest(req)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	res, err := apiClient.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// after recieve
+	if apiClient.afterReponse != nil {
+		err = apiClient.afterReponse(res)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return res, nil
+}
+
+func (apiClient *ApiClient) Get(
+	path string,
+	query *url.Values,
+	headers *map[string]string,
+) (*http.Response, error) {
+	return apiClient.Do("GET", path, query, nil, headers)
+}
+
+func UnmarshalResponse(res *http.Response, v interface{}) error {
+	defer res.Body.Close()
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(resBody, &v)
+}
--- test/plugins/plugins_test.go
+++ test/plugins/plugins_test.go
@@ -1,12 +1,12 @@
 package plugins
 
 import (
-	"fmt"
-	"github.com/merico-dev/lake/plugins"
 	"path"
 	"runtime"
 	"strings"
 	"testing"
+
+	"github.com/merico-dev/lake/plugins"
 )
 
 func TestPluginsLoading(t *testing.T) {
@@ -18,24 +18,23 @@ func TestPluginsLoading(t *testing.T) {
 	}
 	if len(plugins.Plugins) == 0 {
 		t.Errorf("No plugin found")
-		return
 	}
 
-	name := "jira"
-	options := map[string]interface{}{
-		"boardId": 20,
-	}
-	progress := make(chan float32)
-	fmt.Printf("start runing plugin %v\n", name)
-	go func() {
-		_ = plugins.RunPlugin(name, options, progress)
-	}()
-	for p := range progress {
-		fmt.Printf("running plugin %v, progress: %v\n", name, p*100)
-	}
-	fmt.Printf("end running plugin %v\n", name)
-	if err != nil {
-		t.Error(err)
-	}
+	// name := "jira"
+	// options := map[string]interface{}{
+	// 	"boardId": 20,
+	// }
+	// progress := make(chan float32)
+	// fmt.Printf("start runing plugin %v\n", name)
+	// go func() {
+	// 	_ = plugins.RunPlugin(name, options, progress)
+	// }()
+	// for p := range progress {
+	// 	fmt.Printf("running plugin %v, progress: %v\n", name, p*100)
+	// }
+	// fmt.Printf("end running plugin %v\n", name)
+	// if err != nil {
+	// 	t.Error(err)
+	// }
 
 }
