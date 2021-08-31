9452910a0f1dc08e3e9f1453df2f67d6eaa8bf29
Cong Wang <cong.wang@merico.dev>
2021-08-31 18:04:33 +0800

    feat: save request for jenkins build
    now we only request 2 api for a jenkins job

--- plugins/jenkins/tasks/worker.go
+++ plugins/jenkins/tasks/worker.go
@@ -3,9 +3,11 @@ package tasks
 import (
 	"context"
 	"net/http"
+	"time"
 
 	"github.com/bndr/gojenkins"
 	"github.com/merico-dev/lake/logger"
+	"github.com/merico-dev/lake/plugins/jenkins/models"
 )
 
 type JenkinsWorker struct {
@@ -28,10 +30,41 @@ func (worker *JenkinsWorker) SyncJobs() {
 		logger.Error("Failed to get jobs from jenkins", err)
 		return
 	}
-	err = worker.storage.SaveJobs(jobs)
+	for _, job := range jobs {
+		worker.syncJob(ctx, job)
+	}
+}
+
+func (worker *JenkinsWorker) syncJob(ctx context.Context, job *gojenkins.Job) {
+	jobCtx, err := worker.storage.SaveJob(models.JenkinsJobProps{
+		Name:  job.Raw.Name,
+		Class: job.Raw.Class,
+		Color: job.Raw.Color,
+	})
 	if err != nil {
-		logger.Error("Failed to save jobs", err)
+		logger.Error("failed to save job", err)
 		return
 	}
-	// TODO: get builds
+	var builds struct {
+		Builds []models.JenkinsBuildProps `json:"allBuilds"`
+	}
+	_, err = job.Jenkins.Requester.GetJSON(ctx, job.Base, &builds, map[string]string{"tree": "allBuilds[number,timestamp,duration,estimatedDuration,displayName,result]"})
+	if err != nil {
+		logger.Error("failed to get jenkins job builds", err)
+		return
+	}
+	// jenkins api is not supported to filter data with timestampe
+	// so we need to filter it manually
+	timestampHalfYearAgo := time.Now().AddDate(0, -6, 0).Unix() * 1000
+	var filteredData = make([]models.JenkinsBuildProps, 0)
+	for _, item := range builds.Builds {
+		if item.Timestamp >= timestampHalfYearAgo {
+			filteredData = append(filteredData, item)
+		}
+	}
+	if len(filteredData) == 0 {
+		logger.Info("job has no build exists in past half year", job.Raw.Name)
+		return
+	}
+	worker.storage.SaveBuilds(filteredData, jobCtx)
 }
