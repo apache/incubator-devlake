23f520a8a9bf98dd27f1ed6f045e4d576edcb1d3
Klesh Wong <zhenmian.huang@merico.dev>
2021-09-01 17:35:38 +0800

    feat: add jira-api-client for pagination handling

--- plugins/jira/tasks/jiraapiclient.go
+++ plugins/jira/tasks/jiraapiclient.go
@@ -2,17 +2,25 @@ package tasks
 
 import (
 	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
 	"time"
 
 	"github.com/merico-dev/lake/config"
 	"github.com/merico-dev/lake/plugins/core"
 )
 
-var jiraApiClient *core.ApiClient
+type JiraApiClient struct {
+	core.ApiClient
+}
 
-func GetJiraApiClient() *core.ApiClient {
+var jiraApiClient *JiraApiClient
+
+func GetJiraApiClient() *JiraApiClient {
 	if jiraApiClient == nil {
-		jiraApiClient = core.NewApiClient(
+		jiraApiClient = &JiraApiClient{}
+		jiraApiClient.Setup(
 			config.V.GetString("JIRA_ENDPOINT"),
 			map[string]string{
 				"Authorization": fmt.Sprintf("Basic %v", config.V.GetString("JIRA_BASIC_AUTH_ENCODED")),
@@ -22,4 +30,33 @@ func GetJiraApiClient() *core.ApiClient {
 		)
 	}
 	return jiraApiClient
+}
+
+type JiraPaginationHandler func(res *http.Response) (*JiraPagination, error)
+
+func (jiraApiClient *JiraApiClient) FetchPages(path string, query *url.Values, handler JiraPaginationHandler) error {
+	if query == nil {
+		query = &url.Values{}
+	}
+	nextStart, total, query := 0, 1, &url.Values{}
+	for nextStart < total {
+		// fetch page
+		query.Set("maxResults", "100")
+		query.Set("startAt", strconv.Itoa(nextStart))
+		res, err := jiraApiClient.Get(path, query, nil)
+		if err != nil {
+			return err
+		}
+
+		// call page handler
+		pagination, err := handler(res)
+		if err != nil {
+			return err
+		}
+
+		// next page
+		nextStart = pagination.StartAt + pagination.MaxResults
+		total = pagination.Total
+	}
+	return nil
 }
