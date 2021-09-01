ccc9f51024a063436261854106a572c373a31568
kevin-kline <21stcenturykline@gmail.com>
2021-09-01 11:01:46 -0300

    feat: merge request and reviewer collection

---
+++ plugins/gitlab/models/gitlabReviewers.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"github.com/merico-dev/lake/models"
+)
+
+type GitlabReviewer struct {
+	GitlabId       int `gorm:"primary_key"`
+	MergeRequestId int
+	Name           string
+	Username       string
+	State          string
+	AvatarUrl      string
+	WebUrl         string
+	models.NoPKModel
+}
---
+++ plugins/gitlab/tasks/gitlabMergeRequestCollector.go
@@ -0,0 +1,84 @@
+package tasks
+
+import (
+	"fmt"
+
+	"github.com/merico-dev/lake/logger"
+	lakeModels "github.com/merico-dev/lake/models"
+	"github.com/merico-dev/lake/plugins/core"
+	"github.com/merico-dev/lake/plugins/gitlab/models"
+	"gorm.io/gorm/clause"
+)
+
+type ApiMergeRequestResponse []struct {
+	GitlabId        int `json:"id"`
+	ProjectId       int `json:"project_id"`
+	State           string
+	Title           string
+	Description     string
+	WebUrl          string `json:"web_url"`
+	UserNotesCount  int    `json:"user_notes_count"`
+	WorkInProgress  bool   `json:"work_in_progress"`
+	SourceBranch    string `json:"source_branch"`
+	MergedAt        string `json:"merged_at"`
+	GitlabCreatedAt string `json:"created_at"`
+	ClosedAt        string `json:"closed_at"`
+	MergedBy        struct {
+		Username string `json:"username"`
+	} `json:"merged_by"`
+	Author struct {
+		Username string `json:"username"`
+	}
+	Reviewers []Reviewer
+}
+
+func CollectMergeRequests(projectId int) error {
+	gitlabApiClient := CreateApiClient()
+
+	res, err := gitlabApiClient.Get(fmt.Sprintf("projects/%v/merge_requests", projectId), nil, nil)
+	if err != nil {
+		return err
+	}
+
+	gitlabApiResponse := &ApiMergeRequestResponse{}
+
+	logger.Info("res", res)
+
+	err = core.UnmarshalResponse(res, gitlabApiResponse)
+
+	if err != nil {
+		logger.Error("Error: ", err)
+		return nil
+	}
+
+	for _, mr := range *gitlabApiResponse {
+		gitlabMergeRequest := &models.GitlabMergeRequest{
+			GitlabId:         mr.GitlabId,
+			ProjectId:        mr.ProjectId,
+			State:            mr.State,
+			Title:            mr.Title,
+			Description:      mr.Description,
+			WebUrl:           mr.WebUrl,
+			UserNotesCount:   mr.UserNotesCount,
+			WorkInProgress:   mr.WorkInProgress,
+			SourceBranch:     mr.SourceBranch,
+			MergedAt:         mr.MergedAt,
+			GitlabCreatedAt:  mr.GitlabCreatedAt,
+			ClosedAt:         mr.ClosedAt,
+			MergedByUsername: mr.MergedBy.Username,
+			AuthorUsername:   mr.Author.Username,
+		}
+
+		err = lakeModels.Db.Clauses(clause.OnConflict{
+			UpdateAll: true,
+		}).Create(&gitlabMergeRequest).Error
+
+		if err != nil {
+			logger.Error("Could not upsert: ", err)
+		}
+
+		CreateReviewers(mr.GitlabId, mr.Reviewers)
+	}
+
+	return nil
+}
