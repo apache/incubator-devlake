2f2ab8cfa5089b127f2922f4e853cc56eef098e0
Jinxiu Zhou <jinxiu.zhou@merico.dev>
2021-09-02 16:24:44 +0800

    test: add jira issue collector tests

---
+++ test/plugins/jira/tasks/jiraussuecollector_test.go
@@ -0,0 +1,19 @@
+package plugins
+
+import (
+	"testing"
+	lakeModels "github.com/merico-dev/lake/models"
+	"github.com/merico-dev/lake/plugins/jira/models"
+	"github.com/merico-dev/lake/plugins/jira/tasks"
+)
+
+func TestPluginsLoading(t *testing.T) {
+	err := lakeModels.Db.AutoMigrate(&models.JiraIssue{}, &models.JiraBoard{})
+	if err != nil {
+		t.Errorf("Failed to Migrate %v", err)
+	}
+	collectErr := tasks.CollectIssues(8)
+	if collectErr != nil {
+		t.Errorf("Failed to Collect Issue %v", collectErr)
+	}
+}
