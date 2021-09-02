4ec3a88f0fbc7a30c4f3a4bbde60bf9d9a199550
kevin-kline <21stcenturykline@gmail.com>
2021-09-02 11:03:24 -0300

    refactor: refactor Jira plugin to use core Api Client for paging

--- plugins/jira/tasks/jiraapiclient.go
+++ plugins/jira/tasks/jiraapiclient.go
@@ -2,27 +2,18 @@ package tasks
 
 import (
 	"fmt"
-	"github.com/merico-dev/lake/logger"
-	"github.com/merico-dev/lake/utils"
-	"net/http"
-	"net/url"
-	"strconv"
 	"time"
 
 	"github.com/merico-dev/lake/config"
 	"github.com/merico-dev/lake/plugins/core"
 )
 
-type JiraApiClient struct {
-	core.ApiClient
-}
+var apiClient *core.ApiClient
 
-var jiraApiClient *JiraApiClient
-
-func GetJiraApiClient() *JiraApiClient {
-	if jiraApiClient == nil {
-		jiraApiClient = &JiraApiClient{}
-		jiraApiClient.Setup(
+func GetJiraApiClient() *core.ApiClient {
+	if apiClient == nil {
+		apiClient = &core.ApiClient{}
+		apiClient.Setup(
 			config.V.GetString("JIRA_ENDPOINT"),
 			map[string]string{
 				"Authorization": fmt.Sprintf("Basic %v", config.V.GetString("JIRA_BASIC_AUTH_ENCODED")),
@@ -31,78 +22,5 @@ func GetJiraApiClient() *JiraApiClient {
 			3,
 		)
 	}
-	return jiraApiClient
-}
-
-type JiraPagination struct {
-	StartAt    int `json:"startAt"`
-	MaxResults int `json:"maxResults"`
-	Total      int `json:"total"`
-}
-
-type JiraPaginationHandler func(res *http.Response) error
-
-func (jiraApiClient *JiraApiClient) FetchPages(path string, query *url.Values, handler JiraPaginationHandler) error {
-	if query == nil {
-		query = &url.Values{}
-	}
-	nextStart, total, pageSize := 0, 1, 100
-
-	// 获取issue总数
-	// get issue count
-	pageQuery := &url.Values{}
-	*pageQuery = *query
-	pageQuery.Set("maxResults", "0")
-	res, err := jiraApiClient.Get(path, query, nil)
-	if err != nil {
-		return err
-	}
-	jiraApiResponse := &JiraPagination{}
-	err = core.UnmarshalResponse(res, jiraApiResponse)
-	if err != nil {
-		logger.Error("Error: ", err)
-		return nil
-	}
-	total = jiraApiResponse.Total
-
-	scheduler, err := utils.NewWorkerScheduler(10, 50)
-	if err != nil {
-		return err
-	}
-	defer scheduler.Release()
-
-	for nextStart < total {
-		nextStartTmp := nextStart
-		err = scheduler.Submit(func() error {
-			// fetch page
-			detailQuery := &url.Values{}
-			*detailQuery = *query
-			detailQuery.Set("maxResults", strconv.Itoa(pageSize))
-			detailQuery.Set("startAt", strconv.Itoa(nextStartTmp))
-			res, err := jiraApiClient.Get(path, query, nil)
-			if err != nil {
-				return err
-			}
-
-			// call page handler
-			err = handler(res)
-			if err != nil {
-				logger.Error("Error: ", err)
-				return err
-			}
-
-			logger.Info("jira api client page loaded", map[string]interface{}{
-				"path":      path,
-				"nextStart": nextStartTmp,
-				"total":     total,
-			})
-			return nil
-		})
-		if err != nil {
-			return err
-		}
-		nextStart += pageSize
-	}
-	scheduler.WaitUntilFinish()
-	return nil
+	return apiClient
 }
