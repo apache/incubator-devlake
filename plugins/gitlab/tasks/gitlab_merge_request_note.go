4e742af6742d37ebf071a0fb625b06baf0ff87e6
kevin-kline <21stcenturykline@gmail.com>
2021-09-03 16:10:41 -0300

    feat: adds FirstCommentTime to GL MRs

--- plugins/gitlab/tasks/gitlab_merge_request_note.go
+++ plugins/gitlab/tasks/gitlab_merge_request_note.go
@@ -3,6 +3,7 @@ package tasks
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/merico-dev/lake/logger"
 	lakeModels "github.com/merico-dev/lake/models"
@@ -11,7 +12,7 @@ import (
 	"gorm.io/gorm/clause"
 )
 
-type ApiMergeRequestNoteResponse []struct {
+type MergeRequestNote struct {
 	GitlabId        int    `json:"id"`
 	NoteableId      int    `json:"noteable_id"`
 	MergeRequestIid int    `json:"noteable_iid"`
@@ -23,11 +24,49 @@ type ApiMergeRequestNoteResponse []struct {
 		Username string `json:"username"`
 	}
 }
+type ApiMergeRequestNoteResponse []MergeRequestNote
 
-func CollectMergeRequestNotes(projectId int, mrId int) error {
+func FindEarliestNote(notes *ApiMergeRequestNoteResponse) (*MergeRequestNote, error) {
+	var earliestNote *MergeRequestNote
+
+	earliestTime := time.Now()
+	for _, note := range *notes {
+		noteTime, parseErr := time.Parse(time.RFC3339, note.GitlabCreatedAt)
+		if parseErr != nil {
+			return nil, parseErr
+		}
+		if noteTime.Before(earliestTime) {
+			earliestTime = noteTime
+			earliestNote = &note
+		}
+	}
+	return earliestNote, nil
+}
+
+// we need a metric that measures a merge request duration as the time from first comment to MR close
+func updateMergeRequestWithFirstCommentTime(notes *ApiMergeRequestNoteResponse, mr *models.GitlabMergeRequest) error {
+	earliestNote, err := FindEarliestNote(notes)
+	if err != nil {
+		return err
+	}
+	if earliestNote != nil {
+		mr.FirstCommentTime = earliestNote.GitlabCreatedAt
+
+		err = lakeModels.Db.Model(&mr).Where("gitlab_id = ?", mr.GitlabId).Clauses(clause.OnConflict{
+			UpdateAll: true,
+		}).Update("FirstCommentTime", mr.FirstCommentTime).Error
+
+		if err != nil {
+			logger.Error("Could not upsert: ", err)
+			return err
+		}
+	}
+	return nil
+}
+func CollectMergeRequestNotes(projectId int, mr *models.GitlabMergeRequest) error {
 	gitlabApiClient := CreateApiClient()
 
-	getUrl := fmt.Sprintf("projects/%v/merge_requests/%v/notes?system=false", projectId, mrId)
+	getUrl := fmt.Sprintf("projects/%v/merge_requests/%v/notes?system=false", projectId, mr.Iid)
 	return gitlabApiClient.FetchWithPaginationAnts(getUrl, "100",
 		func(res *http.Response) error {
 
@@ -59,6 +98,10 @@ func CollectMergeRequestNotes(projectId int, mrId int) error {
 					logger.Error("Could not upsert: ", err)
 					return err
 				}
+			}
+			mergeRequestUpdateErr := updateMergeRequestWithFirstCommentTime(gitlabApiResponse, mr)
+			if mergeRequestUpdateErr != nil {
+				return err
 			}
 			return nil
 		})
