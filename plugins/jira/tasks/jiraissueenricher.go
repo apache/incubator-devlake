a29cc0c574307e16e36eb3313a0c06587f067c03
Klesh Wong <zhenmian.huang@merico.dev>
2021-09-03 16:18:13 +0800

    feat: jira issue type/status mapping
    fixup! feat: jira issue type/status mapping

--- plugins/jira/tasks/jiraissueenricher.go
+++ plugins/jira/tasks/jiraissueenricher.go
@@ -1,6 +1,9 @@
 package tasks
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/merico-dev/lake/config"
 	lakeModels "github.com/merico-dev/lake/models"
 	"github.com/merico-dev/lake/plugins/jira/models"
@@ -35,10 +38,73 @@ func EnrichIssues(boardId uint64) error {
 			jiraIssue.LeadTime = uint(jiraIssue.ResolutionDate.Time.Unix()-jiraIssue.Created.Unix()) / 60
 		}
 		jiraIssue.StdWorkload = uint(jiraIssue.Workload * workloadCoefficient)
+		jiraIssue.StdType = getStdType(jiraIssue)
+		jiraIssue.StdStatus = getStdStatus(jiraIssue)
 		err = lakeModels.Db.Save(jiraIssue).Error
 		if err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+var typeMapping map[string]string
+
+func getStdType(jiraIssue *models.JiraIssue) string {
+	if typeMapping == nil {
+		typeMapping = getStringMapFromConfig("JIRA_ISSUE_TYPE_MAPPING")
+	}
+	if stdType, ok := typeMapping[jiraIssue.Type]; ok {
+		return stdType
+	}
+	return jiraIssue.Type
+}
+
+var statusMappings map[string]map[string]string
+
+func getStdStatus(jiraIssue *models.JiraIssue) string {
+	if statusMappings == nil {
+		statusMappings = make(map[string]map[string]string)
+	}
+	if _, ok := statusMappings[jiraIssue.Type]; !ok {
+		statusMappings[jiraIssue.Type] = getStringMapFromConfig(
+			fmt.Sprintf("JIRA_ISSUE_%v_STATUS_MAPPING", jiraIssue.Type),
+		)
+	}
+	statusMapping := statusMappings[jiraIssue.Type]
+	if stdStatus, ok := statusMapping[jiraIssue.StatusName]; ok {
+		return stdStatus
+	}
+	return jiraIssue.StatusName
+}
+
+func getStringMapFromConfig(key string) map[string]string {
+	mapping := make(map[string]string)
+	mappingCfg := strings.TrimSpace(config.V.GetString(key))
+	if mappingCfg == "" {
+		return mapping
+	}
+	for _, comp := range strings.Split(mappingCfg, ";") {
+		comp := strings.TrimSpace(comp)
+		if comp == "" {
+			continue
+		}
+		tmp := strings.Split(comp, ":")
+		if len(tmp) != 2 {
+			panic(fmt.Errorf("invalid config %v: %v", key, comp))
+		}
+		std := strings.TrimSpace(tmp[0])
+		if std == "" {
+			panic(fmt.Errorf("invalid config %v: %v", key, comp))
+		}
+		orgs := tmp[1]
+		for _, org := range strings.Split(orgs, ",") {
+			org := strings.TrimSpace(org)
+			if org == "" {
+				continue
+			}
+			mapping[org] = std
+		}
+	}
+	return mapping
 }
