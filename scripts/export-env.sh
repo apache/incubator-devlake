af1611ff10cac8b67ec4ada08761296adb7b67d8
Jonathan O'Donnell <joncodo@meri.co>
2021-09-10 14:46:52 -0300

    fix: export env remove all comments

--- scripts/export-env.sh
+++ scripts/export-env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 FILE=.env
 if [ -f "$FILE" ]; then
-    export $(grep -v '^#' .env | xargs)
+    export $(grep -v '^#' .env | sed 's/#.*$//g' | xargs)
 else 
     echo "$FILE does not exist."
 fi
