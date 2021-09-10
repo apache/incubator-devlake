a197de97326aa13b1e4a10f8d6345fbb18ae9b64
Justin <justin.brazeau@merico.dev>
2021-09-10 15:34:17 -0400

    fix: remove vendor css links

--- config-ui/pages/_app.js
+++ config-ui/pages/_app.js
@@ -1,7 +1,5 @@
 import 'normalize.css'
 import '@blueprintjs/core/lib/css/blueprint.css'
-// import '../styles/vendor/normalize.css'
-// import '../styles/vendor/blueprint.css'
 import '../styles/globals.css'
 
 
