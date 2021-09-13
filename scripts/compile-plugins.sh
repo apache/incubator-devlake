cccdeff8bbf3f1a01396433b0377ac595d794c89
Jingyang Liang <jingyang@merico.dev>
2021-09-13 20:24:58 +0800

    refactor: compile each plugin to its own output dir

--- scripts/compile-plugins.sh
+++ scripts/compile-plugins.sh
@@ -7,6 +7,6 @@ PLUGIN_OUTPUT_DIR=$SCRIPT_DIR/../bin/plugins
 
 for PLUG in $(find $PLUGIN_SRC_DIR/* -maxdepth 0 -type d -not -name core -not -empty); do
   NAME=$(basename $PLUG)
-  echo "Building plugin $NAME to bin/plugins/$NAME.so"
-  go build -buildmode=plugin "$@" -o $PLUGIN_OUTPUT_DIR/$NAME.so $PLUG/*.go
+  echo "Building plugin $NAME to bin/plugins/$NAME/$NAME.so"
+  go build -buildmode=plugin "$@" -o $PLUGIN_OUTPUT_DIR/$NAME/$NAME.so $PLUG/*.go
 done
