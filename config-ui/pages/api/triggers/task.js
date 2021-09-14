0e37ac50fd4cfdd2cea1de79ca9efc2ffb7d6e07
Klesh Wong <zhenmian.huang@merico.dev>
2021-09-14 16:00:40 +0800

    fix: api response stalling / react key warning

--- config-ui/pages/api/triggers/task.js
+++ config-ui/pages/api/triggers/task.js
@@ -1,11 +1,7 @@
 import axios from 'axios'
 import { DEVLAKE_ENDPOINT } from './config'
 
-export default function handler(req, res) {
-
-  axios.post(`${DEVLAKE_ENDPOINT}/task`, req.body ).then(res => {
-    console.log(res.data)
-  }).catch(e => {
-    console.log(e)
-  })
+export default async function handler(req, res) {
+  const r = await axios.post(`${DEVLAKE_ENDPOINT}/task`, req.body)
+  res.json(r.data)
 }
