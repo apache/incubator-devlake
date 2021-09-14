2f3fa6e7ff771652af750f901072e590cebcf1cf
Klesh Wong <zhenmian.huang@merico.dev>
2021-09-14 15:19:58 +0800

    fix: envvar typo

--- config-ui/pages/api/triggers/config.js
+++ config-ui/pages/api/triggers/config.js
@@ -1,2 +1,2 @@
-export const DEVLAKE_ENDPOINT = process.env.DEVLAKENDPOINT || 'http://localhost:8080'
+export const DEVLAKE_ENDPOINT = process.env.DEVLAKE_ENDPOINT || 'http://localhost:8080'
 export const GRAFANA_PORT = process.env.GRAFANA_PORT || '3002'
