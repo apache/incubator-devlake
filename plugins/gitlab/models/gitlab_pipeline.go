72b521fa7ec218362c3a936b05522b448ed2371e
Jonathan O'Donnell <joncodo@meri.co>
2021-09-17 12:43:03 -0300

    feat: trying to get the pipelines one by one

--- plugins/gitlab/models/gitlab_pipeline.go
+++ plugins/gitlab/models/gitlab_pipeline.go
@@ -4,8 +4,15 @@ import "github.com/merico-dev/lake/models"
 
 type GitlabPipeline struct {
 	GitlabId        int `gorm:"primaryKey"`
-	ProjectId       int    `gorm:"index"`
+	ProjectId       int `gorm:"index"`
 	GitlabCreatedAt string
 	Status          string
+	Ref             string
+	Sha             string
+	WebUrl          string
+	Duration        int
+	StartedAt       string
+	FinishedAt      string
+	Coverage        string
 	models.NoPKModel
 }
--- plugins/gitlab/tasks/gitlab_pipelinie_collector.go
+++ plugins/gitlab/tasks/gitlab_pipelinie_collector.go
@@ -7,7 +7,8 @@ import (
 	"github.com/merico-dev/lake/logger"
 	lakeModels "github.com/merico-dev/lake/models"
 	"github.com/merico-dev/lake/plugins/core"
-	"github.com/merico-dev/lake/plugins/gitlab/models"
+	gitlabModels "github.com/merico-dev/lake/plugins/gitlab/models"
+	"github.com/merico-dev/lake/utils"
 	"gorm.io/gorm/clause"
 )
 
@@ -15,13 +16,30 @@ type ApiPipelineResponse []struct {
 	GitlabId        int    `json:"id"`
 	ProjectId       int    `json:"project_id"`
 	GitlabCreatedAt string `json:"created_at"`
+	Ref             string
+	Sha             string
+	WebUrl          string `json:"web_url"`
 	Status          string
 }
 
-func CollectPipelines(projectId int) error {
+type ApiSinglePipelineResponse struct {
+	GitlabId        int    `json:"id"`
+	ProjectId       int    `json:"project_id"`
+	GitlabCreatedAt string `json:"created_at"`
+	Ref             string
+	Sha             string
+	WebUrl          string `json:"web_url"`
+	Duration        int
+	StartedAt       string `json:"started_at"`
+	FinishedAt      string `json:"finished_at"`
+	Coverage        string
+	Status          string
+}
+
+func CollectAllPipelines(projectId int) error {
 	gitlabApiClient := CreateApiClient()
 
-	return gitlabApiClient.FetchWithPaginationAnts(fmt.Sprintf("projects/%v/pipelines", projectId), 100,
+	return gitlabApiClient.FetchWithPaginationAnts(fmt.Sprintf("projects/%v/pipelines?order_by=updated_at&sort=desc", projectId), 100,
 		func(res *http.Response) error {
 
 			apiPipelineResponse := &ApiPipelineResponse{}
@@ -33,10 +51,13 @@ func CollectPipelines(projectId int) error {
 			}
 
 			for _, value := range *apiPipelineResponse {
-				gitlabPipeline := &models.GitlabPipeline{
+				gitlabPipeline := &gitlabModels.GitlabPipeline{
 					GitlabId:        value.GitlabId,
 					ProjectId:       value.ProjectId,
 					GitlabCreatedAt: value.GitlabCreatedAt,
+					Ref:             value.Ref,
+					Sha:             value.Sha,
+					WebUrl:          value.WebUrl,
 					Status:          value.Status,
 				}
 
@@ -51,4 +72,75 @@ func CollectPipelines(projectId int) error {
 
 			return nil
 		})
+}
+
+func CollectChildrenOnPipelines(projectIdInt int) error {
+	var pipelines []gitlabModels.GitlabPipeline
+	lakeModels.Db.Find(&pipelines)
+
+	// Gilab's authenticated api rate limit is 2000 per min
+	// 15 tasks/s* ~2 requests/task * 60s/min = 1800 per min < 2000 per min
+	scheduler, err := utils.NewWorkerScheduler(50, 15)
+	if err != nil {
+		return err
+	}
+
+	defer scheduler.Release()
+
+	// for i := 0; i < len(pipelines); i++ {
+	for i := 0; i < 10; i++ {
+		pipeline := (pipelines)[i]
+		logger.Info("JON >>> pipeline", pipeline)
+		schedulerErr := scheduler.Submit(func() error {
+			getUrl := fmt.Sprintf("projects/%v/pipelines/%v", projectIdInt, pipeline.GitlabId)
+			logger.Info("JON >>> getUrl", getUrl)
+			res, err := gitlabApiClient.Get(getUrl, nil, nil)
+
+			if err != nil {
+				logger.Error("Error: ", err)
+				return nil
+			}
+
+			pipelineRes := &ApiSinglePipelineResponse{}
+			err2 := core.UnmarshalResponse(res, pipelineRes)
+
+			if err2 != nil {
+				logger.Error("Error: ", err2)
+				return nil
+			}
+
+			logger.Info("JON >>> pipelineRes", pipelineRes)
+			gitlabPipeline := &gitlabModels.GitlabPipeline{
+				GitlabId:        pipelineRes.GitlabId,
+				ProjectId:       pipelineRes.ProjectId,
+				GitlabCreatedAt: pipelineRes.GitlabCreatedAt,
+				Ref:             pipelineRes.Ref,
+				Sha:             pipelineRes.Sha,
+				WebUrl:          pipelineRes.WebUrl,
+				Duration:        pipelineRes.Duration,
+				StartedAt:       pipelineRes.StartedAt,
+				FinishedAt:      pipelineRes.FinishedAt,
+				Coverage:        pipelineRes.Coverage,
+				Status:          pipelineRes.Status,
+			}
+
+			err = lakeModels.Db.Clauses(clause.OnConflict{
+				UpdateAll: true,
+			}).Create(&gitlabPipeline).Error
+
+			if err != nil {
+				logger.Error("Could not upsert: ", err)
+			}
+			return nil
+		})
+
+		if schedulerErr != nil {
+			logger.Error("Error: ", schedulerErr)
+			return nil
+		}
+
+	}
+	logger.Info("JON >>> wait", true)
+	scheduler.WaitUntilFinish()
+	return nil
 }
