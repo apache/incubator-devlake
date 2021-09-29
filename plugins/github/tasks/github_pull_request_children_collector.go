d492799283232e853fad0cae3e9af2dabf6a037e
kevin-kline <21stcenturykline@gmail.com>
2021-09-29 10:56:14 -0300

    feat: issue comment / event collect w/sched fix on pr collect

--- plugins/github/tasks/github_pull_request_children_collector.go
+++ plugins/github/tasks/github_pull_request_children_collector.go
@@ -7,43 +7,34 @@ import (
 	"github.com/merico-dev/lake/utils"
 )
 
-func CollectChildrenOnPullRequests(owner string, repositoryName string, repositoryId int, scheduler *utils.WorkerScheduler) {
+func CollectChildrenOnPullRequests(owner string, repositoryName string, repositoryId int, scheduler *utils.WorkerScheduler) error {
 	var prs []models.GithubPullRequest
 	lakeModels.Db.Find(&prs)
-
 	for i := 0; i < len(prs); i++ {
 		pr := (prs)[i]
-		err := scheduler.Submit(func() error {
-			reviewErr := CollectPullRequestReviews(owner, repositoryName, repositoryId, &pr, scheduler)
-			if reviewErr != nil {
-				logger.Error("Could not collect PR reviews", reviewErr)
-				return reviewErr
-			}
-			commentsErr := CollectPullRequestComments(owner, repositoryName, &pr, scheduler)
-			if commentsErr != nil {
-				logger.Error("Could not collect PR Comments", commentsErr)
-				return commentsErr
-			}
-			commitsErr := CollectPullRequestCommits(owner, repositoryName, &pr, scheduler)
-			if commitsErr != nil {
-				logger.Error("Could not collect PR Comments", commitsErr)
-				return commitsErr
-			}
-
-			// This call is to update the details of the individual pull request with additions / deletions / etc.
-			// prErr := CollectPullRequest(owner, repositoryName, repositoryId, &pr)
-			// if prErr != nil {
-			// 	logger.Error("Could not collect PRs to update details", reviewErr)
-			// 	return reviewErr
-			// }
-
-			return nil
-		})
-		if err != nil {
-			logger.Error("INFO >>> Scheduler error: ", err)
-			return
+		reviewErr := CollectPullRequestReviews(owner, repositoryName, repositoryId, &pr, scheduler)
+		if reviewErr != nil {
+			logger.Error("Could not collect PR reviews", reviewErr)
+			return reviewErr
 		}
-	}
+		commentsErr := CollectPullRequestComments(owner, repositoryName, &pr, scheduler)
+		if commentsErr != nil {
+			logger.Error("Could not collect PR Comments", commentsErr)
+			return commentsErr
+		}
+		commitsErr := CollectPullRequestCommits(owner, repositoryName, &pr, scheduler)
+		if commitsErr != nil {
+			logger.Error("Could not collect PR Comments", commitsErr)
+			return commitsErr
+		}
 
-	scheduler.WaitUntilFinish()
+		// This call is to update the details of the individual pull request with additions / deletions / etc.
+		// prErr := CollectPullRequest(owner, repositoryName, repositoryId, &pr)
+		// if prErr != nil {
+		// 	logger.Error("Could not collect PRs to update details", reviewErr)
+		// 	return reviewErr
+		// }
+
+	}
+	return nil
 }
