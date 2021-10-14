82510ec719ee0269292db190ca802f3c6ea4c35d
Justin <justin.brazeau@merico.dev>
2021-10-14 12:02:53 -0400

    fix: add routes pkg and fix to prod builds

---
+++ config-ui/server.js
@@ -0,0 +1,9 @@
+const next = require('next')
+const routes = require('./routes')
+const app = next({dev: process.env.NODE_ENV !== 'production'})
+const handler = routes.getRequestHandler(app)
+
+const express = require('express')
+app.prepare().then(() => {
+  express().use(handler).listen(4000)
+})
