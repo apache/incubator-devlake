591c97c95b2afe89d54d3265bb42256777e2274c
Rankin Zheng <runjin@merico.dev>
2021-10-25 20:05:29 +0800

    Query epic lead time

--- sql/lake.sql
+++ sql/lake.sql
@@ -1,6 +1,16 @@
 /* count of issue type  */
 WITH
     jira_types as (SELECT DISTINCT `type` FROM jira_issues)
-SELECT t.type,(SELECT COUNT(*) FROM jira_issues WHERE `type`=t.type) FROM jira_types t
+SELECT t.type,(SELECT COUNT(*) FROM jira_issues WHERE `type`=t.type) FROM jira_types t;
 
 /* Lead-time of Epic */
+SELECT 
+  i.key AS 'Jira Key',
+  i.summary AS '项目概述',
+  i.std_status AS '项目状态',
+  i.lead_time DIV 1440 AS '需求交付周期',
+  i.changelog_updated AS '最后更新时间'
+FROM jira_issues i
+WHERE 
+  i.type = 'Epic'
+ORDER BY i.updated_at DESC
