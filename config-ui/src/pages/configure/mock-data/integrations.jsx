d3e4b05071d11fee3e1c11aae2bace7fed481f64
Julien Chinapen <julien.chinapen@merico.dev>
2021-10-26 12:48:40 -0400

    fix: extract provider connection form

---
+++ config-ui/src/pages/configure/mock-data/integrations.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { ReactComponent as GitlabProvider } from '@/images/integrations/gitlab.svg'
+import { ReactComponent as JenkinsProvider } from '@/images/integrations/jenkins.svg'
+import { ReactComponent as JiraProvider } from '@/images/integrations/jira.svg'
+
+const integrationsData = [
+  {
+    id: 'gitlab',
+    name: 'Gitlab',
+    icon: <GitlabProvider className='providerIconSvg' width='30' height='30' style={{ float: 'left', marginTop: '5px' }} />
+  },
+  {
+    id: 'jenkins',
+    name: 'Jenkins',
+    icon: <JenkinsProvider className='providerIconSvg' width='30' height='30' style={{ float: 'left', marginTop: '5px' }} />
+  },
+  {
+    id: 'jira',
+    name: 'JIRA',
+    icon: <JiraProvider className='providerIconSvg' width='30' height='30' style={{ float: 'left', marginTop: '5px' }} />
+  },
+]
+
+export {
+  integrationsData
+}
