1c2f28b24d82a8ecae5d656030881e0cac0e092a
Julien Chinapen <julien.chinapen@merico.dev>
2021-11-01 12:00:18 -0400

    fix: extract mock data for boards and projects

---
+++ config-ui/src/pages/configure/mock-data/boards.jsx
@@ -0,0 +1,12 @@
+const boardsData = [
+  { id: 1, title: 'Open', value: 1 },
+  { id: 2, title: 'Backlog', value: 2 },
+  { id: 3, title: 'QA / Testing', value: 3 },
+  { id: 4, title: 'Pre-release', value: 4 },
+  { id: 5, title: 'Production', value: 5 },
+  { id: 6, title: 'Closed', value: 6 }
+]
+
+export {
+  boardsData
+}
