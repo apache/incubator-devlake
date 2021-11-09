c384c5049dcde16eaf141b6ca1d28171d838f46d
kevin-kline <21stcenturykline@gmail.com>
2021-11-09 13:53:18 -0400

    feat: discord link in config-ui top right

--- config-ui/src/components/Nav.jsx
+++ config-ui/src/components/Nav.jsx
@@ -6,6 +6,7 @@ import {
 } from '@blueprintjs/core'
 import '../styles/nav.scss'
 
+const DISCORD_IMG_URL = './src/images/icon_clyde_black_RGB.svg'
 const Nav = () => {
   return (
     <Navbar className='navbar'>
@@ -16,6 +17,10 @@ const Nav = () => {
         <Navbar.Divider />
         <a href='mailto:hello@merico.dev' rel='noreferrer' target='_blank' className='navIconLink'>
           <Icon icon='envelope' size={16} />
+        </a>
+        <Navbar.Divider />
+        <a href='https://discord.com/invite/83rDG6ydVZ' rel='noreferrer' target='_blank' className='navIconLink'>
+          <img className='discordIcon' src={DISCORD_IMG_URL}/>
         </a>
       </Navbar.Group>
     </Navbar>
