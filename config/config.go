4673df34a9f0159bae91c28803b7d840a9b55e82
Klesh Wong <zhenmian.huang@merico.dev>
2021-11-13 14:49:11 +0800

    feat: config-ui supports set GITHUB_PROXY

--- config/config.go
+++ config/config.go
@@ -1,9 +1,6 @@
 package config
 
 import (
-	"os"
-	"path"
-
 	"github.com/spf13/viper"
 )
 
@@ -35,35 +32,7 @@ var V *viper.Viper
 
 func LoadConfigFile() *viper.Viper {
 	V = viper.New()
-	configFile := os.Getenv("ENV_FILE")
-	if configFile != "" {
-		if !path.IsAbs(configFile) {
-			panic("Please set ENV_FILE with absolute path. " +
-				"Currently it should only be used for go test to load ENVs.")
-		}
-		V.SetConfigFile(configFile)
-		V.Set("WORKING_DIRECTORY", path.Dir(configFile))
-	} else {
-		V.SetConfigName(".env")
-		V.SetConfigType("env")
-
-		V.AddConfigPath(".")
-		// For testing in subdirectories 1 level down (ex. ./config)
-		V.AddConfigPath("../")
-		V.AddConfigPath("conf")
-		V.AddConfigPath("etc")
-
-		execPath, execErr := os.Executable()
-		if execErr == nil {
-			V.AddConfigPath(path.Dir(execPath))
-			V.AddConfigPath(path.Join(path.Dir(execPath), "conf"))
-			V.AddConfigPath(path.Join(path.Dir(execPath), "etc"))
-		}
-
-		wdPath, _ := os.Getwd()
-		V.Set("WORKING_DIRECTORY", wdPath)
-	}
-
+	V.SetConfigFile(".env")
 	_ = V.ReadInConfig()
 	V.AutomaticEnv()
 	return V
