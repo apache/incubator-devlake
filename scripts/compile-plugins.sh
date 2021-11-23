375f70b6ccb03a31cc07b2be52a9036eae332ec8
Jonathan O'Donnell <joncodo@meri.co>
2021-11-23 11:14:39 -0400

    feat: only compile the plugins you want

--- scripts/compile-plugins.sh
+++ scripts/compile-plugins.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Change this if you only want to complile the plugin you are working on
+PLUGIN_TO_COMPILE=all # this must be all or the name of the plugin
 
 set -e
 
@@ -6,9 +8,16 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 PLUGIN_SRC_DIR=$SCRIPT_DIR/../plugins
 PLUGIN_OUTPUT_DIR=$SCRIPT_DIR/../bin/plugins
 
-
 for PLUG in $(find $PLUGIN_SRC_DIR/* -maxdepth 0 -type d -not -name core -not -empty); do
   NAME=$(basename $PLUG)
-  echo "Building plugin $NAME to bin/plugins/$NAME/$NAME.so"
-  go build -buildmode=plugin "$@" -o $PLUGIN_OUTPUT_DIR/$NAME/$NAME.so $PLUG/*.go
+
+  if [ "$NAME" == "$PLUGIN_TO_COMPILE" ]; then
+      echo "Building plugin $NAME to bin/plugins/$NAME/$NAME.so"
+      go build -buildmode=plugin "$@" -o $PLUGIN_OUTPUT_DIR/$NAME/$NAME.so $PLUG/*.go
+  fi
+
+  if [ "$PLUGIN_TO_COMPILE" == "all" ]; then
+      echo "Building plugin $NAME to bin/plugins/$NAME/$NAME.so"
+      go build -buildmode=plugin "$@" -o $PLUGIN_OUTPUT_DIR/$NAME/$NAME.so $PLUG/*.go
+  fi
 done
