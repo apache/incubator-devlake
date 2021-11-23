e4adf52bfed7ac560e9fc280217ff3d19340ce4c
kevin-kline <21stcenturykline@gmail.com>
2021-11-23 11:56:14 -0400

    feat: dynamic JSON for triggers page

---
+++ config-ui/test/json.js
@@ -0,0 +1,28 @@
+var assert = require("assert");
+const AVAILABLE_PLUGINS = require("../src/data/availablePlugins");
+const TEST_DATA = require('./testData')
+const { getCollectorJson, getCollectionJson, getDomainJson } = require("../src/utils/triggersUtil");
+
+describe("Json utils", () => {
+  describe("getCollectionJson", function () {
+    it("gets default JSON for plugins based on an array of names", function () {
+      let expected = TEST_DATA.completeTriggersJson
+      let actual = getCollectionJson(AVAILABLE_PLUGINS);
+      assert.deepEqual(expected, actual);
+    });
+  });
+  describe("getCollectorJson", function () {
+    it("gets default JSON for a collector plugin based on the name", function () {
+      const expected = TEST_DATA.gitlabTriggersJson
+      let actual = getCollectorJson("gitlab");
+      assert.deepEqual(expected, actual);
+    });
+  });
+  describe("getDomainJson", function () {
+    it("gets default JSON for a domain conversion plugin based on the name", function () {
+      const expected = TEST_DATA.gitlabDomainTriggersJson
+      let actual = getDomainJson("gitlab");
+      assert.deepEqual(expected, actual);
+    });
+  });
+});
