a160bef29bb784b1c51b417032b5213e77fcdb41
Jonathan O'Donnell <joncodo@meri.co>
2021-11-24 14:00:22 -0400

    feat: ae-plugin working on testing projects and commit collection

--- plugins/merico-analysis-engine/init.go
+++ plugins/merico-analysis-engine/init.go
@@ -3,20 +3,14 @@ package main
 import (
 	"github.com/merico-dev/lake/logger"
 	lakeModels "github.com/merico-dev/lake/models"
-	"github.com/merico-dev/lake/plugins/ae/models"
+	"github.com/merico-dev/lake/plugins/merico-analysis-engine/models"
 )
 
 func (plugin AE) Init() {
 	logger.Info("INFO >>> init go plugin", true)
 	err := lakeModels.Db.AutoMigrate(
 		&models.AEProject{},
-		&models.AEMergeRequest{},
-		&models.AECommit{},
-		&models.AEPipeline{},
-		&models.AEReviewer{},
-		&models.AEMergeRequestNote{},
-		&models.AEMergeRequestCommit{},
-		&models.AEMergeRequestCommitMergeRequest{})
+		&models.AECommit{})
 	if err != nil {
 		logger.Error("Error migrating ae: ", err)
 		panic(err)
