41d135ebc8b600fed99a089619d55861107135d2
kevin-kline <kevin.kline@merico.dev>
2021-11-25 10:48:36 -0400

    feat: jira /test route

--- plugins/jira/api/jira_connections.go
+++ plugins/jira/api/jira_connections.go
@@ -1,13 +1,33 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/merico-dev/lake/plugins/core"
+	"github.com/merico-dev/lake/plugins/jira/tasks"
 )
 
-func TestConnection(input *core.ApiResourceInput) (*core.ApiResourceOutput, error) {
-	// Get the source ID from the input
-	// Get the source from the DB
-	// Use endpoint and auth to make a request to JIRA
+type ApiMyselfResponse struct {
+	AccountId   string
+	DisplayName string
+}
 
+func TestConnection(input *core.ApiResourceInput) (*core.ApiResourceOutput, error) {
+	jiraSource, err := findSourceByInputQuery(input)
+	if err != nil {
+		return nil, err
+	}
+	jiraApiClient := tasks.NewJiraApiClient(jiraSource.Endpoint, jiraSource.BasicAuthEncoded)
+
+	res, err := jiraApiClient.Get("api/3/myself", nil, nil)
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("Your connection configuration is invalid for this source: %v", jiraSource.Name)
+	}
+	myselfFromApi := &ApiMyselfResponse{}
+
+	err = core.UnmarshalResponse(res, myselfFromApi)
+	if err != nil {
+		return nil, err
+	}
 	return &core.ApiResourceOutput{Body: true}, nil
 }
--- plugins/jira/api/jira_sources.go
+++ plugins/jira/api/jira_sources.go
@@ -21,14 +21,28 @@ func findSourceByInputParam(input *core.ApiResourceInput) (*models.JiraSource, e
 	if err != nil {
 		return nil, fmt.Errorf("invalid sourceId")
 	}
+	return getJiraSourceById(jiraSourceId)
+}
+func findSourceByInputQuery(input *core.ApiResourceInput) (*models.JiraSource, error) {
+	sourceId := input.Query["sourceId"][0]
+	if sourceId == "" {
+		return nil, fmt.Errorf("missing sourceid")
+	}
+	jiraSourceId, err := strconv.ParseUint(sourceId, 10, 64)
+	if err != nil {
+		return nil, fmt.Errorf("invalid sourceId")
+	}
+	return getJiraSourceById(jiraSourceId)
+}
+
+func getJiraSourceById(id uint64) (*models.JiraSource, error) {
 	jiraSource := &models.JiraSource{}
-	err = lakeModels.Db.First(jiraSource, jiraSourceId).Error
+	err := lakeModels.Db.First(jiraSource, id).Error
 	if err != nil {
 		return nil, err
 	}
 	return jiraSource, nil
 }
-
 func mergeFieldsToJiraSource(jiraSource *models.JiraSource, sources ...map[string]interface{}) error {
 	// decode
 	for _, source := range sources {
