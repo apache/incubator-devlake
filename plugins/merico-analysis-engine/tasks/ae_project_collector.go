9ed47b296157fe2dff5384a4effab21668960695
Jonathan O'Donnell <joncodo@meri.co>
2021-11-25 22:28:34 -0400

    docs: update readme from jingyangs notes on ae-plugin auth

--- plugins/merico-analysis-engine/tasks/ae_project_collector.go
+++ plugins/merico-analysis-engine/tasks/ae_project_collector.go
@@ -1,10 +1,14 @@
 package tasks
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"net/http"
+	"net/url"
 	"time"
 
+	"github.com/merico-dev/lake/config"
 	"github.com/merico-dev/lake/logger"
 	lakeModels "github.com/merico-dev/lake/models"
 	"github.com/merico-dev/lake/plugins/core"
@@ -20,9 +24,27 @@ type ApiProjectResponse struct {
 	AEUpdateTime *time.Time `json:"update_time"`
 }
 
+// This function is required by AE to prevent Man-in-the-middle attacks
+// You need to fully encode all query parameters and other things in order to get a
+// correct sign value in the url
+func getMD5Hash(text string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func setQueryParams() *url.Values {
+	queryParams := &url.Values{}
+	queryParams.Set("app_id", config.V.GetString("AE_APP_ID"))
+	queryParams.Set("nonce_str", config.V.GetString("AE_NONCE_STR"))
+	queryParams.Set("sign", getMD5Hash(config.V.GetString("AE_SECRET_KEY")))
+	return queryParams
+}
+
 func CollectProject(projectId int) error {
 	aeApiClient := CreateApiClient()
-	res, err := aeApiClient.Get(fmt.Sprintf("projects/%v", projectId), nil, nil)
+
+	res, err := aeApiClient.Get(fmt.Sprintf("/projects/%v", projectId), setQueryParams(), nil)
 	if err != nil {
 		logger.Error("Error: ", err)
 		return err
