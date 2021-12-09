f314c7c8e13674f9ff85dc5706e2851df2d8c4aa
kevin-kline <kevin.kline@merico.dev>
2021-12-09 14:02:46 -0400

    fix: improved naming

--- api/push/push_util.go
+++ api/push/push_util.go
@@ -4,8 +4,8 @@ import (
 	"github.com/merico-dev/lake/models"
 )
 
-func InsertThing(tableName string, thingToInsert map[string]interface{}) (int64, error) {
-	tx := models.Db.Table(tableName).Create(thingToInsert)
+func InsertRow(tableName string, rowToInsert map[string]interface{}) (int64, error) {
+	tx := models.Db.Table(tableName).Create(rowToInsert)
 	if tx.Error != nil {
 		return 0, tx.Error
 	}
