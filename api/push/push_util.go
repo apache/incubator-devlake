bb114f0e2da6b5257be1303c7f89ead5c6a6bdd0
kevin-kline <kevin.kline@merico.dev>
2021-12-09 13:54:01 -0400

    feat: insert map directly, no raw SQL

--- api/push/push_util.go
+++ api/push/push_util.go
@@ -1,33 +1,13 @@
 package push
 
 import (
-	"fmt"
-
 	"github.com/merico-dev/lake/models"
 )
 
 func InsertThing(tableName string, thingToInsert map[string]interface{}) (int64, error) {
-	keys, values := GetKeysAndValues(thingToInsert)
-
-	rawSql := fmt.Sprintf("INSERT INTO %v (%v) VALUES(%v);", tableName, keys, values)
-
-	tx := models.Db.Exec(rawSql)
+	tx := models.Db.Table(tableName).Create(thingToInsert)
 	if tx.Error != nil {
 		return 0, tx.Error
 	}
 	return tx.RowsAffected, nil
-}
-
-func GetKeysAndValues(thingToInsert map[string]interface{}) (keys string, values string) {
-	count := 1
-	endString := ", "
-	for key, value := range thingToInsert {
-		if count == len(thingToInsert) {
-			endString = ""
-		}
-		keys += fmt.Sprintf("%v%v", key, endString)
-		values += fmt.Sprintf("\"%v\"%v", value, endString)
-		count++
-	}
-	return keys, values
 }
