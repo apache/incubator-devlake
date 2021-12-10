ce8c38370f0ce5e691867a5ff3f9f2b06744ce6e
Jonathan O'Donnell <joncodo@meri.co>
2021-12-10 10:24:08 -0400

    feat: refactor for grafana url in front end

--- config-ui/src/utils/config.js
+++ config-ui/src/utils/config.js
@@ -2,3 +2,4 @@ export const DEVLAKE_ENDPOINT = '/api'
 export const GRAFANA_BASE_URL = '/grafana'
 export const LOCAL_BASE_URL = 'http://localhost:3002'
 export const GRAFANA_ENDPOINT = '/d/0Rjxknc7z/demo-homepage?orgId=1'
+export const GRAFANA_URL = process.env.LOCAL ? LOCAL_BASE_URL + GRAFANA_ENDPOINT : GRAFANA_BASE_URL + GRAFANA_ENDPOINT
