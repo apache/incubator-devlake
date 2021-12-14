25ef5101740de9e61a8f3f167ac2c3fb5249d42b
linyh <yanghui@meri.co>
2021-12-14 23:31:21 +0800

    fix: update feishu plugin for new plugin interface

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 require (
 	github.com/bndr/gojenkins v1.1.0
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.4
 	github.com/go-playground/validator/v10 v10.9.0 // indirect
 	github.com/joho/godotenv v1.3.0
@@ -33,7 +34,6 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/gin-contrib/cors v1.3.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
--- plugins/feishu/init.go
+++ plugins/feishu/init.go
@@ -1,29 +1,18 @@
 package main
 
 import (
-	"github.com/merico-dev/lake/config"
+	"github.com/merico-dev/lake/logger"
+	lakeModels "github.com/merico-dev/lake/models"
 	"github.com/merico-dev/lake/plugins/feishu/models"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-	"gorm.io/gorm/schema"
 )
 
-var db *gorm.DB
-
 func (plugin Feishu) Init() {
-	var connectionString = config.V.GetString("DB_URL")
-	var err error
-	db, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{
-		NamingStrategy: schema.NamingStrategy{
-			TablePrefix: "feishu_plugin_",
-		},
-	})
+	logger.Info("INFO >>> init Feishu plugin", true)
+	err := lakeModels.Db.AutoMigrate(
+		&models.FeishuMeetingTopUserItem{},
+	)
 	if err != nil {
-		panic(err)
-	}
-
-	err = db.AutoMigrate(&models.MeetingTopUserItem{})
-	if err != nil {
+		logger.Error("Error migrating feishu: ", err)
 		panic(err)
 	}
 }
