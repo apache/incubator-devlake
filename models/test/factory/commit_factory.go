49cffc03b1fa74fc1be5cad64d36859f67e5ec3b
kevin-kline <kevin.kline@merico.dev>
2021-12-15 16:32:35 -0500

    feat: insert tests for some domain level DB models - WIP

---
+++ models/test/factory/commit_factory.go
@@ -0,0 +1,29 @@
+package factory
+
+import (
+	"time"
+
+	"github.com/merico-dev/lake/models/domainlayer"
+	"github.com/merico-dev/lake/models/domainlayer/code"
+)
+
+func CreateCommit(repoId uint64) (*code.Commit, error) {
+	commit := &code.Commit{
+		DomainEntity: domainlayer.DomainEntity{
+			OriginKey: "something",
+		},
+		RepoId:         repoId,
+		Sha:            "dosifj9302hf80h23f",
+		Additions:      33,
+		Deletions:      33,
+		DevEq:          33,
+		Message:        "",
+		AuthorName:     "",
+		AuthorEmail:    "",
+		AuthoredDate:   time.Now(),
+		CommitterName:  "",
+		CommitterEmail: "",
+		CommittedDate:  time.Now(),
+	}
+	return commit, nil
+}
