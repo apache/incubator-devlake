7a7f114182b75c5cb328e1096bd1d314116ec42f
Julien Chinapen <julien.chinapen@merico.dev>
2021-12-17 21:55:49 -0500

    fix: cleanup unused imports and comments

--- config-ui/src/pages/pipelines/create.jsx
+++ config-ui/src/pages/pipelines/create.jsx
@@ -1,12 +1,16 @@
 import React, { Fragment, useEffect, useCallback, useState, useRef } from 'react'
 import { CSSTransition } from 'react-transition-group'
-import { useHistory, useParams, Link } from 'react-router-dom'
+import {
+  useHistory,
+  Link,
+  // useParams,
+} from 'react-router-dom'
 import { GRAFANA_URL } from '@/utils/config'
 // import { ToastNotification } from '@/components/Toast'
 // import { DEVLAKE_ENDPOINT } from '@/utils/config'
 // import request from '@/utils/request'
 import {
-  Classes,
+  // Classes,
   Button, Icon, Intent, Switch,
   // H2, Card, Elevation, Tag,
   Menu,
@@ -16,7 +20,7 @@ import {
   Popover,
   Tooltip,
   Position,
-  Spinner,
+  // Spinner,
   Colors,
   // Alignment
 } from '@blueprintjs/core'
@@ -60,9 +64,9 @@ const CreatePipeline = (props) => {
     `${today.getFullYear()}${today.getMonth() + 1}${today.getDate()}${today.getMinutes()}${today.getSeconds()}`, // 202112154936
     today.toUTCString(), // Thu, 16 Dec 2021 04:49:52 GMT
   ]
-  const [autoRun, setAutoRun] = useState(false)
-  const [enableThrottling, setEnableThrottling] = useState(false)
-  const [ready, setReady] = useState([])
+  // const [autoRun, setAutoRun] = useState(false)
+  // const [enableThrottling, setEnableThrottling] = useState(false)
+  const [readyProviders, setReadyProviders] = useState([])
   // const [isRunning, setIsRunning] = useState(false)
 
   const [enabledProviders, setEnabledProviders] = useState([])
@@ -112,14 +116,14 @@ const CreatePipeline = (props) => {
   useEffect(() => {
     integrationsData.forEach((i, idx) => {
       setTimeout(() => {
-        setReady(r => [...r, i.id])
+        setReadyProviders(r => [...r, i.id])
       }, idx * 50)
     })
   }, [])
 
-  useEffect(() => {
-    console.log('>> READY LIST = ', ready)
-  }, [ready])
+  // useEffect(() => {
+  //   console.log('>> READY LIST = ', readyProviders)
+  // }, [readyProviders])
 
   const isProviderEnabled = (providerId) => {
     return enabledProviders.includes(providerId)
@@ -309,9 +313,7 @@ const CreatePipeline = (props) => {
   }
 
   useEffect(() => {
-    // setValidationErrors(errors => (!pipelineName || pipelineName.length < 2) && !errors.some(e => e.startsWith('Name:'))
-    //   ? [...errors, 'Name: Enter a valid Pipeline Name']
-    //   : errors.filter(e => !e.startsWith('Name:')))
+
   }, [pipelineName])
 
   useEffect(() => {
@@ -331,15 +333,7 @@ const CreatePipeline = (props) => {
     setRunTasks(PipelineTasks)
     console.log('>> CONFIGURED PIPELINE TASKS = ', PipelineTasks)
     validate()
-    // setValidationErrors(errors => enabledProviders.includes(Providers.GITLAB) && !projectId ? ['GitLab: Specify Run Settings'] : errors.filter(e => !e.startsWith('GitLab:')))
-    // setValidationErrors(errors => enabledProviders.includes(Providers.JIRA) && (!boardId || !sourceId) ? ['JIRA: Specify Run Settings'] : errors.filter(e => !e.startsWith('JIRA:')))
-    // setValidationErrors(errors => enabledProviders.includes(Providers.GITHUB) && (!repositoryName || !owner) ? ['GitHub: Specify Run Settings'] : errors.filter(e => !e.startsWith('GitHub:')))
-    // setValidationErrors(errors => enabledProviders.length === 0 ? ['Pipeline: Invalid/Empty Configuration'] : errors.filter(e => !e.startsWith('Pipeline:')))
   }, [enabledProviders, projectId, boardId, sourceId, owner, repositoryName, configureProvider])
-
-  // useEffect(() => {
-  //   console.log('>> PIPELINE VALIDATION ERRORS...', validationErrors)
-  // }, [validationErrors])
 
   useEffect(() => {
     console.log('>> PIPELINE LAST RUN OBJECT CHANGED!!...', pipelineRun)
@@ -498,13 +492,15 @@ const CreatePipeline = (props) => {
               <h2 className='headline'>
                 <Icon icon='database' height={16} size={16} color='rgba(0,0,0,0.5)' /> Data Providers<span className='requiredStar'>*</span>
               </h2>
-              <p className='group-caption'>Configure available plugins to enable for this <strong>Pipeline Run</strong>.<br />Turn the switch to the ON position to activate.</p>
-
+              <p className='group-caption'>
+                Configure available plugins to enable for this <strong>Pipeline Run</strong>.<br />
+                Turn the switch to the ON position to activate.
+              </p>
               <div className='data-providers' style={{ marginTop: '8px', width: '100%' }}>
                 {integrations.map((provider) => (
                   <CSSTransition
                     key={`fx-key-provider-${provider.id}`}
-                    in={ready.includes(provider.id)}
+                    in={readyProviders.includes(provider.id)}
                     timeout={350}
                     classNames='provider-datarow'
                     unmountOnExit
@@ -643,176 +639,6 @@ const CreatePipeline = (props) => {
         </Content>
       </div>
       <PipelineIndicator pipeline={pipelineRun} graphsUrl={GRAFANA_URL} onFetch={fetchPipeline} />
-      {/* <CSSTransition
-        in={pipelineRun && pipelineRun.ID !== null}
-        timeout={300} classNames='lastrun-module'
-        unmountOnExit
-      >
-        <div
-          className='trigger-module-lastrun'
-          style={{
-            position: 'fixed',
-            borderRadius: '40px',
-            backgroundColor: '#ffffff',
-            width: '40px',
-            height: '40px',
-            right: '30px',
-            bottom: '20px',
-            zIndex: 500,
-            boxShadow: '0px 0px 6px rgba(0, 0, 0, 0.25)',
-            display: 'flex',
-            alignItems: 'center',
-            alignContent: 'center',
-            justifyContent: 'center',
-            cursor: 'pointer'
-          }}
-        >
-          <div style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: '40px',
-            height: '40px',
-            position: 'relative'
-          }}
-          >
-            <Popover
-              key='popover-lastrun-info'
-              className='popover-trigger-lastrun'
-              popoverClassName='popover-lastrun'
-              position={Position.LEFT_BOTTOM}
-              autoFocus={false}
-              enforceFocus={false}
-              usePortal={false}
-              onOpening={() => fetchPipeline(pipelineRun.ID, true)}
-              // onOpened=
-            >
-              <>
-                <Spinner
-                  value={pipelineRun.status === 'TASK_COMPLETED' ? 100 : null}
-                  className='lastrun-spinner'
-                  intent={pipelineRun.status === 'TASK_COMPLETED' ? Intent.WARNING : Intent.PRIMARY}
-                  size={40} style={{
-
-                  }}
-                />
-                {(() => {
-                  switch (pipelineRun.status){
-                  case 'TASK_COMPLETED':
-                    return <Icon
-                      icon={<PipelineCompleteIcon width={40} height={40} style={{ marginTop: '3px', display: 'flex', alignSelf: 'center' }} />}
-                      size={40}
-                    />
-                    break
-                  case 'TASK_FAILED':
-                    return <Icon
-                      icon={<PipelineFailedIcon width={40} height={40} style={{ marginTop: '3px', display: 'flex', alignSelf: 'center' }} />}
-                      size={40}
-                    />
-                    break
-                  case 'TASK_RUNNING':
-                  default:
-                    return <Icon
-                      icon={<PipelineRunningIcon width={40} height={40} style={{ marginTop: '3px', display: 'flex', alignSelf: 'center' }} />}
-                      size={40}
-                    />
-                    break
-                  }
-                })()}
-              </>
-              <>
-                <div style={{ fontSize: '12px', padding: '12px', minWidth: '420px', maxWidth: '420px', overflow: 'hidden' }}>
-                  <h3
-                    className='group-header' style={{
-                      marginTop: '0',
-                      marginBottom: '6px',
-                      textOverflow: 'ellipsis',
-                      overflow: 'hidden',
-                      whiteSpace: 'nowrap'
-                    }}
-                  >
-                    <Icon icon={pipelineRun.status === 'TASK_FAILED' ? 'warning-sign' : 'help'} size={16} style={{ marginRight: '5px' }} /> {pipelineRun.name || 'Last Pipeline Run'}
-                  </h3>
-                  <p style={{ fontSize: '11px', color: pipelineRun.status === 'TASK_FAILED' ? Colors.RED4 : Colors.DARK_GRAY4 }}>{pipelineRun.message}</p>
-                  <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
-                    <div>
-                      <label><strong>Pipeline ID</strong></label>
-                      <div style={{ fontSize: '13px', fontWeight: 800 }}>{pipelineRun.ID}</div>
-                    </div>
-                    <div style={{ padding: '0 12px' }}>
-                      <label><strong>Tasks</strong></label>
-                      <div style={{ fontSize: '13px' }}>{pipelineRun.finishedTasks}/{pipelineRun.totalTasks}</div>
-                    </div>
-                    <div>
-                      <label><strong>Status</strong></label>
-                      <div style={{ fontSize: '13px' }}>{pipelineRun.status}</div>
-                    </div>
-                    <div style={{ paddingLeft: '10px', justifyContent: 'flex-end', alignSelf: 'flex-end' }}>
-                      {pipelineRun.status === 'TASK_COMPLETED' && (
-                        <a
-                          className='bp3-button bp3-intent-primary bp3-small'
-                          href={ GRAFANA_URL }
-                          target='_blank'
-                          rel='noreferrer'
-                          style={{ backgroundColor: '#3bd477', color: '#ffffff' }}
-                        >
-                          <Icon icon='doughnut-chart' size={13} /> <span className='bp3-button-text'>Graphs</span>
-                        </a>
-                      )}
-                      {pipelineRun.status === 'TASK_RUNNING' && (
-                        <Button
-                          className='btn-cancel-pipeline'
-                          small icon='stop' text='CANCEL' intent='primary'
-                          onClick={() => cancelPipeline(pipelineRun.ID)}
-                        />
-                      )}
-                      {pipelineRun.status === 'TASK_FAILED' && (
-                        <Button
-                          className={`btn-retry-pipeline ${Classes.POPOVER_DISMISS}`}
-                          intent='danger'
-                          icon='reset' text='RETRY'
-                          style={{ color: '#ffffff' }}
-                          small
-                        />
-                      )}
-                      <Button
-                        minimal
-                        className={`btn-ok ${Classes.POPOVER_DISMISS}`}
-                        small text='OK'
-                        style={{ marginLeft: '3px' }}
-                      />
-                    </div>
-                  </div>
-                  <div style={{
-                   paddingTop: '7px',
-                   borderTop: '1px solid #f5f5f5',
-                   marginTop: '14px',
-                  }}>
-                    {pipelineRun?.tasks?.map((t, tIdx) => (
-                      <div
-                        className='pipeline-task-'
-                        key={`pipeline-task-key-${tIdx}`}
-                        style={{ display: 'flex', padding: '4px 6px', justifyContent: 'space-between'
-                      }}>
-                        <div style={{ paddingRight: '8px' }}><Spinner className='mini-task-spinner' size={14} intent={t.status === 'TASK_COMPLETED' ? 'success' : 'warning' } value={t.status === 'TASK_COMPLETED' ? 1 : t.progress} /></div>
-                        <div style={{ padding: '0 8px', width: '100%' }}>
-                          <strong style={{ overflow: 'hidden', textOverflow:'ellipsis', whiteSpace: 'nowrap' }}>{t.plugin}</strong>
-                          {t.status === 'TASK_COMPLETED' && (<Icon icon='small-tick' size={14} color={Colors.GREEN5} style={{marginLeft: '5px'}} />)}
-                          {t.status === 'TASK_FAILED' && (<Icon icon='warning-sign' size={11} color={Colors.RED5} style={{marginLeft: '5px', marginBottom: '3px'}} />)}
-                        </div>
-                        <div style={{ padding: '0', minWidth: '80px', textAlign: 'right' }}><strong>{Number(t.spentSeconds / 60).toFixed(2)}mins</strong></div>
-                        <div style={{ padding: '0 8px', minWidth: '100px', textAlign: 'right' }}><span color={Colors.GRAY5}>{Number(t.status === 'TASK_COMPLETED' ? 100 : (t.progress/1) * 100).toFixed(2)}%</span></div>
-                        <div></div>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              </>
-            </Popover>
-          </div>
-        </div>
-      </CSSTransition> */}
-      {/* )} */}
     </>
   )
 }
