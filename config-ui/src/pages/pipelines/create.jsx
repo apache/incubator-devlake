25e4b47ab7f49f2203df53971353a926d8a8801e
Julien Chinapen <julien.chinapen@merico.dev>
2021-12-17 22:59:27 -0500

    fix: extract pipeline provider settings

--- config-ui/src/pages/pipelines/create.jsx
+++ config-ui/src/pages/pipelines/create.jsx
@@ -30,6 +30,7 @@ import usePipelineValidation from '@/hooks/usePipelineValidation'
 import FormValidationErrors from '@/components/messages/FormValidationErrors'
 import PipelineIndicator from '@/components/widgets/PipelineIndicator'
 import PipelinePresetsMenu from '@/components/menus/PipelinePresetsMenu'
+import ProviderSettings from '@/components/pipelines/ProviderSettings'
 import Nav from '@/components/Nav'
 import Sidebar from '@/components/Sidebar'
 import AppCrumbs from '@/components/Breadcrumbs'
@@ -159,141 +160,6 @@ const CreatePipeline = (props) => {
       }
     }
   }, [getProviderOptions])
-
-  const showProviderSettings = (providerId) => {
-    let providerSettings = null
-    switch (providerId) {
-      case Providers.JENKINS:
-        providerSettings = <p><strong style={{ fontWeight: 900 }}>AUTO-CONFIGURED</strong><br />No Additional Settings</p>
-        break
-      case Providers.JIRA:
-        providerSettings = (
-          <>
-            <FormGroup
-              disabled={isRunning || !isProviderEnabled(providerId)}
-              label={<strong>Source ID<span className='requiredStar'>*</span></strong>}
-              labelInfo={<span style={{ display: 'block' }}>Enter Connection Instance ID</span>}
-              inline={false}
-              labelFor='source-id'
-              className=''
-              contentClassName=''
-              fill
-            >
-              <InputGroup
-                id='source-id'
-                disabled={isRunning || !isProviderEnabled(providerId)}
-                placeholder='eg. 54'
-                value={sourceId}
-                onChange={(e) => setSourceId(e.target.value)}
-                className='input-source-id'
-                autoComplete='off'
-                fill={false}
-              />
-            </FormGroup>
-            <FormGroup
-              disabled={isRunning || !isProviderEnabled(providerId)}
-              label={<strong>Board ID<span className='requiredStar'>*</span></strong>}
-              labelInfo={<span style={{ display: 'block' }}>Enter JIRA Board No.</span>}
-              inline={false}
-              labelFor='board-id'
-              className=''
-              contentClassName=''
-              style={{ marginLeft: '12px' }}
-              fill
-            >
-              <InputGroup
-                id='board-id'
-                disabled={isRunning || !isProviderEnabled(providerId)}
-                placeholder='eg. 8'
-                value={boardId}
-                onChange={(e) => setBoardId(e.target.value)}
-                className='input-board-id'
-                autoComplete='off'
-                fill={false}
-              />
-            </FormGroup>
-          </>
-        )
-        break
-      case Providers.GITHUB:
-        providerSettings = (
-          <>
-            <FormGroup
-              disabled={isRunning || !isProviderEnabled(providerId)}
-              label={<strong>Owner<span className='requiredStar'>*</span></strong>}
-              labelInfo={<span style={{ display: 'block' }}>Enter Project Owner</span>}
-              inline={false}
-              labelFor='owner'
-              className=''
-              contentClassName=''
-              fill
-            >
-              <InputGroup
-                id='owner'
-                disabled={isRunning || !isProviderEnabled(providerId)}
-                placeholder='eg. merio-dev'
-                value={owner}
-                onChange={(e) => setOwner(e.target.value)}
-                className='input-owner'
-                autoComplete='off'
-              />
-            </FormGroup>
-            <FormGroup
-              disabled={isRunning || !isProviderEnabled(providerId)}
-              label={<strong>Repository Name<span className='requiredStar'>*</span></strong>}
-              labelInfo={<span style={{ display: 'block' }}>Enter Git repository</span>}
-              inline={false}
-              labelFor='repository-name'
-              className=''
-              contentClassName=''
-              style={{ marginLeft: '12px' }}
-              fill
-            >
-              <InputGroup
-                id='repository-name'
-                disabled={isRunning || !isProviderEnabled(providerId)}
-                placeholder='eg. lake'
-                value={repositoryName}
-                onChange={(e) => setRepositoryName(e.target.value)}
-                className='input-repository-name'
-                autoComplete='off'
-                fill={false}
-              />
-            </FormGroup>
-          </>
-        )
-        break
-      case Providers.GITLAB:
-        providerSettings = (
-          <>
-            <FormGroup
-              disabled={isRunning || !isProviderEnabled(providerId)}
-              label={<strong>Project ID<span className='requiredStar'>*</span></strong>}
-              labelInfo={<span style={{ display: 'block' }}>Enter the GitLab Project ID No.</span>}
-              inline={false}
-              labelFor='project-id'
-              className=''
-              contentClassName=''
-            >
-              <InputGroup
-                id='project-id'
-                disabled={isRunning || !isProviderEnabled(providerId)}
-                placeholder='eg. 937810831'
-                value={projectId}
-                onChange={(e) => setProjectId(pId => e.target.value)}
-                className='input-project-id'
-                autoComplete='off'
-              />
-            </FormGroup>
-          </>
-        )
-        break
-      default:
-        break
-    }
-
-    return providerSettings
-  }
 
   useEffect(() => {
 
@@ -506,7 +372,22 @@ const CreatePipeline = (props) => {
                         </Tooltip>
                       </div>
                       <div className='provider-settings'>
-                        {showProviderSettings(provider.id)}
+                        {/* showProviderSettings(provider.id) */}
+                        <ProviderSettings
+                          providerId={provider.id}
+                          projectId={projectId}
+                          owner={owner}
+                          repositoryName={repositoryName}
+                          sourceId={sourceId}
+                          boardId={boardId}
+                          setProjectId={setProjectId}
+                          setOwner={setOwner}
+                          setRepositoryName={setRepositoryName}
+                          setSourceId={setSourceId}
+                          setBoardId={setBoardId}
+                          isEnabled={isProviderEnabled}
+                          isRunning={isRunning}
+                        />
                       </div>
                       <div className='provider-actions'>
                         <ButtonGroup minimal rounded='true'>
