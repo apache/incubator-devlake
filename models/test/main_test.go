9389fba2fa8e4a10a5cbf8a372069687ca6f5b44
Jonathan O'Donnell <joncodo@meri.co>
2021-12-20 09:44:23 -0400

    fix: lint

--- models/test/main_test.go
+++ models/test/main_test.go
@@ -1,11 +1,9 @@
 package test
 
 import (
-	"fmt"
 	"os"
 	"testing"
 
-	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/mysql"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
@@ -15,8 +13,8 @@ import (
 // ...before and after m.Run()
 // http://cs-guy.com/blog/2015/01/test-main/
 
-var ROOT_CONNECTION_STRING string = "mysql://root:admin@tcp(localhost:3306)/lake"
-var MIGRATIONS_PATH string = "file://../../db/migration"
+// var ROOT_CONNECTION_STRING string = "mysql://root:admin@tcp(localhost:3306)/lake"
+// var MIGRATIONS_PATH string = "file://../../db/migration"
 
 func TestMain(m *testing.M) {
 	err := setup()
@@ -47,36 +45,36 @@ func setup() error {
 	return nil
 }
 
-func runMigrationsUp() error {
-	m, err := migrate.New(
-		MIGRATIONS_PATH,
-		ROOT_CONNECTION_STRING)
+// func runMigrationsUp() error {
+// 	m, err := migrate.New(
+// 		MIGRATIONS_PATH,
+// 		ROOT_CONNECTION_STRING)
 
-	if err != nil {
-		fmt.Println("ERROR: Could not init migrate for UP: ", err)
-		return err
-	}
-	err = m.Up()
-	if err != nil {
-		fmt.Println("ERROR: Could not run migrations UP: ", err)
-		return err
-	}
-	return nil
-}
+// 	if err != nil {
+// 		fmt.Println("ERROR: Could not init migrate for UP: ", err)
+// 		return err
+// 	}
+// 	err = m.Up()
+// 	if err != nil {
+// 		fmt.Println("ERROR: Could not run migrations UP: ", err)
+// 		return err
+// 	}
+// 	return nil
+// }
 
-func runMigrationsDown() error {
-	m, err := migrate.New(
-		MIGRATIONS_PATH,
-		ROOT_CONNECTION_STRING)
+// func runMigrationsDown() error {
+// 	m, err := migrate.New(
+// 		MIGRATIONS_PATH,
+// 		ROOT_CONNECTION_STRING)
 
-	if err != nil {
-		fmt.Println("ERROR: Could not init migrate for DOWN: ", err)
-		return err
-	}
-	err = m.Down()
-	if err != nil {
-		fmt.Println("ERROR: Could not run migrations DOWN: ", err)
-		return err
-	}
-	return nil
-}
+// 	if err != nil {
+// 		fmt.Println("ERROR: Could not init migrate for DOWN: ", err)
+// 		return err
+// 	}
+// 	err = m.Down()
+// 	if err != nil {
+// 		fmt.Println("ERROR: Could not run migrations DOWN: ", err)
+// 		return err
+// 	}
+// 	return nil
+// }
