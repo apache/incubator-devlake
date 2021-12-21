84285170b94cd3dbdc8225e07d817ea647b8f628
Jonathan O'Donnell <joncodo@meri.co>
2021-12-21 12:34:25 -0400

    feat: make the migrations run all the migrations on make dev

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/merico-dev/lake/api"
+	"github.com/merico-dev/lake/db"
 	"github.com/merico-dev/lake/plugins"
 )
 
 func main() {
+	migrateDB()
+	startAPI()
+}
+
+func startAPI() {
 	err := plugins.LoadPlugins(plugins.PluginDir())
 	if err != nil {
 		panic(err)
 	}
 	api.CreateApiService()
 	println("Hello, lake")
+}
+
+func migrateDB() {
+	err := db.RunMigrationsUp("lake")
+	if err != nil {
+		fmt.Println("INFO: ", err)
+	}
 }
