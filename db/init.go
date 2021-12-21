397452f8e7c6386b785b0930ff0a130f6599d90e
Jonathan O'Donnell <joncodo@meri.co>
2021-12-21 13:38:49 -0400

    feat: running pluginn migrations on make dev

--- db/init.go
+++ db/init.go
@@ -5,11 +5,11 @@ import (
 
 	_ "github.com/golang-migrate/migrate/v4/database/mysql"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/merico-dev/lake/config"
 
 	"github.com/golang-migrate/migrate/v4"
 )
 
-var ROOT_CONNECTION_STRING string = "mysql://root:admin@tcp(localhost:3306)/%v"
 var MIGRATIONS_PATH string = "file://./db/migration"
 
 func MigrateDB(dbName string) {
@@ -17,12 +17,35 @@ func MigrateDB(dbName string) {
 	if err != nil {
 		fmt.Println("INFO: ", err)
 	}
+	RunPluginMigrationsUp(dbName, "github")
+}
+
+func RunPluginMigrationsUp(dbName string, pluginName string) error {
+	connectionString := config.V.GetString("DB_URL")
+
+	dbParams := fmt.Sprintf("x-migrations-table=schema_migrations_%v&x-migrations-table-quoted=1", pluginName)
+
+	m, err := migrate.New(
+		fmt.Sprintf("file://./plugins/%v/migration", pluginName),
+		fmt.Sprintf("%v/%v?%v", connectionString, dbName, dbParams))
+
+	if err != nil {
+		fmt.Println("ERROR: Could not init migrate for UP: ", err)
+		return err
+	}
+	err = m.Up()
+	if err != nil {
+		fmt.Println("ERROR: Could not run migrations UP: ", err)
+		return err
+	}
+	return nil
 }
 
 func RunMigrationsUp(dbName string) error {
+	connectionString := config.V.GetString("DB_URL")
 	m, err := migrate.New(
 		MIGRATIONS_PATH,
-		fmt.Sprintf(ROOT_CONNECTION_STRING, dbName))
+		fmt.Sprintf("%v/%v", connectionString, dbName))
 
 	if err != nil {
 		fmt.Println("ERROR: Could not init migrate for UP: ", err)
@@ -37,9 +60,11 @@ func RunMigrationsUp(dbName string) error {
 }
 
 func RunMigrationsDown(dbName string) error {
+	connectionString := config.V.GetString("DB_URL")
+
 	m, err := migrate.New(
 		MIGRATIONS_PATH,
-		fmt.Sprintf(ROOT_CONNECTION_STRING, dbName))
+		fmt.Sprintf("%v/%v", connectionString, dbName))
 
 	if err != nil {
 		fmt.Println("ERROR: Could not init migrate for DOWN: ", err)
