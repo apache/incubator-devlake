aa72b0d463ec6acdb8bd2b2023793fd7bbabd230
Julien Chinapen <julien.chinapen@merico.dev>
2021-12-22 18:33:11 -0500

    fix: refactor ux layout based on feedback

---
+++ config-ui/src/components/pipelines/StagePanel.jsx
@@ -0,0 +1,136 @@
+import React from 'react'
+import { CSSTransition } from 'react-transition-group'
+import {
+  // Classes,
+  Card,
+  Button, Icon, Intent, Switch,
+  // H2, Card, Elevation, Tag,
+  // Menu,
+  FormGroup,
+  ButtonGroup,
+  Elevation,
+  InputGroup,
+  Popover,
+  Tooltip,
+  Position,
+  // Spinner,
+  Colors,
+  // Alignment
+} from '@blueprintjs/core'
+import { ReactComponent as PipelineRunningIcon } from '@/images/synchronize.svg'
+import { ReactComponent as PipelineFailedIcon } from '@/images/no-synchronize.svg'
+import { ReactComponent as PipelineCompleteIcon } from '@/images/check-circle.svg'
+
+const StagePanel = (props) => {
+  const { activePipeline, pipelineReady = false } = props
+
+  return (
+    <>
+      <CSSTransition
+        in={pipelineReady}
+        timeout={350}
+        classNames='activity-panel'
+      >
+        <Card
+          elevation={Elevation.TWO}
+          style={{
+            display: 'flex',
+            width: '100%',
+            justifySelf: 'flex-start',
+            marginBottom: '8px',
+            padding: 0,
+            backgroundColor: activePipeline.status === 'TASK_COMPLETED' ? 'rgba(245, 255, 250, 0.99)' : 'inherit'
+          }}
+        >
+
+          <ButtonGroup style={{ backgroundColor: 'transparent' }}>
+            <Button minimal active style={{ backgroundColor: '#eeeeee' }}>
+              <h3 style={{ margin: 0, fontSize: '20px', display: 'flex' }}>
+                Stage 1
+                {(() => {
+                  let statusIcon = null
+                  switch (activePipeline.status) {
+                    case 'TASK_COMPLETED':
+                      statusIcon = (
+                        <Icon
+                          icon={<PipelineCompleteIcon width={24} height={24} style={{ margin: '0 0 0 10px', display: 'flex', alignSelf: 'center' }} />}
+                          size={24}
+                        />
+                      )
+                      break
+                    case 'TASK_FAILED':
+                      statusIcon = (
+                        <Icon
+                          icon={<PipelineFailedIcon width={24} height={24} style={{ margin: '0 0 0 10px', display: 'flex', alignSelf: 'center' }} />}
+                          size={24}
+                        />
+                      )
+                      break
+                    case 'TASK_RUNNING':
+                    default:
+                      statusIcon = (
+                        <Icon
+                          icon={<PipelineRunningIcon width={24} height={24} style={{ margin: '0 0 0 10px', display: 'flex', alignSelf: 'center' }} />}
+                          size={24}
+                        />
+                      )
+                      break
+                  }
+                  return statusIcon
+                })()}
+              </h3>
+            </Button>
+            {/* <Button
+              minimal style={{
+                backgroundColor: '#eeeeee',
+                color: '#cccccc',
+                fontSize: '35px',
+                lineHeight: '20px',
+                padding: 0,
+                fontWeight: 100,
+              }}
+            >/
+            </Button> */}
+            {/* <Button
+              disabled style={{
+                backgroundColor: '#eeeeee',
+              }}
+            ><h3 style={{ margin: 0, fontSize: '20px' }}>Stage 2</h3>
+            </Button> */}
+            <Button
+              className='btn-stage-endcap'
+              minimal
+              style={{
+                marginLeft: '1px',
+                background: '#ffffff!!important',
+                width: 0,
+                height: 0,
+                borderTop: '16px solid transparent',
+                borderBottom: '16px solid transparent',
+                borderLeft: '16px solid #eeeeee',
+                pointerEvents: 'none'
+              }}
+            />
+          </ButtonGroup>
+          <h3 style={{
+            textTransform: 'uppercase',
+            lineHeight: '33px',
+            margin: 0,
+            fontFamily: 'Montserrat',
+            fontWeight: 800,
+            letterSpacing: '2px'
+          }}
+          >Finished Tasks &middot; <span style={{ color: Colors.GREEN5 }}>{activePipeline.finishedTasks}</span>
+            <em style={{ color: '#dddddd', padding: '0 4px', textTransform: 'lowercase' }}>/</em>{activePipeline.totalTasks}
+          </h3>
+          <div style={{ display: 'flex', fontSize: '16px', fontWeight: 700, marginLeft: 'auto', lineHeight: '33px', padding: '0 10px' }}>
+
+            {Number((activePipeline.finishedTasks / activePipeline.totalTasks) * 100).toFixed(1)}%
+          </div>
+        </Card>
+      </CSSTransition>
+    </>
+  )
+}
+
+export default StagePanel
---
+++ config-ui/src/components/pipelines/TaskActivity.jsx
@@ -0,0 +1,155 @@
+
+import React from 'react'
+// import { CSSTransition } from 'react-transition-group'
+import {
+  // Classes,
+  Icon,
+  Spinner,
+  Colors,
+  // Alignment
+} from '@blueprintjs/core'
+
+const TaskActivity = (props) => {
+  const { activePipeline, dayjs } = props
+
+  return (
+    <>
+
+      <div
+        className='pipeline-task-activity' style={{
+          padding: '20px',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis'
+          // paddingTop: '7px',
+          // borderTop: '1px solid #f5f5f5',
+          // marginTop: '14px',
+          // marginBottom: '36px'
+        }}
+      >
+        {activePipeline?.ID && activePipeline.tasks && activePipeline.tasks.map((t, tIdx) => (
+          <div
+            className='pipeline-task-row'
+            key={`pipeline-task-key-${tIdx}`}
+            style={{ display: 'flex', padding: '4px 6px', justifyContent: 'space-between', fontSize: '12px' }}
+          >
+            <div style={{ display: 'flex', justifyContent: 'center', paddingRight: '8px', width: '32px', minWidth: '32px' }}>
+              {t.status === 'TASK_COMPLETED' && (
+                <Icon icon='small-tick' size={18} color={Colors.GREEN5} style={{ marginLeft: '0' }} />
+              )}
+              {t.status === 'TASK_FAILED' && (
+                <Icon icon='warning-sign' size={14} color={Colors.RED5} style={{ marginLeft: '0', marginBottom: '3px' }} />
+              )}
+              {t.status === 'TASK_RUNNING' && (
+                <Spinner
+                  className='task-spinner'
+                  size={14}
+                  intent={t.status === 'TASK_COMPLETED' ? 'success' : 'warning'}
+                  value={t.status === 'TASK_COMPLETED' ? 1 : t.progress}
+                />
+              )}
+            </div>
+            <div
+              className='pipeline-task-cell-name'
+              style={{ padding: '0 8px', minWidth: '100px', display: 'flex', justifyContent: 'space-between' }}
+            >
+              <strong
+                className='task-plugin-name'
+                style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}
+              >
+                {t.plugin}
+              </strong>
+
+            </div>
+            <div
+              className='pipeline-task-cell-settings'
+              style={{
+                padding: '0 8px',
+                display: 'flex',
+                width: '20%',
+                minWidth: '20%',
+                // whiteSpace: 'nowrap',
+                justifyContent: 'flex-start',
+                // overflow: 'hidden',
+                // textOverflow: 'ellipsis',
+                flexGrow: 1
+              }}
+            >
+              {t.plugin !== 'jenkins' && (
+                <div style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+                  <span style={{ color: Colors.GRAY2 }}>
+                    <Icon icon='link' size={8} style={{ marginBottom: '3px' }} /> {t.options[Object.keys(t.options)[0]]}
+                  </span>
+                  {t.plugin === 'github' && (
+                    <span style={{ fontWeight: 60 }}>/{t.options[Object.keys(t.options)[1]]}</span>
+                  )}
+                </div>
+              )}
+            </div>
+            <div
+              className='pipeline-task-cell-duration'
+              style={{
+                padding: '0',
+                minWidth: '80px',
+                // whiteSpace: 'nowrap',
+                textAlign: 'right'
+              }}
+            >
+              <span style={{ whiteSpace: 'nowrap' }}>
+                {(() => {
+                  let statusRelativeTime = dayjs(t.CreatedAt).toNow(true)
+                  switch (t.status) {
+                    case 'TASK_COMPLETED':
+                    case 'TASK_FAILED':
+                      statusRelativeTime = dayjs(t.UpdatedAt).from(t.CreatedAt, true)
+                      break
+                    case 'TASK_RUNNING':
+                    default:
+                      statusRelativeTime = dayjs(t.CreatedAt).toNow(true)
+                      break
+                  }
+                  return statusRelativeTime
+                })()}
+              </span>
+            </div>
+            <div
+              className='pipeline-task-cell-progress'
+              style={{
+                padding: '0 8px',
+                minWidth: '100px',
+                textAlign: 'right'
+              }}
+            >
+              <span style={{ fontWeight: t.status === 'TASK_COMPLETED' ? 800 : 600 }}>
+                {Number(t.status === 'TASK_COMPLETED' ? 100 : (t.progress / 1) * 100).toFixed(2)}%
+              </span>
+            </div>
+            <div
+              className='pipeline-task-cell-message'
+              style={{ display: 'flex', flexGrow: 1, width: '64%' }}
+            >
+              {/* {t.plugin !== 'jenkins' && (
+                <>
+                  <span style={{ color: Colors.GRAY2 }}>
+                    <Icon icon='link' size={8} style={{ marginBottom: '3px' }} /> {t.options[Object.keys(t.options)[0]]}
+                  </span>
+                  {t.plugin === 'github' && (
+                    <span style={{ fontWeight: 60 }}>/{t.options[Object.keys(t.options)[1]]}</span>
+                  )}
+                </>
+              )} */}
+              {t.message && (
+                <div style={{ width: '98%', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                  <span style={{ color: t.status === 'TASK_FAILED' ? Colors.RED4 : Colors.GRAY3, paddingLeft: '10px' }}>
+                    {t.message}
+                  </span>
+                </div>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default TaskActivity
