d24d397b2beb3a3167bb3ea3caab12492b8d6ee9
Julien Chinapen <julien.chinapen@merico.dev>
2022-01-21 21:52:20 -0500

    fix: add details popover for task module name

--- config-ui/src/components/pipelines/StageTask.jsx
+++ config-ui/src/components/pipelines/StageTask.jsx
@@ -2,6 +2,10 @@
 import React from 'react'
 // import { CSSTransition } from 'react-transition-group'
 import {
+  Providers,
+  ProviderLabels
+} from '@/data/Providers'
+import {
   // Classes,
   Icon,
   Spinner,
@@ -11,6 +15,8 @@ import {
   Intent,
   Card,
   Elevation,
+  Popover,
+  TextArea,
   H4,
   // Alignment
 } from '@blueprintjs/core'
@@ -86,7 +92,76 @@ const StageTask = (props) => {
           }}
         >
           <div style={{ padding: '4px 2px 4px 0' }}>
-            {task.plugin}<br />
+            <Popover
+              className='trigger-pipeline-activity-help'
+              popoverClassName='popover-help-pipeline-activity'
+              position={Position.RIGHT}
+              autoFocus={false}
+              enforceFocus={false}
+              usePortal={true}
+            >
+              <span>{task.plugin}</span>
+              <>
+                <div style={{ textShadow: 'none', fontSize: '12px', padding: '12px', maxWidth: '320px' }}>
+                  <div style={{ marginBottom: '10px', fontWeight: 700, fontSize: '14px', fontFamily: '"Montserrat", sans-serif' }}>
+                    <Icon icon='layer' size={16} /> {ProviderLabels[task.plugin.toUpperCase()] || 'System Task'}<br />
+                  </div>
+                  <div style={{ fontSize: '10px' }}>
+                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                      <div>
+                        <label style={{ color: Colors.GRAY2 }}>ID</label><br />
+                        <span>{task.ID}</span>
+                      </div>
+                      <div style={{ marginLeft: '20px' }}>
+                        <label style={{ color: Colors.GRAY2 }}>Created</label><br />
+                        <span>{dayjs(task.CreatedAt).format('L LT')}</span>
+                      </div>
+                      {task.finishedAt && (
+                        <div style={{ marginLeft: '20px' }}>
+                          <label style={{ color: Colors.GRAY2 }}>Finished</label><br />
+                          <span>{dayjs(task.finishedAt).format('L LT')}</span>
+                        </div>
+                      )}
+                    </div>
+                    <div style={{ marginTop: '6px' }}>
+                      <label style={{ color: Colors.GRAY2 }}>Status</label><br />
+                      <strong>{task.status}</strong> <strong className='bp3-tag bp3-intent-primary' style={{ minHeight: '16px', fontSize: '10px', padding: '2px 6px', borderRadius: '6px'}}>PROGRESS {task.progress * 100}%</strong>
+                    </div>
+                    <div style={{ marginTop: '6px' }}>
+                      <label style={{ color: Colors.GRAY2 }}>Options</label><br />
+                      <TextArea
+                        readonly
+                        fill value={JSON.stringify(task.options)} style={{ fontSize: '10px', backgroundColor: '#f8f8f8', resize: 'none' }}
+                      />
+                      {/* <span>
+                        <pre style={{ margin: 0 }}>
+                          <code>
+                            {JSON.stringify(task.options)}
+                          </code>
+                        </pre>
+                      </span> */}
+                    </div>
+                    {task.message !== '' && (
+                      <div style={{ marginTop: '6px' }}>
+                        <label style={{ color: Colors.DARK_GRAY1 }}>Message</label><br />
+                        <span style={{ color: task.status === 'TASK_FAILED' ? Colors.RED3 : Colors.BLACK }}>
+                          {task.status === 'TASK_FAILED' && (
+                            <Icon
+                              icon='warning-sign'
+                              color={Colors.RED5}
+                              size={10}
+                              style={{ marginRight: '3px' }}
+                            />
+                          )}
+                          {task.message}
+                        </span>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </>
+            </Popover>
+
             <span style={{
               opacity: 0.4,
               display: 'block',
