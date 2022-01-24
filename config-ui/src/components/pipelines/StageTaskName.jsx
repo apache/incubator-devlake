35324a752eeff533abcdc3c077c0a4570cc7b914
Julien Chinapen <julien.chinapen@merico.dev>
2022-01-24 15:10:42 -0500

    fix: change popover details for pending tasks

--- config-ui/src/components/pipelines/StageTaskName.jsx
+++ config-ui/src/components/pipelines/StageTaskName.jsx
@@ -73,64 +73,73 @@ const StageTaskName = (props) => {
                 {ProviderIcons[task.plugin.toLowerCase()](32, 32)}
               </div>
             </div>
-
-            <div style={{ fontSize: '10px' }}>
-              <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                <div>
-                  <label style={{ color: Colors.GRAY2 }}>ID</label><br />
-                  <span>{task.ID}</span>
-                </div>
-                <div style={{ marginLeft: '20px' }}>
-                  <label style={{ color: Colors.GRAY2 }}>Created</label><br />
-                  <span>{dayjs(task.CreatedAt).format('L LT')}</span>
-                </div>
-                {task.finishedAt && (
-                  <div style={{ marginLeft: '20px' }}>
-                    <label style={{ color: Colors.GRAY2 }}>Finished</label><br />
-                    <span>{dayjs(task.finishedAt).format('L LT')}</span>
+            {task.status === 'TASK_CREATED' && (
+              <div style={{ fontSize: '10px' }}>
+                <p style={{ fontSize: '14px' }}>
+                  This task (ID #{task.ID}) is <strong>PENDING</strong> and has not yet started.
+                </p>
+                <strong>Created Date &mdash;</strong> <span>{dayjs(task.CreatedAt).format('L LT')}</span>
+              </div>
+            )}
+            {task.status !== 'TASK_CREATED' && (
+              <div style={{ fontSize: '10px' }}>
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                  <div>
+                    <label style={{ color: Colors.GRAY2 }}>ID</label><br />
+                    <span>{task.ID}</span>
                   </div>
-                )}
-              </div>
-              <div style={{ marginTop: '6px' }}>
-                <label style={{ color: Colors.GRAY2 }}>Status</label><br />
-                <strong>{task.status}</strong>{' '}
-                <strong
-                  className='bp3-tag bp3-intent-primary'
-                  style={{ minHeight: '16px', fontSize: '10px', padding: '2px 6px', borderRadius: '6px' }}
-                >PROGRESS {task.progress * 100}%
-                </strong>
-              </div>
-              <div style={{ marginTop: '6px' }}>
-                <label style={{ color: Colors.GRAY2 }}>Options</label><br />
-                <TextArea
-                  readOnly
-                  fill value={JSON.stringify(task.options)} style={{ fontSize: '10px', backgroundColor: '#f8f8f8', resize: 'none' }}
-                />
-                {/* <span>
+                  <div style={{ marginLeft: '20px' }}>
+                    <label style={{ color: Colors.GRAY2 }}>Created</label><br />
+                    <span>{dayjs(task.CreatedAt).format('L LT')}</span>
+                  </div>
+                  {task.finishedAt && (
+                    <div style={{ marginLeft: '20px' }}>
+                      <label style={{ color: Colors.GRAY2 }}>Finished</label><br />
+                      <span>{dayjs(task.finishedAt).format('L LT')}</span>
+                    </div>
+                  )}
+                </div>
+                <div style={{ marginTop: '6px' }}>
+                  <label style={{ color: Colors.GRAY2 }}>Status</label><br />
+                  <strong>{task.status}</strong>{' '}
+                  <strong
+                    className='bp3-tag bp3-intent-primary'
+                    style={{ minHeight: '16px', fontSize: '10px', padding: '2px 6px', borderRadius: '6px' }}
+                  >PROGRESS {task.progress * 100}%
+                  </strong>
+                </div>
+                <div style={{ marginTop: '6px' }}>
+                  <label style={{ color: Colors.GRAY2 }}>Options</label><br />
+                  <TextArea
+                    readOnly
+                    fill value={JSON.stringify(task.options)} style={{ fontSize: '10px', backgroundColor: '#f8f8f8', resize: 'none' }}
+                  />
+                  {/* <span>
                         <pre style={{ margin: 0 }}>
                           <code>
                             {JSON.stringify(task.options)}
                           </code>
                         </pre>
                       </span> */}
-              </div>
-              {task.message !== '' && (
-                <div style={{ marginTop: '6px' }}>
-                  <label style={{ color: Colors.DARK_GRAY1 }}>Message</label><br />
-                  <span style={{ color: task.status === 'TASK_FAILED' ? Colors.RED3 : Colors.BLACK }}>
-                    {task.status === 'TASK_FAILED' && (
-                      <Icon
-                        icon='warning-sign'
-                        color={Colors.RED5}
-                        size={10}
-                        style={{ marginRight: '3px' }}
-                      />
-                    )}
-                    {task.message}
-                  </span>
                 </div>
-              )}
-            </div>
+                {task.message !== '' && (
+                  <div style={{ marginTop: '6px' }}>
+                    <label style={{ color: Colors.DARK_GRAY1 }}>Message</label><br />
+                    <span style={{ color: task.status === 'TASK_FAILED' ? Colors.RED3 : Colors.BLACK }}>
+                      {task.status === 'TASK_FAILED' && (
+                        <Icon
+                          icon='warning-sign'
+                          color={Colors.RED5}
+                          size={10}
+                          style={{ marginRight: '3px' }}
+                        />
+                      )}
+                      {task.message}
+                    </span>
+                  </div>
+                )}
+              </div>
+            )}
           </div>
         </>
       </Popover>
