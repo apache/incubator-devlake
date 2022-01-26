d82c2e032c62d989d0e3bb112ce5f5dff2d82aec
Julien Chinapen <julien.chinapen@merico.dev>
2022-01-26 09:36:35 -0500

    fix: reflow the pipeline stage panel display

--- config-ui/src/components/pipelines/StagePanel.jsx
+++ config-ui/src/components/pipelines/StagePanel.jsx
@@ -19,108 +19,155 @@ const StagePanel = (props) => {
 
   return (
     <>
-      <CSSTransition
+      {/* <CSSTransition
         in={pipelineReady}
         timeout={350}
         classNames='activity-panel'
+      > */}
+      <Card
+        elevation={isLoading ? Elevation.THREE : Elevation.TWO}
+        className='stage-panel-card'
+        style={{
+          transition: 'all 0.3s ease-out',
+          display: 'flex',
+          width: '100%',
+          justifySelf: 'flex-start',
+          marginBottom: '8px',
+          padding: 0,
+          backgroundColor: activePipeline.status === 'TASK_COMPLETED' ? 'rgba(245, 255, 250, 0.99)' : 'inherit',
+          overflow: 'hidden',
+          whiteSpace: 'nowrap',
+          textOverflow: 'ellipsis'
+        }}
       >
-        <Card
-          elevation={isLoading ? Elevation.THREE : Elevation.TWO}
-          className='stage-panel-card'
-          style={{
-            transition: 'all 0.3s ease-out',
-            display: 'flex',
-            width: '100%',
-            justifySelf: 'flex-start',
-            marginBottom: '8px',
-            padding: 0,
-            backgroundColor: activePipeline.status === 'TASK_COMPLETED' ? 'rgba(245, 255, 250, 0.99)' : 'inherit',
-            overflow: 'hidden',
-            whiteSpace: 'nowrap',
-            textOverflow: 'ellipsis'
-          }}
-        >
 
-          <ButtonGroup style={{ backgroundColor: 'transparent' }}>
-            <Button minimal active style={{ width: '32px', backgroundColor: '#eeeeee', padding: 0 }}>
-              <div style={{
-                margin: 0,
-                display: 'flex',
-                position: 'relative',
-                justifyContent: 'center',
-                alignItems: 'center',
-                alignContent: 'center'
-              }}
-              >
-                {isLoading && (
-                  <span style={{
-                    position: 'absolute',
-                    width: '24px',
-                    height: '24px',
-                    marginLeft: '4px',
-                    display: 'flex',
-                    justifyContent: 'center'
-                  }}
-                  >
-                    <Spinner size={20} intent={Intent.PRIMARY} />
-                  </span>)}
-                {(() => {
-                  let statusIcon = null
-                  switch (activePipeline.status) {
-                    case 'TASK_COMPLETED':
-                      statusIcon = (
-                        <Icon
-                          icon={<PipelineCompleteIcon
-                            width={24} height={24} style={{
-                              margin: '0 0 0 0',
-                              display: 'flex',
-                              alignSelf: 'center'
-                            }}
-                                />}
-                          size={24}
-                        />
-                      )
-                      break
-                    case 'TASK_FAILED':
-                      statusIcon = (
-                        <Icon
-                          icon={<PipelineFailedIcon
-                            width={24}
-                            height={24} style={{
-                              margin: '0 0 0 0',
-                              display: 'flex',
-                              alignSelf: 'center'
-                            }}
-                                />}
-                          size={24}
-                        />
-                      )
-                      break
-                    case 'TASK_RUNNING':
-                    default:
-                      statusIcon = (
-                        <Icon
-                          style={{ margin: 0, padding: 0, float: 'left' }}
-                          icon={<PipelineRunningIcon
-                            width={24}
-                            height={24} style={{
-                              margin: '0 0 0 0',
-                              padding: 0,
-                              display: 'inline',
-                              alignSelf: 'center'
-                            }}
-                                />}
-                          size={24}
-                        />
-                      )
-                      break
-                  }
-                  return !isLoading && (<span style={{ position: 'absolute', marginLeft: '3px', width: '24px', height: '24px' }}>{statusIcon}</span>)
-                })()}
-              </div>
-            </Button>
-            {/* @todo: re-activate "stage" ux in a future release */}
-            {Object.keys(stages).length > 0 && (
+        <ButtonGroup style={{ backgroundColor: 'transparent', zIndex: '-1' }}>
+          <Button minimal active style={{ width: '32px', backgroundColor: '#eeeeee', padding: 0 }}>
+            <div style={{
+              margin: 0,
+              display: 'flex',
+              position: 'relative',
+              justifyContent: 'center',
+              alignItems: 'center',
+              alignContent: 'center'
+            }}
+            >
+              {isLoading && (
+                <span style={{
+                  position: 'absolute',
+                  width: '24px',
+                  height: '24px',
+                  marginLeft: '4px',
+                  display: 'flex',
+                  justifyContent: 'center'
+                }}
+                >
+                  <Spinner size={20} intent={Intent.PRIMARY} />
+                </span>)}
+              {(() => {
+                let statusIcon = null
+                switch (activePipeline.status) {
+                  case 'TASK_COMPLETED':
+                    statusIcon = (
+                      <Icon
+                        icon={<PipelineCompleteIcon
+                          width={24} height={24} style={{
+                            margin: '0 0 0 0',
+                            display: 'flex',
+                            alignSelf: 'center'
+                          }}
+                              />}
+                        size={24}
+                      />
+                    )
+                    break
+                  case 'TASK_FAILED':
+                    statusIcon = (
+                      <Icon
+                        icon={<PipelineFailedIcon
+                          width={24}
+                          height={24} style={{
+                            margin: '0 0 0 0',
+                            display: 'flex',
+                            alignSelf: 'center'
+                          }}
+                              />}
+                        size={24}
+                      />
+                    )
+                    break
+                  case 'TASK_RUNNING':
+                  default:
+                    statusIcon = (
+                      <Icon
+                        style={{ margin: 0, padding: 0, float: 'left' }}
+                        icon={<PipelineRunningIcon
+                          width={24}
+                          height={24} style={{
+                            margin: '0 0 0 0',
+                            padding: 0,
+                            display: 'inline',
+                            alignSelf: 'center'
+                          }}
+                              />}
+                        size={24}
+                      />
+                    )
+                    break
+                }
+                return !isLoading && (<span style={{ position: 'absolute', marginLeft: '3px', width: '24px', height: '24px' }}>{statusIcon}</span>)
+              })()}
+            </div>
+          </Button>
+          <Button
+              // key={`stage-btn-key${sIdx}`}
+              // disabled={activeStageId !== (sIdx + 1)}
+            minimal
+            style={{
+              position: 'relative',
+              backgroundColor: '#eeeeee',
+              paddingRight: '20px',
+            }}
+          >
+            <h3
+              className='stage-panel-stage-name'
+              style={{ margin: 0, color: Colors.BLACK }}
+            >
+              Active Stage
+            </h3>
+          </Button>
+          <Button
+            className='stage-panel-stage-endcap'
+            minimal
+            style={{
+              marginLeft: '1px',
+              background: '#ffffff!!important',
+              width: 0,
+              height: 0,
+              borderTop: '16px solid transparent',
+              borderBottom: '16px solid transparent',
+              borderLeft: '16px solid #eeeeee',
+              pointerEvents: 'none'
+            }}
+          />
+          <h3
+            className='active-stage-panel-display'
+            style={{
+              color: activePipeline.status === 'TASK_FAILED' ? Colors.RED4 : 'rgb(0, 102, 255)',
+              textTransform: 'uppercase',
+              lineHeight: '33px',
+              margin: 0,
+              fontFamily: 'Montserrat',
+              fontWeight: 800,
+              fontSize: '13px',
+              letterSpacing: '2px',
+              justifySelf: 'flex-start'
+            }}
+          >
+            Stage {activeStageId}
+          </h3>
+          {/* @todo: re-activate "stage steps" ux in a future release */}
+          {/* {Object.keys(stages).length > 0 && (
               <>
                 {Object.keys(stages).map((s, sIdx) => (
                   // <Button
@@ -189,64 +236,64 @@ const StagePanel = (props) => {
                   }}
                 />
               </>
-            )}
-            {Object.keys(stages).length === 0 && (
-              <>
-                <Button
-                  disabled
-                  minimal
-                  style={{
-                    position: 'relative',
-                    backgroundColor: '#eeeeee',
-                    paddingRight: '50px',
-                  }}
+            )} */}
+          {Object.keys(stages).length === 0 && (
+            <>
+              <Button
+                disabled
+                minimal
+                style={{
+                  position: 'relative',
+                  backgroundColor: '#eeeeee',
+                  paddingRight: '50px',
+                }}
+              >
+                <h3
+                  className='stage-panel-stage-name'
+                  style={{ margin: 0, fontSize: '18px', color: Colors.GRAY3 }}
                 >
-                  <h3
-                    className='stage-panel-stage-name'
-                    style={{ margin: 0, fontSize: '18px', color: Colors.GRAY3 }}
-                  >
-                    No Stages
-                  </h3>
-                </Button>
-                <Button
-                  className='stage-panel-stage-endcap'
-                  minimal
-                  style={{
-                    marginLeft: '1px',
-                    background: '#ffffff!!important',
-                    width: 0,
-                    height: 0,
-                    borderTop: '16px solid transparent',
-                    borderBottom: '16px solid transparent',
-                    borderLeft: '16px solid #eeeeee',
-                    pointerEvents: 'none'
-                  }}
-                />
-              </>
+                  No Stages
+                </h3>
+              </Button>
+              <Button
+                className='stage-panel-stage-endcap'
+                minimal
+                style={{
+                  marginLeft: '1px',
+                  background: '#ffffff!!important',
+                  width: 0,
+                  height: 0,
+                  borderTop: '16px solid transparent',
+                  borderBottom: '16px solid transparent',
+                  borderLeft: '16px solid #eeeeee',
+                  pointerEvents: 'none'
+                }}
+              />
+            </>
 
-            )}
-          </ButtonGroup>
-          <div style={{ display: 'flex', marginLeft: 'auto', padding: '0 10px' }}>
-            <h3
-              className='h3-finished-tasks-indicator'
-              style={{
-                textTransform: 'uppercase',
-                lineHeight: '33px',
-                margin: 0,
-                fontFamily: 'Montserrat',
-                fontWeight: 800,
-                fontSize: '13px',
-                letterSpacing: '2px',
-                justifySelf: 'flex-end'
-              }}
-            >Finished Tasks &middot; <span style={{ color: Colors.GREEN5 }}>{activePipeline.finishedTasks}</span>
-              <em style={{ color: '#dddddd', padding: '0 4px', textTransform: 'lowercase' }}>/</em>{activePipeline.totalTasks}
-            </h3>
-            {/* <span style={{fontSize: '16px', fontWeight: 700, marginLeft: 'auto', lineHeight: '33px'}} /> */}
-            {/* {Number((activePipeline.finishedTasks / activePipeline.totalTasks) * 100).toFixed(1)}% */}
-          </div>
-        </Card>
-      </CSSTransition>
+          )}
+        </ButtonGroup>
+        <div style={{ display: 'flex', marginLeft: 'auto', padding: '0 10px' }}>
+          <h3
+            className='h3-finished-tasks-indicator'
+            style={{
+              textTransform: 'uppercase',
+              lineHeight: '33px',
+              margin: 0,
+              fontFamily: 'Montserrat',
+              fontWeight: 800,
+              fontSize: '13px',
+              letterSpacing: '2px',
+              justifySelf: 'flex-end'
+            }}
+          >Finished Tasks &middot; <span style={{ color: Colors.GREEN5 }}>{activePipeline.finishedTasks}</span>
+            <em style={{ color: '#dddddd', padding: '0 4px', textTransform: 'lowercase' }}>/</em>{activePipeline.totalTasks}
+          </h3>
+          {/* <span style={{fontSize: '16px', fontWeight: 700, marginLeft: 'auto', lineHeight: '33px'}} /> */}
+          {/* {Number((activePipeline.finishedTasks / activePipeline.totalTasks) * 100).toFixed(1)}% */}
+        </div>
+      </Card>
+      {/* </CSSTransition> */}
     </>
   )
 }
