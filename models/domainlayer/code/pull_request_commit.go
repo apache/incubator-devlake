2885de82020163cc300c85ed99edfe324259bb73
warrenchen <703288221@qq.com>
2022-01-28 00:12:07 +0800

    fix(plugins): use generated id for pull_request_id in pull_request_commit
    use generated id for pull_request_id in pull_request_commit

--- models/domainlayer/code/pull_request_commit.go
+++ models/domainlayer/code/pull_request_commit.go
@@ -2,5 +2,5 @@ package code
 
 type PullRequestCommit struct {
 	CommitSha     string `gorm:"primaryKey"`
-	PullRequestId int    `gorm:"primaryKey;autoIncrement:false"`
+	PullRequestId string `json:"id" gorm:"primaryKey;type:varchar(255);comment:This key is generated based on details from the original plugin"` // format: <Plugin>:<Entity>:<PK0>:<PK1>
 }
