e1205cc52ce0fd1c2d8efa6cb5394be108e895d8
kevin-kline <kevin.kline@merico.dev>
2022-02-01 14:50:43 -0400

    fix: add config file for db url

--- e2e/database.go
+++ e2e/database.go
@@ -5,20 +5,14 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/go-sql-driver/mysql"
-	"gorm.io/gorm"
 	mysqlGorm "gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func InitializeDb() (*sql.DB, error) {
-	cfg := mysql.Config{
-		User:   "merico",
-		Passwd: "merico",
-		Net:    "tcp",
-		Addr:   "127.0.0.1:3306",
-		DBName: "lake",
-	}
-	db, err := sql.Open("mysql", cfg.FormatDSN())
+	V := LoadConfigFile()
+	dbUrl := V.GetString("DB_URL")
+	db, err := sql.Open("mysql", dbUrl)
 	if err != nil {
 		return nil, err
 	}
@@ -31,12 +25,11 @@ func InitializeDb() (*sql.DB, error) {
 	return db, nil
 }
 
-func InitializeGormDb() (*gorm.DB, error ){
+func InitializeGormDb() (*gorm.DB, error) {
 	connectionString := "merico:merico@tcp(localhost:3306)/lake"
 	db, err := gorm.Open(mysqlGorm.Open(connectionString))
 	if err != nil {
 		return nil, err
 	}
-	
 	return db, nil
 }
