870af5221376a8e5fb8bce58ce33c6ef31d170fc
kevin-kline <kevin.kline@merico.dev>
2022-02-01 12:15:28 -0400

    feat: wip continued, added db connection

--- e2e/github_repo_test.go
+++ e2e/github_repo_test.go
@@ -1,12 +1,40 @@
 package e2e
 
-import "testing"
+import (
+	"fmt"
+	"log"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 // This test should only run once main_test is complete and ready
 
-func TestSum(t *testing.T) {
-	total := 5
-	if total != 10 {
-		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+type Repo struct {
+	ID string
+}
+
+func TestGitHubRepos(t *testing.T) {
+	var repos []Repo
+	db, err := InitializeDb()
+	assert.Nil(t, err)
+	if err != nil {
+		log.Fatal(err)
 	}
+	rows, err := db.Query("SELECT * FROM github_repos")
+	if err != nil {
+		fmt.Println("KEVIN >>> err", err)
+	}
+	assert.Nil(t, err)
+	defer rows.Close()
+	for rows.Next() {
+		var repo Repo
+		// This fails because we need all the column names
+		// It only passes when there are no repos in the DB.
+		if err := rows.Scan(&repo.ID); err != nil {
+			panic(err)
+		}
+		repos = append(repos, repo)
+	}
+	assert.Equal(t, len(repos) == 0, true)
 }
