5a3ea20d42f833de4e3c5f210d36ece867fdb974
kevin-kline <kevin.kline@merico.dev>
2022-02-01 13:38:13 -0400

    fix: get mr iid

--- e2e/gitlab_mr_test.go
+++ e2e/gitlab_mr_test.go
@@ -10,38 +10,30 @@ import (
 
 // This test should only run once main_test is complete and ready
 
-// type Mr struct {
-// 	iid int
-// }
+type Mr struct {
+	iid int
+}
 
 func TestGitLabMrs(t *testing.T) {
-	// var mrs []Mr
-	// var mr Mr
-	db, err := InitializeGormDb()
+	var mrs []Mr
+	db, err := InitializeDb()
 	assert.Nil(t, err)
 	if err != nil {
 		log.Fatal(err)
 	}
-	sqlCommand := "SELECT * FROM gitlab_merge_requests"
-	tx := db.Debug().Exec(sqlCommand)
-	fmt.Println("KEVIN >>> tx.RowsAffected", tx.RowsAffected)
-	fmt.Println("KEVIN >>> tx.Error", tx.Error)
-	assert.Equal(t, tx.RowsAffected > 0, true)
-	// assert.Nil(t, err)
-	// defer rows.Close()
-
-	// colTypes, _ := rows.ColumnTypes()
-	// for _, col := range colTypes {
-
-	// 	fmt.Println("KEVIN >>> col Name: ", col.Name())
-	// 	fmt.Println("KEVIN >>> col Type: ", col.DatabaseTypeName())
-	// }
-	// for rows.Next() {
-	// 	var mr Mr
-	// 	if err := rows.Scan(colTypes); err != nil {
-	// 		panic(err)
-	// 	}
-	// 	mrs = append(mrs, mr)
-	// }
-	// assert.Equal(t, len(mrs) > 0, true)
+	sqlCommand := "SELECT iid FROM gitlab_merge_requests"
+	rows, err := db.Query(sqlCommand)
+	if err != nil {
+		fmt.Println("KEVIN >>> err", err)
+	}
+	assert.Nil(t, err)
+	defer rows.Close()
+	for rows.Next() {
+		var mr Mr
+		if err := rows.Scan(&mr.iid); err != nil {
+			panic(err)
+		}
+		mrs = append(mrs, mr)
+	}
+	assert.Equal(t, len(mrs) > 0, true)
 }
