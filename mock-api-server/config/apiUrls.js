c5477aad0f44542340634e0321fd316af470477d
Jonathan O'Donnell <joncodo@meri.co>
2022-02-03 11:53:23 -0400

    feat: remove mock server

--- mock-api-server/config/apiUrls.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  github: {repo: 'lake', owner: 'merico-dev', baseUrl: 'https://api.github.com'}
-}
-
-module.exports = [
-  { name: 'github-commits', url: `${config.github.baseUrl}/repos/${config.github.owner}/${config.github.repo}/commits`, paging: true },
-  { name: 'github-issues', url: `${config.github.baseUrl}/repos/${config.github.owner}/${config.github.repo}/issues`, paging: true }
-]
