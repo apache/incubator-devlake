8360ee59b56b75f1b7a77217c65f52eeebb393ac
Nddtfjiang <jzc001@qq.com>
2022-03-01 02:00:50 +0000

    feat: add github response field
    Add github response field for /plugins/github/sources/1
    Delete structure GitHubConfig and merge structure GithubResponse and GithubConfig
    Add method to write struct directly to configuration
    
    Nddtfjiang <zhicheng.jiang@merico.dev>

--- config/config.go
+++ config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"reflect"
+
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
@@ -10,6 +12,45 @@ var v *viper.Viper = nil
 
 func GetConfig() *viper.Viper {
 	return v
+}
+
+// Set a struct for viper
+// if IgnoreEmpty is set to true, empty string will be ignored
+func SetStruct(S interface{}, IgnoreEmpty bool) {
+	tf := reflect.TypeOf(S)
+	vf := reflect.ValueOf(S)
+
+	for i := 0; i < tf.NumField(); i++ {
+		tfield := tf.Field(i)
+		vfield := vf.Field(i)
+
+		// Check if the first letter is uppercase (indicates a public element, accessible)
+		ascii := rune(tfield.Name[0])
+		if int(ascii) < int('A') || int(ascii) > int('Z') {
+			continue
+		}
+
+		// View their tags in order to filter out members who don't have a valid tag set
+		ft := tfield.Tag.Get("json")
+		if ft == "" {
+			ft = tfield.Tag.Get("mapstructure")
+		}
+		if ft == "" {
+			continue
+		}
+
+		if IgnoreEmpty {
+			switch tfield.Type.Kind() {
+			case reflect.String:
+				if vfield.String() == "" {
+					continue
+				}
+			}
+		}
+
+		v.Set(ft, vfield.Interface())
+	}
+	v.WriteConfig()
 }
 
 // Set default value for no .env or .env not set it
--- config/config_test.go
+++ config/config_test.go
@@ -6,6 +6,14 @@ import (
 	"github.com/magiconair/properties/assert"
 )
 
+type TestStruct struct {
+	F1 string  `json:"TEST_F1"`
+	F2 int     `mapstructure:"TEST_F2"`
+	F3 float64 `json:"TEST_F3" mapstructure:"TEST_F3"`
+	F4 string  `json:"TEST_F4"`
+	f5 string  `json:"TEST_F5"`
+}
+
 func TestReadAndWriteToConfig(t *testing.T) {
 	v := GetConfig()
 	currentDbUrl := v.GetString("DB_URL")
@@ -20,4 +28,30 @@ func TestReadAndWriteToConfig(t *testing.T) {
 	v.Set("DB_URL", currentDbUrl)
 	err = v.WriteConfig()
 	assert.Equal(t, err == nil, true)
+}
+
+func TestSetStruct(t *testing.T) {
+	ts := TestStruct{
+		F1: "123",
+		F2: 123,
+		F3: 1.23,
+		F4: "Test",
+		f5: "No Use",
+	}
+
+	v := GetConfig()
+	SetStruct(ts, false)
+	v1 := v.GetString("TEST_F1")
+	assert.Equal(t, v1, "123")
+	v2 := v.GetInt("TEST_F2")
+	assert.Equal(t, v2, 123)
+	v3 := v.GetFloat64("TEST_F3")
+	assert.Equal(t, v3, 1.23)
+	v4 := v.GetString("TEST_F4")
+	assert.Equal(t, v4, "Test")
+
+	ts.F4 = ""
+	SetStruct(ts, true)
+	v1 = v.GetString("TEST_F4")
+	assert.Equal(t, v1, "Test")
 }
