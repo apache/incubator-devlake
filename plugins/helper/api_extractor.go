aedef5ef4cca06f8edbfc0049226906ebd4ac166
Klesh Wong <zhenmian.huang@merico.dev>
2022-03-08 00:15:56 +0800

    feat: api extractor implementation

--- plugins/helper/api_extractor.go
+++ plugins/helper/api_extractor.go
@@ -1,18 +1,18 @@
 package helper
 
 import (
-	"encoding/json"
+	"fmt"
+	"reflect"
 
 	"github.com/merico-dev/lake/plugins/core"
+	"gorm.io/gorm"
 )
 
 // Accept raw json body and params, return list of entities that need to be stored
-type RawDataExtractor func(body json.RawMessage, params json.RawMessage) ([]interface{}, error)
+type RawDataExtractor func(row *RawData) ([]interface{}, error)
 
 type ApiExtractorArgs struct {
-	Ctx       core.TaskContext
-	Table     string `comment:"Raw data table name"`
-	Url       string `comment:"Raw data table name"`
+	RawDataSubTaskArgs
 	Params    interface{}
 	RowData   interface{}
 	Extract   RawDataExtractor
@@ -20,15 +20,131 @@ type ApiExtractorArgs struct {
 }
 
 type ApiExtractor struct {
+	*RawDataSubTask
 	args *ApiExtractorArgs
 }
 
 func NewApiExtractor(args ApiExtractorArgs) (*ApiExtractor, error) {
-	return &ApiExtractor{args: &args}, nil
+	// process args
+	rawDataSubTask, err := newRawDataSubTask(args.RawDataSubTaskArgs)
+	if err != nil {
+		return nil, err
+	}
+	if args.BatchSize == 0 {
+		args.BatchSize = 500
+	}
+	return &ApiExtractor{
+		RawDataSubTask: rawDataSubTask,
+		args:           &args,
+	}, nil
 }
 
 func (extractor *ApiExtractor) Execute() error {
+	// load data from database
+	db := extractor.args.Ctx.GetDb()
+	cursor, err := db.Table(extractor.table).Order("id ASC").Rows()
+	if err != nil {
+		return err
+	}
+	defer cursor.Close()
+	row := &RawData{}
+
+	// cache for batch insertion
+	cache := make(map[reflect.Type]*subCache)
+
+	// iterate all rows
+	for cursor.Next() {
+		err = db.ScanRows(cursor, row)
+		if err != nil {
+			return err
+		}
+
+		results, err := extractor.args.Extract(row)
+		if err != nil {
+			return err
+		}
+
+		for _, result := range results {
+			resultType := reflect.TypeOf(result)
+			// get the cache for the specific type
+			resultCache := cache[resultType]
+			// create one if not exists
+			if resultCache == nil {
+				resultCache, err = newSubCache(extractor.args.Ctx.GetDb(), resultType, extractor.args.BatchSize)
+				if err != nil {
+					return err
+				}
+				cache[resultType] = resultCache
+			}
+			// records get saved into db when slots were max outed
+			err = resultCache.Add(result)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	// close all caches so all rest records get saved into db as well
+	for _, resultCache := range cache {
+		resultCache.Close()
+	}
+
 	return nil
 }
 
 var _ core.SubTask = (*ApiExtractor)(nil)
+
+type subCache struct {
+	slotType reflect.Type
+	// slots can not be []interface{}, because gorm wouldn't take it
+	// I'm guessing the reason is the type information lost when converted to interface{}
+	slots   reflect.Value
+	db      *gorm.DB
+	current int
+	size    int
+}
+
+func newSubCache(db *gorm.DB, slotType reflect.Type, size int) (*subCache, error) {
+	return &subCache{
+		slotType: slotType,
+		slots:    reflect.MakeSlice(reflect.SliceOf(slotType), size, size),
+		db:       db,
+		size:     size,
+	}, nil
+}
+
+func (c *subCache) Add(slot interface{}) error {
+	// type checking
+	if reflect.TypeOf(slot) != c.slotType {
+		return fmt.Errorf("sub cache type mismatched")
+	}
+	if reflect.ValueOf(slot).Kind() != reflect.Ptr {
+		return fmt.Errorf("slot is not a pointer")
+	}
+	//return c.db.Create(slot).Error
+
+	// push into slot
+	c.slots.Index(c.current).Set(reflect.ValueOf(slot))
+	c.current++
+	// flush out into database if max outed
+	if c.current == c.size {
+		return c.Flush()
+	}
+	return nil
+}
+
+func (c *subCache) Flush() error {
+	err := c.db.CreateInBatches(c.slots.Slice(0, c.current).Interface(), c.current).Error
+	c.current = 0
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *subCache) Close() error {
+	if c.current > 0 {
+		return c.Flush()
+	}
+	return nil
+}
--- plugins/helper/api_rawdata.go
+++ plugins/helper/api_rawdata.go
@@ -1,8 +1,11 @@
 package helper
 
 import (
+	"encoding/json"
+	"fmt"
 	"time"
 
+	"github.com/merico-dev/lake/plugins/core"
 	"gorm.io/datatypes"
 )
 
@@ -18,10 +21,48 @@ type RawData struct {
 // embedded fields for tool layer tables
 type ExtractedRawData struct {
 	// can be used for flushing outdated records from table
-	RawDataParams string `gorm:"column:_raw_data_params,type:varchar(255);index"`
+	RawDataParams string `gorm:"column:_raw_data_params;type:varchar(255);index"`
 	RawDataTable  uint64 `gorm:"column:_raw_data_table"`
 	// can be used for debugging
 	RawDataId uint64 `gorm:"column:_raw_data_id"`
 	// we can store record index into this field, which is helpful for debugging
 	RawDataRemark string `gorm:"column:_raw_data_remark"`
+}
+
+type RawDataSubTaskArgs struct {
+	Ctx    core.SubTaskContext
+	Table  string      `comment:"Raw data table name"`
+	Params interface{} `comment:"To identify a set of records with same UrlTemplate, i.e. {SourceId, BoardId} for jira entities"`
+}
+
+// Common features for raw data sub tasks
+type RawDataSubTask struct {
+	args   *RawDataSubTaskArgs
+	table  string
+	params string
+}
+
+func newRawDataSubTask(args RawDataSubTaskArgs) (*RawDataSubTask, error) {
+	if args.Ctx == nil {
+		return nil, fmt.Errorf("Ctx is required for RawDataSubTask")
+	}
+	if args.Table == "" {
+		return nil, fmt.Errorf("Table is required for RawDataSubTask")
+	}
+	paramsString := ""
+	if args.Params == nil {
+		args.Ctx.GetLogger().Warn("Missing `Params` for raw data subtask %s", args.Ctx.GetName())
+	} else {
+		// TODO: maybe sort it to make it consisitence
+		paramsBytes, err := json.Marshal(args.Params)
+		if err != nil {
+			return nil, err
+		}
+		paramsString = string(paramsBytes)
+	}
+	return &RawDataSubTask{
+		args:   &args,
+		table:  fmt.Sprintf("_raw_%s", args.Table),
+		params: paramsString,
+	}, nil
 }
