8868680e1369bfa6961592299c43338d3f6d7d5d
Klesh Wong <zhenmian.huang@merico.dev>
2022-03-08 14:23:39 +0800

    feat: extract Batch Insertion into 2 new helper

--- plugins/jira/models/jira_board_issue.go
+++ plugins/jira/models/jira_board_issue.go
@@ -6,5 +6,5 @@ type JiraBoardIssue struct {
 	SourceId uint64 `gorm:"primaryKey"`
 	BoardId  uint64 `gorm:"primaryKey"`
 	IssueId  uint64 `gorm:"primaryKey"`
-	helper.ExtractedRawData
+	helper.RawDataOrigin
 }
