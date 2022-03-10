7940d97a96987b226b47e3f9f63329cf9b4936da
warren830 <yingchu.chen@merico.dev>
2022-03-10 22:01:57 +0800

    refactor(github): refactor pr for raw json

---
+++ plugins/github/tasks/github_api_pull_requests_extractor.go
@@ -0,0 +1,88 @@
+package tasks
+
+import (
+	"encoding/json"
+
+	"github.com/merico-dev/lake/plugins/core"
+	"github.com/merico-dev/lake/plugins/github/models"
+	"github.com/merico-dev/lake/plugins/helper"
+)
+
+var _ core.SubTaskEntryPoint = ExtractApiPullRequests
+
+func ExtractApiPullRequests(taskCtx core.SubTaskContext) error {
+	data := taskCtx.GetData().(*GithubTaskData)
+
+	extractor, err := helper.NewApiExtractor(helper.ApiExtractorArgs{
+		RawDataSubTaskArgs: helper.RawDataSubTaskArgs{
+			Ctx: taskCtx,
+			/*
+				This struct will be JSONEncoded and stored into database along with raw data itself, to identity minimal
+				set of data to be process, for example, we process JiraIssues by Board
+			*/
+			Params: GithubApiParams{
+				Owner: data.Options.Owner,
+				Repo:  data.Options.Repo,
+			},
+			/*
+				Table store raw data
+			*/
+			Table: RAW_PULL_REQUEST_TABLE,
+		},
+		Extract: func(row *helper.RawData) ([]interface{}, error) {
+			body := &ApiPullRequestResponse{}
+			err := json.Unmarshal(row.Data, body)
+			if err != nil {
+				return nil, err
+			}
+			// need to extract 3 kinds of entities here
+			results := make([]interface{}, 0, len(*body)*2)
+			for _, apiPullRequest := range *body {
+				if apiPullRequest.GithubId == 0 {
+					return nil, nil
+				}
+				//If this is a pr, ignore
+
+				githubPr, err := convertGithubPullRequest(&apiPullRequest, data.Repo.GithubId)
+				if err != nil {
+					return nil, err
+				}
+				results = append(results, githubPr)
+				for _, label := range apiPullRequest.Labels {
+					results = append(results, &models.GithubIssueLabel{
+						IssueId:   githubPr.GithubId,
+						LabelName: label.Name,
+					})
+
+				}
+			}
+			return results, nil
+		},
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return extractor.Execute()
+}
+func convertGithubPullRequest(pull *GithubApiPullRequest, repoId int) (*models.GithubPullRequest, error) {
+	githubPull := &models.GithubPullRequest{
+		GithubId:        pull.GithubId,
+		RepoId:          repoId,
+		Number:          pull.Number,
+		State:           pull.State,
+		Title:           pull.Title,
+		GithubCreatedAt: pull.GithubCreatedAt.ToTime(),
+		GithubUpdatedAt: pull.GithubUpdatedAt.ToTime(),
+		ClosedAt:        core.Iso8601TimeToTime(pull.ClosedAt),
+		MergedAt:        core.Iso8601TimeToTime(pull.MergedAt),
+		MergeCommitSha:  pull.MergeCommitSha,
+		Body:            pull.Body,
+		BaseRef:         pull.Base.Ref,
+		BaseCommitSha:   pull.Base.Sha,
+		HeadRef:         pull.Head.Ref,
+		HeadCommitSha:   pull.Head.Sha,
+	}
+	return githubPull, nil
+}
