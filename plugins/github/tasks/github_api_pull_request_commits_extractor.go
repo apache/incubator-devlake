1e14df97524e98b62ae8f7b7d69eb62b32105693
warren830 <yingchu.chen@merico.dev>
2022-03-13 17:17:30 +0800

    refactor(github): refactor pr_commits
    also fixed pr_enricher

---
+++ plugins/github/tasks/github_api_pull_request_commits_extractor.go
@@ -0,0 +1,127 @@
+package tasks
+
+import (
+	"encoding/json"
+	"github.com/merico-dev/lake/plugins/github/models"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/merico-dev/lake/plugins/core"
+	"github.com/merico-dev/lake/plugins/helper"
+)
+
+var _ core.SubTaskEntryPoint = ExtractApiPullRequestCommits
+
+type ApiPullRequestCommitResponse []PrCommitsResponse
+type PrCommitsResponse struct {
+	Sha    string `json:"sha"`
+	Commit PullRequestCommit
+	Url    string
+}
+
+type PullRequestCommit struct {
+	Author struct {
+		Id    int
+		Name  string
+		Email string
+		Date  core.Iso8601Time
+	}
+	Committer struct {
+		Name  string
+		Email string
+		Date  core.Iso8601Time
+	}
+	Message string
+}
+
+func ExtractApiPullRequestCommits(taskCtx core.SubTaskContext) error {
+	data := taskCtx.GetData().(*GithubTaskData)
+
+	pullNumberRegex := regexp.MustCompile(`.*\/pulls\/(\d+)\/commits\?.*`)
+
+	extractor, err := helper.NewApiExtractor(helper.ApiExtractorArgs{
+		RawDataSubTaskArgs: helper.RawDataSubTaskArgs{
+			Ctx: taskCtx,
+			/*
+				This struct will be JSONEncoded and stored into database along with raw data itself, to identity minimal
+				set of data to be process, for example, we process JiraIssues by Board
+			*/
+			Params: GithubApiParams{
+				Owner: data.Options.Owner,
+				Repo:  data.Options.Repo,
+			},
+			/*
+				Table store raw data
+			*/
+			Table: RAW_PULL_REQUEST_COMMIT_TABLE,
+		},
+		Extract: func(row *helper.RawData) ([]interface{}, error) {
+			body := &ApiPullRequestCommitResponse{}
+			if strings.HasPrefix(string(row.Data), "{\"message\": \"Not Found\"") {
+				return nil, nil
+			}
+			err := json.Unmarshal(row.Data, body)
+			if err != nil {
+				return nil, err
+			}
+			// need to extract 2 kinds of entities here
+			results := make([]interface{}, 0, len(*body)*2)
+			for _, apiPullRequestCommit := range *body {
+				var pullNumber int
+				groups := pullNumberRegex.FindStringSubmatch(row.Url)
+				if len(groups) > 0 {
+					pullNumber, err = strconv.Atoi(groups[1])
+					if err != nil {
+						return nil, err
+					}
+				}
+
+				pull := models.GithubPullRequest{}
+				err = taskCtx.GetDb().Model(&pull).Where("number = ? and repo_id = ?", pullNumber, data.Repo.GithubId).
+					Limit(1).Find(&pull).Error
+				if err != nil {
+					return nil, err
+				}
+
+				githubCommit, err := convertPullRequestCommit(&apiPullRequestCommit)
+				if err != nil {
+					return nil, err
+				}
+				results = append(results, githubCommit)
+
+				githubPullRequestCommit := &models.GithubPullRequestCommit{
+					CommitSha:     apiPullRequestCommit.Sha,
+					PullRequestId: pull.GithubId,
+				}
+				if err != nil {
+					return nil, err
+				}
+				results = append(results, githubPullRequestCommit)
+			}
+			return results, nil
+		},
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return extractor.Execute()
+}
+
+func convertPullRequestCommit(prCommit *PrCommitsResponse) (*models.GithubCommit, error) {
+	githubCommit := &models.GithubCommit{
+		Sha:            prCommit.Sha,
+		Message:        prCommit.Commit.Message,
+		AuthorId:       prCommit.Commit.Author.Id,
+		AuthorName:     prCommit.Commit.Author.Name,
+		AuthorEmail:    prCommit.Commit.Author.Email,
+		AuthoredDate:   prCommit.Commit.Author.Date.ToTime(),
+		CommitterName:  prCommit.Commit.Committer.Name,
+		CommitterEmail: prCommit.Commit.Committer.Email,
+		CommittedDate:  prCommit.Commit.Committer.Date.ToTime(),
+		Url:            prCommit.Url,
+	}
+	return githubCommit, nil
+}
