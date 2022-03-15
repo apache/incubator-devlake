a0a317d6f9589c0af9f55d3d9c583b49d6a47625
Klesh Wong <zhenmian.huang@merico.dev>
2022-03-15 18:11:31 +0800

    feat: refactor ae to adopt ResponseParser

--- plugins/ae/models/ae_commit.go
+++ plugins/ae/models/ae_commit.go
@@ -1,9 +1,13 @@
 package models
 
+import "github.com/merico-dev/lake/plugins/helper"
+
 type AECommit struct {
 	HexSha      string `gorm:"primaryKey"`
 	AnalysisId  string
 	AuthorEmail string
 	DevEq       int
 	AEProjectId int
+
+	helper.RawDataOrigin
 }
