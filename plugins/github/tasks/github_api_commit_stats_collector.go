e07416f0e82540602ee6db0e5a70255cb30d34ac
Yingchu Chen <yingchu.chen@merico.dev>
2022-03-16 17:12:01 +0800

    refactor(github): use repoId to scope

--- plugins/github/tasks/github_api_commit_stats_collector.go
+++ plugins/github/tasks/github_api_commit_stats_collector.go
@@ -23,8 +23,7 @@ func CollectApiCommitStats(taskCtx core.SubTaskContext) error {
 
 	var latestUpdated models.GithubCommitStat
 	err := db.Model(&latestUpdated).Joins("left join github_repo_commits on github_commit_stats.sha = github_repo_commits.commit_sha").
-		Joins("left join github_repos on github_repo_commits.repo_id = github_repos.github_id").
-		Where("github_repos.`name` = ? and github_repos.owner_login = ?", data.Options.Repo, data.Options.Owner).
+		Where("github_repo_commits.`repo_id` = ?", data.Repo.GithubId).
 		Order("committed_date DESC").Limit(1).Find(&latestUpdated).Error
 	if err != nil {
 		return fmt.Errorf("failed to get latest github commit record: %w", err)
