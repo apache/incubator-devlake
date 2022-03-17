a50179488340f20cd1af58b600f9faee8300ae14
Julien Chinapen <julien.chinapen@merico.dev>
2022-03-17 11:18:31 -0400

    chore: cleanup cypress support scripts

--- config-ui/cypress/support/commands.js
+++ config-ui/cypress/support/commands.js
@@ -1,10 +1,8 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
+// ***********************************************************
+// CONFIG-UI: Cypress Commands
+// ***********************************************************
 //
-// For more comprehensive examples of custom
-// commands please read more here:
+// Docs:
 // https://on.cypress.io/custom-commands
 // ***********************************************
 //
