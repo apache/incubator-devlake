e9c755fc9564a1adc1fc0d3598a064fe6572b4c9
Yingchu Chen <yingchu.chen@merico.dev>
2022-03-19 12:28:14 +0800

    refactor(jenkins): done

--- plugins/jenkins/tasks/jenkins_api_client.go
+++ plugins/jenkins/tasks/jenkins_api_client.go
@@ -1,17 +1,51 @@
 package tasks
 
 import (
+	"context"
+	"fmt"
+	"github.com/merico-dev/lake/plugins/core"
+	"github.com/merico-dev/lake/utils"
 	"net/http"
-
-	"github.com/bndr/gojenkins"
+	"time"
 )
 
 type JenkinsApiClient struct {
-	jenkins *gojenkins.Jenkins
+	core.ApiClient
 }
 
-func CreateApiClient(client *http.Client, base string, auth ...interface{}) *JenkinsApiClient {
-	return &JenkinsApiClient{
-		jenkins: gojenkins.CreateJenkins(client, base, auth...),
+func NewJenkinsApiClient(endpoint string, username string, password string, proxy string, ctx context.Context, scheduler *utils.WorkerScheduler, logger core.Logger) *JenkinsApiClient {
+	jenkinsApiClient := &JenkinsApiClient{}
+	encodedToken := utils.GetEncodedToken(username, password)
+
+	jenkinsApiClient.Setup(
+		endpoint,
+		map[string]string{
+			"Authorization": fmt.Sprintf("Basic %v", encodedToken),
+		},
+		50*time.Second,
+		3,
+		scheduler,
+	)
+
+	jenkinsApiClient.SetAfterFunction(func(res *http.Response) error {
+		if res.StatusCode == http.StatusUnauthorized {
+			return fmt.Errorf("authentication failed, please check your Basic Auth Token")
+		}
+		return nil
+	})
+	if ctx != nil {
+		jenkinsApiClient.SetContext(ctx)
 	}
+	if proxy != "" {
+		err := jenkinsApiClient.SetProxy(proxy)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	jenkinsApiClient.SetLogger(logger)
+	return jenkinsApiClient
 }
+
+type JenkinsPaginationHandler func(res *http.Response) error
+type JenkinsSearchPaginationHandler func(res *http.Response) (int, error)
