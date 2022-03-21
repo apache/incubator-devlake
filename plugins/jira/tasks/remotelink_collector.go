9e39bd5a671ccd2b95dc4a1a4de601d40c2f84c2
zhangliang <liang.zhang@merico.dev>
2022-03-21 11:22:11 +0800

    refactor: leverage iterator

--- plugins/jira/tasks/remotelink_collector.go
+++ plugins/jira/tasks/remotelink_collector.go
@@ -2,17 +2,24 @@ package tasks
 
 import (
 	"encoding/json"
-	"fmt"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+	"time"
+
 	"github.com/merico-dev/lake/plugins/core"
 	"github.com/merico-dev/lake/plugins/helper"
 	"github.com/merico-dev/lake/plugins/jira/models"
-	"io/ioutil"
-	"net/http"
 )
 
 const RAW_REMOTELINK_TABLE = "jira_api_remotelinks"
 
 var _ core.SubTaskEntryPoint = CollectApiRemotelinks
+
+type RemotelinkUpdate struct {
+	IssueId   uint64
+	UpdatedAt time.Time
+}
 
 func CollectApiRemotelinks(taskCtx core.SubTaskContext) error {
 	data := taskCtx.GetData().(*JiraTaskData)
@@ -28,7 +35,7 @@ func CollectApiRemotelinks(taskCtx core.SubTaskContext) error {
 		set its `remotelink_updated` to `updated` at the end.
 	*/
 	cursor, err := db.Model(jiraIssue).
-		Select("jira_issues.issue_id", "jira_issues.updated").
+		Select("jira_issues.issue_id", "NOW() AS updated_at").
 		Joins(`LEFT JOIN jira_board_issues ON (
 			jira_board_issues.source_id = jira_issues.source_id AND
 			jira_board_issues.issue_id = jira_issues.issue_id
@@ -48,50 +55,42 @@ func CollectApiRemotelinks(taskCtx core.SubTaskContext) error {
 	}
 	defer cursor.Close()
 
-	// iterate all rows
-	for cursor.Next() {
-		err = db.ScanRows(cursor, jiraIssue)
-		if err != nil {
-			return err
-		}
-		issueId := jiraIssue.IssueId
-		updated := jiraIssue.Updated
+	// smaller struct can reduce memory footprint, we should try to avoid using big struct
+	iterator, err := helper.NewCursorIterator(db, cursor, reflect.TypeOf(RemotelinkUpdate{}))
+	if err != nil {
+		return err
+	}
 
-		collector, err := helper.NewApiCollector(helper.ApiCollectorArgs{
-			RawDataSubTaskArgs: helper.RawDataSubTaskArgs{
-				Ctx: taskCtx,
-				Params: JiraApiParams{
-					SourceId: data.Source.ID,
-					BoardId:  data.Options.BoardId,
-				},
-				Table: RAW_REMOTELINK_TABLE,
+	collector, err := helper.NewApiCollector(helper.ApiCollectorArgs{
+		RawDataSubTaskArgs: helper.RawDataSubTaskArgs{
+			Ctx: taskCtx,
+			Params: JiraApiParams{
+				SourceId: data.Source.ID,
+				BoardId:  data.Options.BoardId,
 			},
-			ApiClient:   data.ApiClient,
-			UrlTemplate: fmt.Sprintf("api/2/issue/%d/remotelink", issueId),
-			ResponseParser: func(res *http.Response) ([]json.RawMessage, error) {
-				if res.StatusCode == http.StatusNotFound {
-					return nil, nil
-				}
-				blob, err := ioutil.ReadAll(res.Body)
-				if err != nil {
-					return nil, err
-				}
-				res.Body.Close()
-				return []json.RawMessage{blob}, nil
-			},
-		})
-		if err != nil {
-			return err
-		}
-		err = collector.Execute()
-		if err != nil {
-			return err
-		}
-		issue := &models.JiraIssue{SourceId: data.Source.ID, IssueId: issueId}
-		err = db.Model(issue).Update("remotelink_updated", updated).Error
-		if err != nil {
-			return err
-		}
+			Table: RAW_REMOTELINK_TABLE,
+		},
+		ApiClient:   data.ApiClient,
+		Input:       iterator,
+		UrlTemplate: "api/2/issue/{{ .Input.IssueId }}/remotelink",
+		ResponseParser: func(res *http.Response) ([]json.RawMessage, error) {
+			if res.StatusCode == http.StatusNotFound {
+				return nil, nil
+			}
+			blob, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				return nil, err
+			}
+			res.Body.Close()
+			return []json.RawMessage{blob}, nil
+		},
+	})
+	if err != nil {
+		return err
+	}
+	err = collector.Execute()
+	if err != nil {
+		return err
 	}
 	return nil
 }
