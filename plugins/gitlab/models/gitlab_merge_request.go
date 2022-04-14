430cafcf9a7953159e8d27241cf720b20d786b0d
Yingchu Chen <yingchu.chen@merico.dev>
2022-04-14 14:00:22 +0800

    fix(gitlab): modify mr extractor and converter
    close #1573

--- plugins/gitlab/models/gitlab_merge_request.go
+++ plugins/gitlab/models/gitlab_merge_request.go
@@ -7,21 +7,27 @@ import (
 )
 
 type GitlabMergeRequest struct {
-	GitlabId         int    `gorm:"primaryKey"`
-	Iid              int    `gorm:"index"`
-	ProjectId        int    `gorm:"index"`
+	GitlabId         int `gorm:"primaryKey"`
+	Iid              int `gorm:"index"`
+	ProjectId        int `gorm:"index"`
+	SourceProjectId  int
+	TargetProjectId  int
 	State            string `gorm:"type:varchar(255)"`
 	Title            string
 	WebUrl           string `gorm:"type:varchar(255)"`
 	UserNotesCount   int
 	WorkInProgress   bool
 	SourceBranch     string `gorm:"type:varchar(255)"`
+	TargetBranch     string `gorm:"type:varchar(255)"`
+	MergeCommitSha   string `gorm:"type:varchar(255)"`
 	MergedAt         *time.Time
 	GitlabCreatedAt  time.Time
 	ClosedAt         *time.Time
 	MergedByUsername string `gorm:"type:varchar(255)"`
 	Description      string
-	AuthorUsername   string     `gorm:"type:varchar(255)"`
+	AuthorUsername   string `gorm:"type:varchar(255)"`
+	AuthorUserId     int
+	Component        string     `gorm:"type:varchar(255)"`
 	FirstCommentTime *time.Time `gorm:"comment:Time when the first comment occurred"`
 	ReviewRounds     int        `gorm:"comment:How many rounds of review this MR went through"`
 	common.NoPKModel
--- plugins/gitlab/models/gitlab_user.go
+++ plugins/gitlab/models/gitlab_user.go
@@ -3,12 +3,12 @@ package models
 import "github.com/merico-dev/lake/models/common"
 
 type GitlabUser struct {
+	Id    int
 	Email string `gorm:"primaryKey;type:varchar(255)"`
 	Name  string `gorm:"type:varchar(255)"`
 	common.NoPKModel
 }
 
-func (GitlabUser) TableName() string{
+func (GitlabUser) TableName() string {
 	return "_tool_gitlab_users"
 }
-
