2e76684ab5820f04bacbd73796215cf796c9b6a3
Julien Chinapen <julien.chinapen@merico.dev>
2022-04-26 11:47:28 -0400

    fix: hide discord and use slack as primary navlink

--- config-ui/src/components/Nav.jsx
+++ config-ui/src/components/Nav.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import {
   Alignment,
+  Position,
+  Popover,
   Navbar,
   Icon,
 } from '@blueprintjs/core'
 import '@/styles/nav.scss'
-import { ReactComponent as DiscordIcon } from '@/images/discord.svg'
+// import { ReactComponent as DiscordIcon } from '@/images/discord.svg'
+import { ReactComponent as SlackIcon } from '@/images/slack-mark-monochrome-black.svg'
+import { ReactComponent as SlackLogo } from '@/images/slack-rgb.svg'
 
 const Nav = () => {
   return (
@@ -19,9 +23,31 @@ const Nav = () => {
           <Icon icon='envelope' size={16} />
         </a>
         <Navbar.Divider />
-        <a href='https://discord.com/invite/83rDG6ydVZ' rel='noreferrer' target='_blank' className='navIconLink'>
+        {/* DISCORD: !DISABLED! */}
+        {/* <a href='https://discord.com/invite/83rDG6ydVZ' rel='noreferrer' target='_blank' className='navIconLink'>
           <DiscordIcon className='discordIcon' width={16} height={16} />
-        </a>
+        </a> */}
+        {/* SLACK: ENABLED (Primary) */}
+        <Popover position={Position.LEFT}>
+          <SlackIcon className='slackIcon' width={16} height={16} />
+          <>
+            <div style={{ maxWidth: '200px', padding: '10px', fontSize: '11px' }}>
+              <SlackLogo width={131} height={49} style={{ display: 'block', margin: '0 auto' }} />
+              <p>
+                Want to interact with the <strong>Merico Community</strong>? Join us on our Slack Channel.<br />
+                <a
+                  href='https://join.slack.com/t/devlake-io/shared_invite/zt-17b6vuvps-x98pqseoUagM7EAmKC82xQ'
+                  rel='noreferrer'
+                  target='_blank'
+                  className='bp3-button bp3-intent-warning bp3-elevation-1 bp3-small'
+                  style={{ marginTop: '10px' }}
+                >
+                  Message us on&nbsp;<strong>Slack</strong>
+                </a>
+              </p>
+            </div>
+          </>
+        </Popover>
       </Navbar.Group>
     </Navbar>
   )
