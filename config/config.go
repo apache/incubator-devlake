17f22fbb3cda43b18303ff442d8d409461615e95
linyh <yanghui@meri.co>
2022-05-13 21:17:43 +0800

    feat: save .env as original format
    #1863

--- config/config.go
+++ config/config.go
@@ -1,8 +1,14 @@
 package config
 
 import (
+	"fmt"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/afero"
 	"github.com/spf13/viper"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
 )
 
 // Lowcase V for private this. You can use it by call GetConfig.
@@ -20,6 +26,85 @@ func setDefaultValue() {
 	v.SetDefault("TEMPORAL_TASK_QUEUE", "DEVLAKE_TASK_QUEUE")
 	v.SetDefault("GITLAB_ENDPOINT", "https://gitlab.com/api/v4/")
 	v.SetDefault("GITHUB_ENDPOINT", "https://api.github.com/")
+}
+
+// replaceNewEnvItemInOldContent replace old config to new config in env file content
+func replaceNewEnvItemInOldContent(v *viper.Viper, envFileContent string) (error, string) {
+	// prepare reg exp
+	encodeEnvNameReg := regexp.MustCompile(`[^a-zA-Z0-9]`)
+	if encodeEnvNameReg == nil {
+		return fmt.Errorf("encodeEnvNameReg err"), ``
+	}
+
+	for _, key := range v.AllKeys() {
+		envName := strings.ToUpper(key)
+		val := v.Get(envName)
+		encodeEnvName := encodeEnvNameReg.ReplaceAllStringFunc(envName, func(s string) string {
+			return fmt.Sprintf(`\%v`, s)
+		})
+		envItemReg := regexp.MustCompile(fmt.Sprintf(`(?im)^\s*%v\s*\=.*$`, encodeEnvName))
+		if envItemReg == nil {
+			return fmt.Errorf("regexp err"), ``
+		}
+		envFileContent = envItemReg.ReplaceAllStringFunc(envFileContent, func(s string) string {
+			switch ret := val.(type) {
+			case string:
+				ret = strings.Replace(ret, `\`, `\\`, -1)
+				ret = strings.Replace(ret, `=`, `\=`, -1)
+				ret = strings.Replace(ret, `'`, `\'`, -1)
+				ret = strings.Replace(ret, `"`, `\"`, -1)
+				return fmt.Sprintf(`%v="%v"`, envName, ret)
+			default:
+				return fmt.Sprintf(`%v="%v"`, envName, ret)
+			}
+		})
+	}
+	return nil, envFileContent
+}
+
+// WriteConfig save viper to .env file
+func WriteConfig(v *viper.Viper) error {
+	return WriteConfigAs(v, `.env`)
+}
+
+// WriteConfigAs save viper to custom filename
+func WriteConfigAs(v *viper.Viper, filename string) error {
+	aferoFile := afero.NewOsFs()
+	fmt.Println("Attempting to write configuration to .env file.")
+	var configType string
+
+	ext := filepath.Ext(filename)
+	if ext != "" {
+		configType = ext[1:]
+	}
+	if configType != "env" && configType != "dotenv" {
+		return v.WriteConfigAs(filename)
+	}
+
+	// FIXME viper just have setter and have no getter so create new configPermissions and file
+	flags := os.O_CREATE | os.O_TRUNC | os.O_WRONLY
+	configPermissions := os.FileMode(0644)
+	file, err := afero.ReadFile(aferoFile, filename)
+	if err != nil {
+		return err
+	}
+
+	envFileContent := string(file)
+	f, err := aferoFile.OpenFile(filename, flags, configPermissions)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	err, envFileContent = replaceNewEnvItemInOldContent(v, envFileContent)
+	if err != nil {
+		return err
+	}
+
+	if _, err := f.WriteString(envFileContent); err != nil {
+		return err
+	}
+	return f.Sync()
 }
 
 func init() {
--- plugins/jenkins/api/connection.go
+++ plugins/jenkins/api/connection.go
@@ -74,7 +74,7 @@ func PatchConnection(input *core.ApiResourceInput) (*core.ApiResourceOutput, err
 	if err != nil {
 		return nil, err
 	}
-	err = v.WriteConfig()
+	err = config.WriteConfig(v)
 	if err != nil {
 		return nil, err
 	}
