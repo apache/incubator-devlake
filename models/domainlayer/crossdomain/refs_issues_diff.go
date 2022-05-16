a1b44782f3304841ed1170fb57cfc2b2f028594d
abeizn <zikuan.an@merico.dev>
2022-05-16 17:42:36 +0800

    fix: refs_issues_diffs migrationscripts

--- models/domainlayer/crossdomain/refs_issues_diff.go
+++ models/domainlayer/crossdomain/refs_issues_diff.go
@@ -3,11 +3,11 @@ package crossdomain
 import "github.com/merico-dev/lake/models/common"
 
 type RefsIssuesDiffs struct {
-	NewRefId        string `gorm:"type:varchar(255)"`
-	OldRefId        string `gorm:"type:varchar(255)"`
+	NewRefId        string `gorm:"primaryKey;type:varchar(255)"`
+	OldRefId        string `gorm:"primaryKey;type:varchar(255)"`
 	NewRefCommitSha string `gorm:"type:varchar(40)"`
 	OldRefCommitSha string `gorm:"type:varchar(40)"`
 	IssueNumber     string `gorm:"type:varchar(255)"`
-	IssueId         string `gorm:";type:varchar(255)"`
+	IssueId         string `gorm:"primaryKey;type:varchar(255)"`
 	common.NoPKModel
 }
