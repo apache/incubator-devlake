bb853c587ff40a773c5759841927b262385f2846
xgdyp <zxyasrf@163.com>
2022-05-17 23:25:22 +0800

    add annotation in push.go

--- api/push/push.go
+++ api/push/push.go
@@ -17,7 +17,15 @@ import (
 		}
 	]
 */
-
+// @Summary POST /push/:tableName
+// @Description POST /push/:tableName
+// @Tags push
+// @Accept application/json
+// @Param tableName path string true "table name"
+// @Success 200  {object} gin.H "{"rowsAffected": rowsAffected}"
+// @Failure 400  {string} errcode.Error "Bad Request"
+// @Failure 500  {string} errcode.Error "Internel Error"
+// @Router /push/{tableName} [post]
 func Post(c *gin.Context) {
 	var err error
 	tableName := c.Param("tableName")
