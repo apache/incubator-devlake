15b2795acdf1604627d893702a33d8721484658e
xgdyp <zxyasrf@163.com>
2022-05-17 23:24:08 +0800

    add swagger annotation

--- api/domainlayer/repos.go
+++ api/domainlayer/repos.go
@@ -18,6 +18,14 @@ GET /repos
 	"count": 5
 }
 */
+// @Summary Get all repos from database
+// @Description Get all repos from database
+// @Tags domainlayer
+// @Accept application/json
+// @Success 200  {object} gin.H "{"repos": repos, "count": count}"
+// @Failure 400  {string} errcode.Error "Bad Request"
+// @Failure 500  {string} errcode.Error "Internel Error"
+// @Router /domainlayer/repos [get]
 func ReposIndex(c *gin.Context) {
 	repos, count, err := services.GetRepos()
 	if err != nil {
