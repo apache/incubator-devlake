f944114c52a2b36308b702940282c3b952f0fa5c
xgdyp <zxyasrf@163.com>
2022-05-17 23:24:08 +0800

    add swagger annotation

--- api/ping/ping.go
+++ api/ping/ping.go
@@ -6,6 +6,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @Summary Ping
+// @Description Check if http status is OK
+// @Tags Ping
+// @Success 200  "OK"
+// @Failure 400  {string} errcode.Error "Bad Request"
+// @Failure 500  {string} errcode.Error "Internel Error"
+// @Router /ping [get]
 func Get(c *gin.Context) {
 	c.Status(http.StatusOK)
 }
--- api/version/version.go
+++ api/version/version.go
@@ -7,6 +7,14 @@ import (
 	"github.com/merico-dev/lake/version"
 )
 
+// @Summary Get the version of lake
+// @Description return a object
+// @Tags version
+// @Accept application/json
+// @Success 200  {string} json ""
+// @Failure 400  {string} errcode.Error "Bad Request"
+// @Failure 500  {string} errcode.Error "Internel Error"
+// @Router /version [get]
 func Get(c *gin.Context) {
 	c.JSON(http.StatusOK, struct {
 		Version string `json:"version"`
