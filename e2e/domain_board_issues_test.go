a5c03e88294dc9b6663f9bbe4aaee7e8e1b2d565
linyh <yanghui@meri.co>
2022-05-19 22:16:53 +0800

    feat: append apache header

--- e2e/domain_board_issues_test.go
+++ e2e/domain_board_issues_test.go
@@ -1,3 +1,20 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package e2e
 
 import (
@@ -21,7 +38,7 @@ func TestDomainBoardIssues(t *testing.T) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	sqlCommand := "SELECT board_id FROM lake.issues i JOIN board_issues bi on i.id = bi.issue_id where resolution_date < '2021-10-25 17:00:58.000';"
+	sqlCommand := "SELECT board_id FROM lake.issues i JOIN board_issues bi ON i.id = bi.issue_id WHERE resolution_date < '2021-10-25 17:00:58.000';"
 	rows, err := db.Query(sqlCommand)
 	if err != nil {
 		fmt.Println("ERROR: ", err)
--- test/example/greetings_test.go
+++ test/example/greetings_test.go
@@ -1,19 +1,36 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 // https://golang.org/doc/tutorial/add-a-test
 
 package greetings
 
 import (
-    "testing"
-    "regexp"
+	"regexp"
+	"testing"
 )
 
 // TestHelloName calls greetings.Hello with a name, checking
 // for a valid return value.
 func TestHelloName(t *testing.T) {
-    name := "Gladys"
-    want := regexp.MustCompile(`\b`+name+`\b`)
-    msg := Hello("Gladys")
-    if !want.MatchString(msg) {
-        t.Fatalf(`Hello("Gladys") = %q, %v`, msg, want)
-    }
+	name := "Gladys"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	msg := Hello("Gladys")
+	if !want.MatchString(msg) {
+		t.Fatalf(`Hello("Gladys") = %q, %v`, msg, want)
+	}
 }
