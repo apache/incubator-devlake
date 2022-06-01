ab0dc9e8160c624d68fe5b33019f38b63768e5b3
Yingchu Chen <yingchu.chen@merico.dev>
2022-06-01 18:43:12 +0800

    fix(jira): modify migrationscripts to spread basicAuthEncoded to username/password

--- plugins/jira/models/migrationscripts/updateSchemas20220601.go
+++ plugins/jira/models/migrationscripts/updateSchemas20220601.go
@@ -19,11 +19,14 @@ package migrationscripts
 
 import (
 	"context"
+	"encoding/base64"
 	"github.com/apache/incubator-devlake/plugins/helper"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+	"strings"
 )
 
-type JiraConnection20220524 struct {
+type JiraConnection20220601 struct {
 	helper.RestConnection
 	helper.BasicAuth
 	EpicKeyField               string `gorm:"type:varchar(50);" json:"epicKeyField"`
@@ -31,29 +34,65 @@ type JiraConnection20220524 struct {
 	RemotelinkCommitShaPattern string `gorm:"type:varchar(255);comment='golang regexp, the first group will be recognized as commit sha, ref https://github.com/google/re2/wiki/Syntax'" json:"remotelinkCommitShaPattern"`
 }
 
-func (JiraConnection20220524) TableName() string {
+func (JiraConnection20220601) TableName() string {
 	return "_tool_jira_connections"
 }
 
-type UpdateSchemas20220524 struct{}
+type UpdateSchemas20220601 struct{}
 
-func (*UpdateSchemas20220524) Up(ctx context.Context, db *gorm.DB) error {
+func (*UpdateSchemas20220601) Up(ctx context.Context, db *gorm.DB) error {
 	var err error
 	if !db.Migrator().HasColumn(&JiraConnection20220505{}, "password") {
-		err = db.Migrator().AddColumn(&JiraConnection20220524{}, "password")
+		err = db.Migrator().AddColumn(&JiraConnection20220601{}, "password")
 		if err != nil {
 			return err
 		}
 	}
 
 	if !db.Migrator().HasColumn(&JiraConnection20220505{}, "username") {
-		err = db.Migrator().AddColumn(&JiraConnection20220524{}, "username")
+		err = db.Migrator().AddColumn(&JiraConnection20220601{}, "username")
 		if err != nil {
 			return err
 		}
 	}
 
 	if db.Migrator().HasColumn(&JiraConnection20220505{}, "basic_auth_encoded") {
+		connections := make([]*JiraConnection20220505, 0)
+		db.Find(&connections)
+		for i, _ := range connections {
+			err = helper.DecryptConnection(connections[i], "BasicAuthEncoded")
+			if err != nil {
+				return err
+			}
+			decodedStr, err := base64.StdEncoding.DecodeString(connections[i].BasicAuthEncoded)
+			if err != nil {
+				return err
+			}
+			strList := strings.Split(string(decodedStr), ":")
+			if len(strList) > 1 {
+				newConnection := JiraConnection20220601{
+					RestConnection: helper.RestConnection{
+						BaseConnection: helper.BaseConnection{
+							Name:  connections[i].Name,
+							Model: connections[i].Model,
+						},
+						Endpoint:  connections[i].Endpoint,
+						Proxy:     connections[i].Proxy,
+						RateLimit: connections[i].RateLimit,
+					},
+					BasicAuth: helper.BasicAuth{
+						Username: strList[0],
+						Password: strList[1],
+					},
+					EpicKeyField:               connections[i].EpicKeyField,
+					StoryPointField:            connections[i].StoryPointField,
+					RemotelinkCommitShaPattern: connections[i].RemotelinkCommitShaPattern,
+				}
+				db.Clauses(clause.OnConflict{UpdateAll: true}).Create(newConnection)
+
+			}
+			connections[i].Name = strList[0]
+		}
 		err = db.Migrator().DropColumn(&JiraConnection20220505{}, "basic_auth_encoded")
 		if err != nil {
 			return err
@@ -63,10 +102,10 @@ func (*UpdateSchemas20220524) Up(ctx context.Context, db *gorm.DB) error {
 	return nil
 }
 
-func (*UpdateSchemas20220524) Version() uint64 {
-	return 20220507154646
+func (*UpdateSchemas20220601) Version() uint64 {
+	return 20220601154646
 }
 
-func (*UpdateSchemas20220524) Name() string {
-	return "Add icon_url column to JiraIssue"
+func (*UpdateSchemas20220601) Name() string {
+	return "change basic_auth to username/password"
 }
