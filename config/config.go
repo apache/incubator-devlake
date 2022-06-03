944139c9a28c052060f3b0bccedcbe4e926b13e2
Klesh Wong <zhenmian.huang@merico.dev>
2022-06-04 04:11:22 +0800

    fix: remove invalid escaping

--- config/config.go
+++ config/config.go
@@ -69,8 +69,8 @@ func replaceNewEnvItemInOldContent(v *viper.Viper, envFileContent string) (error
 			switch ret := val.(type) {
 			case string:
 				ret = strings.Replace(ret, `\`, `\\`, -1)
-				ret = strings.Replace(ret, `=`, `\=`, -1)
-				ret = strings.Replace(ret, `'`, `\'`, -1)
+				//ret = strings.Replace(ret, `=`, `\=`, -1)
+				//ret = strings.Replace(ret, `'`, `\'`, -1)
 				ret = strings.Replace(ret, `"`, `\"`, -1)
 				return fmt.Sprintf(`%v="%v"`, envName, ret)
 			default:
