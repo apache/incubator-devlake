a5a324abd8f0e852f46d4815ed59baabf557cb07
Klesh Wong <zhenmian.huang@merico.dev>
2022-06-10 15:14:39 +0800

    feat: compile plugins in parallel / compile one plugin only

--- scripts/compile-plugins.sh
+++ scripts/compile-plugins.sh
@@ -21,27 +21,24 @@
 
 set -e
 
+echo "Usage: "
+echo "  build all plugins:              $0 [golang build flags...]"
+echo "  build and keep one plugin only: PLUGIN=jira $0 [golang build flags...]"
+
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 PLUGIN_SRC_DIR=$SCRIPT_DIR/../plugins
 PLUGIN_OUTPUT_DIR=$SCRIPT_DIR/../bin/plugins
 
-if [ ! -z "$PLUGIN" ]; then
-  for PLUG in $(find $PLUGIN_SRC_DIR/* -maxdepth 0 -type d -not -name core -not -empty); do
-    NAME=$(basename $PLUG)
-
-    if [ "$NAME" == "$PLUGIN" ]; then
-        echo "Building plugin $NAME to bin/plugins/$NAME/$NAME.so"
-        go build -buildmode=plugin "$@" -o $PLUGIN_OUTPUT_DIR/$NAME/$NAME.so $PLUG/*.go
-    fi
-  done
+if [ -z "$PLUGIN" ]; then
+    PLUGINS=$(find $PLUGIN_SRC_DIR/* -maxdepth 0 -type d -not -name core -not -name helper -not -empty)
 else
-  # When rebuilding from all plugins, clean out old binaries first
-  rm -rf bin/plugins/*
-
-  for PLUG in $(find $PLUGIN_SRC_DIR/* -maxdepth 0 -type d -not -name core -not -name helper -not -empty); do
-    NAME=$(basename $PLUG)
-
-    echo "Building plugin $NAME to bin/plugins/$NAME/$NAME.so"
-    go build -buildmode=plugin "$@" -o $PLUGIN_OUTPUT_DIR/$NAME/$NAME.so $PLUG/*.go
-  done
+    PLUGINS=$PLUGIN_SRC_DIR/$PLUGIN
 fi
+
+rm -rf $PLUGIN_OUTPUT_DIR/*
+for PLUG in $PLUGINS; do
+    NAME=$(basename $PLUG)
+    echo "Building plugin $NAME to bin/plugins/$NAME/$NAME.so"
+    go build -buildmode=plugin "$@" -o $PLUGIN_OUTPUT_DIR/$NAME/$NAME.so $PLUG/*.go &
+done
+wait
