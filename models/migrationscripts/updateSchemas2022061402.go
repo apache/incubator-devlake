f3948b3685eabdfb026d68e1ffd09a53ea4da77e
cong.wang <cong.wang@merico.dev>
2022-06-14 13:07:02 +0800

    feat: add license header

--- models/migrationscripts/updateSchemas2022061402.go
+++ models/migrationscripts/updateSchemas2022061402.go
@@ -5,9 +5,7 @@ this work for additional information regarding copyright ownership.
 The ASF licenses this file to You under the Apache License, Version 2.0
 (the "License"); you may not use this file except in compliance with
 the License.  You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,26 +13,25 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package ticket
+package migrationscripts
 
 import (
-	"time"
+	"context"
 
-	"github.com/apache/incubator-devlake/models/domainlayer"
+	"github.com/apache/incubator-devlake/models/domainlayer/ticket"
+	"gorm.io/gorm"
 )
 
-type Changelog struct {
-	domainlayer.DomainEntity
+type updateSchemas2022061402 struct{}
 
-	// collected fields
-	IssueId      string `gorm:"index;type:varchar(255)"`
-	AuthorId     string `gorm:"type:varchar(255)"`
-	AuthorName   string `gorm:"type:varchar(255)"`
-	FieldId      string `gorm:"type:varchar(255)"`
-	FieldName    string `gorm:"type:varchar(255)"`
-	FromValue    string
-	StandardFrom string
-	StandardTo   string
-	ToValue      string
-	CreatedDate  time.Time
+func (*updateSchemas2022061402) Up(ctx context.Context, db *gorm.DB) error {
+	return db.Migrator().AutoMigrate(&ticket.Changelog{})
+}
+
+func (*updateSchemas2022061402) Version() uint64 {
+	return 20220614091600
+}
+
+func (*updateSchemas2022061402) Name() string {
+	return "update table: changelogs, add standard_from, standard_to"
 }
