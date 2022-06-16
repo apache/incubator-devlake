334b023fa1bed1913ef5714000014bb8b4a73e39
Julien Chinapen <julien.chinapen@merico.dev>
2022-06-16 10:25:54 -0400

    fix: enable testing on connection dialog

--- config-ui/src/pages/blueprints/create-blueprint.jsx
+++ config-ui/src/pages/blueprints/create-blueprint.jsx
@@ -1581,6 +1581,7 @@ const CreateBlueprint = (props) => {
         onTokenChange={setToken}
         onUsernameChange={setUsername}
         onPasswordChange={setPassword}
+        testStatus={testStatus}
       />
 
       <CodeInspector
