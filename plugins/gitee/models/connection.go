495420cfb4a5713b9a271e038be764959167cd26
Mr.An <42627702+SnowMoon-Dev@users.noreply.github.com>
2022-06-18 02:26:19 +0800

    update api connection.go

--- plugins/gitee/models/connection.go
+++ plugins/gitee/models/connection.go
@@ -17,30 +17,28 @@ limitations under the License.
 
 package models
 
-// This object conforms to what the frontend currently sends.
+import "github.com/apache/incubator-devlake/plugins/helper"
+
 type GiteeConnection struct {
-	Endpoint string `mapstructure:"endpoint" validate:"required" env:"GITEE_ENDPOINT" json:"endpoint"`
-	Auth     string `mapstructure:"auth" validate:"required" env:"GITEE_AUTH"  json:"auth"`
-	Proxy    string `mapstructure:"proxy" env:"GITEE_PROXY" json:"proxy"`
+	helper.RestConnection `mapstructure:",squash"`
+	helper.AccessToken    `mapstructure:",squash"`
 }
 
-// This object conforms to what the frontend currently expects.
 type GiteeResponse struct {
 	Name string `json:"name"`
 	ID   int    `json:"id"`
 	GiteeConnection
 }
 
-// Using User because it requires authentication.
 type ApiUserResponse struct {
 	Id   int
 	Name string `json:"name"`
 }
 
 type TestConnectionRequest struct {
-	Endpoint string `json:"endpoint" validate:"required"`
-	Auth     string `json:"auth" validate:"required"`
-	Proxy    string `json:"proxy"`
+	Endpoint           string `json:"endpoint" validate:"required"`
+	Proxy              string `json:"proxy"`
+	helper.AccessToken `mapstructure:",squash"`
 }
 
 type Config struct {
@@ -52,13 +50,4 @@ type Config struct {
 	IssueTypeBug         string `mapstructure:"issueTypeBug" env:"GITEE_ISSUE_TYPE_BUG" json:"issueTypeBug"`
 	IssueTypeIncident    string `mapstructure:"issueTypeIncident" env:"GITEE_ISSUE_TYPE_INCIDENT" json:"issueTypeIncident"`
 	IssueTypeRequirement string `mapstructure:"issueTypeRequirement" env:"GITEE_ISSUE_TYPE_REQUIREMENT" json:"issueTypeRequirement"`
-}
-
-// Using Public Email because it requires authentication, and it is public information anyway.
-// We're not using email information for anything here.
-type PublicEmail struct {
-	Email      string
-	Primary    bool
-	Verified   bool
-	Visibility string
 }
