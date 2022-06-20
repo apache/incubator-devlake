1d148763196ed253a7a5565500789fb16bb7a37c
abeizn <zikuan.an@merico.dev>
2022-06-20 16:51:57 +0800

    fix: docker-compose platform

--- plugins/github/models/migrationscripts/init_schema_20220611.go
+++ plugins/github/models/migrationscripts/init_schema_20220611.go
@@ -5,9 +5,7 @@ this work for additional information regarding copyright ownership.
 The ASF licenses this file to You under the Apache License, Version 2.0
 (the "License"); you may not use this file except in compliance with
 the License.  You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,12 +17,11 @@ package migrationscripts
 
 import (
 	"context"
-
-	"github.com/apache/incubator-devlake/config"
-	commonArchived "github.com/apache/incubator-devlake/models/migrationscripts/archived"
 	"github.com/apache/incubator-devlake/plugins/core"
-	"github.com/apache/incubator-devlake/plugins/github/models/migrationscripts/archived"
 	"github.com/apache/incubator-devlake/plugins/helper"
+
+	commonArchived "github.com/apache/incubator-devlake/models/migrationscripts/archived"
+	"github.com/apache/incubator-devlake/plugins/github/models/migrationscripts/archived"
 	"gorm.io/gorm"
 )
 
@@ -97,17 +94,17 @@ func (u *InitSchemas) Up(ctx context.Context, db *gorm.DB) error {
 	connection.Proxy = u.config.GetString(`GITHUB_PROXY`)
 	connection.Token = u.config.GetString(`GITHUB_AUTH`)
 	connection.Name = `GitHub`
-	v := config.GetConfig()
-	encKey := v.GetString("ENCODE_KEY")
-
+	if err != nil {
+		return err
+	}
 	err = helper.UpdateEncryptFields(connection, func(plaintext string) (string, error) {
-		return core.Encrypt(encKey, plaintext)
+		return core.Encrypt(u.config.GetString(core.EncodeKeyEnvStr), plaintext)
 	})
 	if err != nil {
 		return err
 	}
 	// update from .env and save to db
-	if connection.Endpoint != `` && connection.Token != `` && encKey != `` {
+	if connection.Endpoint != `` && connection.Token != `` {
 		db.Create(connection)
 	}
 
