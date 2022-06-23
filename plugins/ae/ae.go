ea772ced8f31c3b854521b5ec497ef783471ba73
mappjzc <zhicheng.jiang@merico.dev>
2022-06-23 22:33:06 +0800

    feat: ae e2e test for commits (#2318)
    Add TestAECommitDataFlow
    
    Nddtfjiang <zhicheng.jiang@merico.dev>

--- plugins/ae/ae.go
+++ plugins/ae/ae.go
@@ -18,114 +18,13 @@ limitations under the License.
 package main // must be main for plugin entry point
 
 import (
-	"fmt"
-
-	"github.com/apache/incubator-devlake/migration"
-	"github.com/apache/incubator-devlake/plugins/ae/api"
-	"github.com/apache/incubator-devlake/plugins/ae/models"
-	"github.com/apache/incubator-devlake/plugins/ae/models/migrationscripts"
-	"github.com/apache/incubator-devlake/plugins/ae/tasks"
-	"github.com/apache/incubator-devlake/plugins/core"
-	"github.com/apache/incubator-devlake/plugins/helper"
+	"github.com/apache/incubator-devlake/plugins/ae/impl"
 	"github.com/apache/incubator-devlake/runner"
-	"github.com/mitchellh/mapstructure"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-	"gorm.io/gorm"
 )
 
-var _ core.PluginMeta = (*AE)(nil)
-var _ core.PluginInit = (*AE)(nil)
-var _ core.PluginTask = (*AE)(nil)
-var _ core.PluginApi = (*AE)(nil)
-var _ core.Migratable = (*AE)(nil)
-
-type AE struct{}
-
-func (plugin AE) Init(config *viper.Viper, logger core.Logger, db *gorm.DB) error {
-	api.Init(config, logger, db)
-	return nil
-}
-
-func (plugin AE) Description() string {
-	return "To collect and enrich data from AE"
-}
-
-func (plugin AE) SubTaskMetas() []core.SubTaskMeta {
-	return []core.SubTaskMeta{
-		tasks.CollectProjectMeta,
-		tasks.CollectCommitsMeta,
-		tasks.ExtractProjectMeta,
-		tasks.ExtractCommitsMeta,
-		tasks.ConvertCommitsMeta,
-	}
-}
-
-func (plugin AE) PrepareTaskData(taskCtx core.TaskContext, options map[string]interface{}) (interface{}, error) {
-	var op tasks.AeOptions
-	err := mapstructure.Decode(options, &op)
-	if err != nil {
-		return nil, err
-	}
-	if op.ProjectId <= 0 {
-		return nil, fmt.Errorf("projectId is required")
-	}
-
-	connection := &models.AeConnection{}
-	connectionHelper := helper.NewConnectionHelper(
-		taskCtx,
-		nil,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	err = connectionHelper.FirstById(connection, op.ConnectionId)
-	if err != nil {
-		return nil, err
-	}
-
-	apiClient, err := tasks.CreateApiClient(taskCtx, connection)
-	if err != nil {
-		return nil, err
-	}
-
-	return &tasks.AeTaskData{
-		Options:   &op,
-		ApiClient: apiClient,
-	}, nil
-}
-
-func (plugin AE) RootPkgPath() string {
-	return "github.com/apache/incubator-devlake/plugins/ae"
-}
-
-func (plugin AE) MigrationScripts() []migration.Script {
-	return []migration.Script{
-		new(migrationscripts.InitSchemas),
-		new(migrationscripts.UpdateSchemas20220615),
-	}
-}
-
-func (plugin AE) ApiResources() map[string]map[string]core.ApiResourceHandler {
-	return map[string]map[string]core.ApiResourceHandler{
-		"test": {
-			"GET": api.TestConnection,
-		},
-		"connections": {
-			"GET":  api.ListConnections,
-			"POST": api.PostConnections,
-		},
-		"connections/:connectionId": {
-			"GET":    api.GetConnection,
-			"PATCH":  api.PatchConnection,
-			"DELETE": api.DeleteConnection,
-		},
-	}
-}
-
 // Export a variable named PluginEntry for Framework to search and load
-var PluginEntry AE //nolint
+var PluginEntry impl.AE //nolint
 
 func main() {
 	aeCmd := &cobra.Command{Use: "ae"}
---
+++ plugins/ae/e2e/commit_test.go
@@ -0,0 +1,92 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/apache/incubator-devlake/helpers/e2ehelper"
+	"github.com/apache/incubator-devlake/models/domainlayer/code"
+	"github.com/apache/incubator-devlake/plugins/ae/impl"
+	"github.com/apache/incubator-devlake/plugins/ae/models"
+	"github.com/apache/incubator-devlake/plugins/ae/tasks"
+)
+
+func TestAECommitDataFlow(t *testing.T) {
+	var ae impl.AE
+	dataflowTester := e2ehelper.NewDataFlowTester(t, "ae", ae)
+
+	taskData := &tasks.AeTaskData{
+		Options: &tasks.AeOptions{
+			ConnectionId: 1,
+			ProjectId:    13,
+		},
+	}
+
+	// import raw data table
+	dataflowTester.ImportCsvIntoRawTable("./raw_tables/_raw_ae_commits.csv", "_raw_ae_commits")
+
+	// verify extraction
+	dataflowTester.FlushTabler(&models.AECommit{})
+	dataflowTester.Subtask(tasks.ExtractCommitsMeta, taskData)
+	dataflowTester.VerifyTable(
+		models.AECommit{},
+		fmt.Sprintf("./snapshot_tables/%s.csv", models.AECommit{}.TableName()),
+		[]string{"hex_sha"},
+		[]string{
+			"analysis_id",
+			"author_email",
+			"dev_eq",
+			"ae_project_id",
+			"_raw_data_params",
+			"_raw_data_table",
+			"_raw_data_id",
+			"_raw_data_remark",
+		},
+	)
+
+	// verify conversion
+	dataflowTester.FlushTabler(&code.Commit{})
+	// import data table
+	dataflowTester.ImportCsvIntoTabler("./raw_tables/_raw_commits.csv", &code.Commit{})
+	dataflowTester.Subtask(tasks.ConvertCommitsMeta, taskData)
+	dataflowTester.VerifyTable(
+		code.Commit{},
+		fmt.Sprintf("./snapshot_tables/%s.csv", code.Commit{}.TableName()),
+		[]string{"sha"},
+		[]string{
+			"_raw_data_params",
+			"_raw_data_table",
+			"_raw_data_id",
+			"_raw_data_remark",
+			"additions",
+			"deletions",
+			"dev_eq",
+			"message",
+			"author_name",
+			"author_email",
+			"authored_date",
+			"author_id",
+			"committer_name",
+			"committer_email",
+			"committed_date",
+			"committer_id",
+		},
+	)
+}
