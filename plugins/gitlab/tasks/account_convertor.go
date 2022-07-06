bc9fd39af80b7bd405ae827652c05c4fb4e3bd76
abeizn <zikuan.an@merico.dev>
2022-07-06 15:38:22 +0800

    feat: update gilab user to account

--- plugins/gitlab/tasks/account_convertor.go
+++ plugins/gitlab/tasks/account_convertor.go
@@ -18,16 +18,13 @@ limitations under the License.
 package tasks
 
 import (
-	"github.com/apache/incubator-devlake/models/domainlayer/crossdomain"
 	"reflect"
 
-	"github.com/apache/incubator-devlake/models/domainlayer/crossdomain"
-
-	"github.com/apache/incubator-devlake/plugins/core/dal"
-
 	"github.com/apache/incubator-devlake/models/domainlayer"
+	"github.com/apache/incubator-devlake/models/domainlayer/crossdomain"
 	"github.com/apache/incubator-devlake/models/domainlayer/didgen"
 	"github.com/apache/incubator-devlake/plugins/core"
+	"github.com/apache/incubator-devlake/plugins/core/dal"
 	gitlabModels "github.com/apache/incubator-devlake/plugins/gitlab/models"
 	"github.com/apache/incubator-devlake/plugins/helper"
 )
