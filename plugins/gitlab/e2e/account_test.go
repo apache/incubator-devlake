c1c10f89b55879fdad7f8a8da7d65b418dde6bda
abeizn <zikuan.an@merico.dev>
2022-07-13 22:02:11 +0800

    refactor: add jira,gitlab account e2e

---
+++ plugins/gitlab/e2e/account_test.go
@@ -0,0 +1,91 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/apache/incubator-devlake/helpers/e2ehelper"
+	"github.com/apache/incubator-devlake/models/domainlayer/crossdomain"
+	"github.com/apache/incubator-devlake/plugins/gitlab/impl"
+	"github.com/apache/incubator-devlake/plugins/gitlab/models"
+	"github.com/apache/incubator-devlake/plugins/gitlab/tasks"
+)
+
+func TestGitlabAccountDataFlow(t *testing.T) {
+
+	var gitlab impl.Gitlab
+	dataflowTester := e2ehelper.NewDataFlowTester(t, "gitlab", gitlab)
+
+	taskData := &tasks.GitlabTaskData{
+		Options: &tasks.GitlabOptions{
+			ConnectionId: 1,
+			ProjectId:    12345678,
+		},
+	}
+
+	// import raw data table
+	dataflowTester.ImportCsvIntoRawTable("./raw_tables/_raw_gitlab_api_users.csv",
+		"_raw_gitlab_api_users")
+
+	// verify extraction
+	dataflowTester.FlushTabler(&models.GitlabAccount{})
+	dataflowTester.Subtask(tasks.ExtractAccountMeta, taskData)
+	dataflowTester.VerifyTable(
+		models.GitlabAccount{},
+		fmt.Sprintf("./snapshot_tables/%s.csv", models.GitlabAccount{}.TableName()),
+		[]string{
+			"connection_id",
+			"gitlab_id",
+			"username",
+			"email",
+			"name",
+			"state",
+			"membership_state",
+			"avatar_url",
+			"web_url",
+			"_raw_data_params",
+			"_raw_data_table",
+			"_raw_data_id",
+			"_raw_data_remark",
+		},
+	)
+
+	// verify conversion
+	dataflowTester.FlushTabler(&crossdomain.Account{})
+	dataflowTester.Subtask(tasks.ConvertAccountMeta, taskData)
+	dataflowTester.VerifyTable(
+		crossdomain.Account{},
+		fmt.Sprintf("./snapshot_tables/%s.csv", crossdomain.Account{}.TableName()),
+		[]string{
+			"id",
+			"_raw_data_params",
+			"_raw_data_table",
+			"_raw_data_id",
+			"_raw_data_remark",
+			"email",
+			"full_name",
+			"user_name",
+			"avatar_url",
+			"organization",
+			"created_date",
+			"status",
+		},
+	)
+}
