d32168a3e3576e82d98968ad7ca97c6848e85390
abeizn <zikuan.an@merico.dev>
2022-08-01 17:35:55 +0800

    feat: github add cicd feature

--- plugins/github/models/migrationscripts/register.go
+++ plugins/github/models/migrationscripts/register.go
@@ -25,5 +25,7 @@ import (
 func All() []migration.Script {
 	return []migration.Script{
 		new(addInitTables),
+		new(addGithubRunsTable),
+		new(addGithubJobsTable),
 	}
 }
---
+++ plugins/github/tasks/cicd_job_collector.go
@@ -0,0 +1,103 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package tasks
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"reflect"
+
+	"github.com/apache/incubator-devlake/plugins/core"
+	"github.com/apache/incubator-devlake/plugins/core/dal"
+	"github.com/apache/incubator-devlake/plugins/github/models"
+	"github.com/apache/incubator-devlake/plugins/helper"
+)
+
+const RAW_JOB_TABLE = "github_api_jobs"
+
+var CollectJobsMeta = core.SubTaskMeta{
+	Name:             "collectJobs",
+	EntryPoint:       CollectJobs,
+	EnabledByDefault: true,
+	Description:      "Collect Jobs data from Github action api",
+	DomainTypes:      []string{core.DOMAIN_TYPE_CICD},
+}
+
+func CollectJobs(taskCtx core.SubTaskContext) error {
+	db := taskCtx.GetDal()
+	data := taskCtx.GetData().(*GithubTaskData)
+	cursor, err := db.Cursor(
+		dal.Select("id"),
+		dal.From(models.GithubRun{}.TableName()),
+	)
+	if err != nil {
+		return err
+	}
+	iterator, err := helper.NewDalCursorIterator(db, cursor, reflect.TypeOf(SimpleGithubRun{}))
+	if err != nil {
+		return err
+	}
+
+	collector, err := helper.NewApiCollector(helper.ApiCollectorArgs{
+		RawDataSubTaskArgs: helper.RawDataSubTaskArgs{
+			Ctx: taskCtx,
+			Params: GithubApiParams{
+				ConnectionId: data.Options.ConnectionId,
+				Owner:        data.Options.Owner,
+				Repo:         data.Options.Repo,
+			},
+			Table: RAW_JOB_TABLE,
+		},
+		ApiClient:   data.ApiClient,
+		PageSize:    100,
+		Input:       iterator,
+		Incremental: false,
+		UrlTemplate: "repos/{{ .Params.Owner }}/{{ .Params.Repo }}/actions/runs/{{ .Input.ID }}/jobs",
+		Query: func(reqData *helper.RequestData) (url.Values, error) {
+			query := url.Values{}
+			query.Set("page", fmt.Sprintf("%v", reqData.Pager.Page))
+			query.Set("per_page", fmt.Sprintf("%v", reqData.Pager.Size))
+			return query, nil
+		},
+		GetTotalPages: GetTotalPagesFromResponse,
+		ResponseParser: func(res *http.Response) ([]json.RawMessage, error) {
+			body := &GithubRawJobsResult{}
+			err := helper.UnmarshalResponse(res, body)
+			if err != nil {
+				return nil, err
+			}
+			return body.GithubWorkflowJobs, nil
+		},
+	})
+
+	if err != nil {
+		return err
+	}
+	return collector.Execute()
+}
+
+type SimpleGithubRun struct {
+	ID int64
+}
+
+type GithubRawJobsResult struct {
+	TotalCount         int64             `json:"total_count"`
+	GithubWorkflowJobs []json.RawMessage `json:"jobs"`
+}
