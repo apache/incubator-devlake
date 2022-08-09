9ac7cedfbb32f97072f83dd7bd260a47444b6992
linyh <yanghui@meri.co>
2022-08-09 17:37:40 +0800

    test: add unittest

--- services/blueprint.go
+++ services/blueprint.go
@@ -280,22 +280,28 @@ func GeneratePlanJsonV100(settings *models.BlueprintSettings) (core.PipelinePlan
 			return nil, fmt.Errorf("plugin %s does not support blueprint protocol version 1.0.0", connection.Plugin)
 		}
 	}
+
+	mergedPipelinePlan := MergePipelinePlans(plans...)
+	return FormatPipelinePlans(settings.BeforePlan, mergedPipelinePlan, settings.AfterPlan)
+}
+
+// FormatPipelinePlans merges multiple pipelines and append before and after pipeline
+func FormatPipelinePlans(beforePlanJson json.RawMessage, mainPlan core.PipelinePlan, afterPlanJson json.RawMessage) (core.PipelinePlan, error) {
 	newPipelinePlan := core.PipelinePlan{}
-	if settings.BeforePlan != nil {
+	if beforePlanJson != nil {
 		beforePipelinePlan := core.PipelinePlan{}
-		err = json.Unmarshal(settings.BeforePlan, &beforePipelinePlan)
+		err := json.Unmarshal(beforePlanJson, &beforePipelinePlan)
 		if err != nil {
 			return nil, err
 		}
 		newPipelinePlan = append(newPipelinePlan, beforePipelinePlan...)
 	}
 
-	mergedPipelinePlan := MergePipelinePlans(plans...)
-	newPipelinePlan = append(newPipelinePlan, mergedPipelinePlan...)
+	newPipelinePlan = append(newPipelinePlan, mainPlan...)
 
-	if settings.AfterPlan != nil {
+	if afterPlanJson != nil {
 		afterPipelinePlan := core.PipelinePlan{}
-		err = json.Unmarshal(settings.AfterPlan, &afterPipelinePlan)
+		err := json.Unmarshal(afterPlanJson, &afterPipelinePlan)
 		if err != nil {
 			return nil, err
 		}
