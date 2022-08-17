cd9775bd5dc82432b2ac704c8ceb121eacf0d2c4
likyh <l@likyh.com>
2022-08-17 19:01:05 +0800

    fix: add getAfterResponse to overwrite by helper (#2755)
    * fix: fix afterResponse callback will overwrite by helper
    
    * fix: fix for ci
    
    Co-authored-by: linyh <yanghui@meri.co>

--- plugins/jira/tasks/api_client.go
+++ plugins/jira/tasks/api_client.go
@@ -36,12 +36,6 @@ func NewJiraApiClient(taskCtx core.TaskContext, connection *models.JiraConnectio
 	if err != nil {
 		return nil, err
 	}
-	apiClient.SetAfterFunction(func(res *http.Response) error {
-		if res.StatusCode == http.StatusUnauthorized {
-			return fmt.Errorf("authentication failed, please check your AccessToken")
-		}
-		return nil
-	})
 
 	// create rate limit calculator
 	rateLimiter := &helper.ApiRateLimitCalculator{
