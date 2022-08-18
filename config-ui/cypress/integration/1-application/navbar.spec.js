93baae664af75df25674a1487a406b8cd41bbab2
linyh <yanghui@meri.co>
2022-08-18 18:46:32 +0800

    fix: fix for ci

--- config-ui/cypress/integration/1-application/navbar.spec.js
+++ config-ui/cypress/integration/1-application/navbar.spec.js
@@ -37,12 +37,4 @@ context('Navbar', () => {
       .should('be.visible')
       .and('have.class', 'navIconLink')
   })
-
-  it('shows merico discord icon link', () => {
-    cy.get('.navbar')
-      .should('have.class', 'bp3-navbar')
-      .find('a[href="https://discord.com/invite/83rDG6ydVZ"]')
-      .should('be.visible')
-      .and('have.class', 'navIconLink')
-  })
 })
