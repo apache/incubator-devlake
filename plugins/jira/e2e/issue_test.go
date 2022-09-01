e23ed2a6a53b38c2c39a5608bc437f757c7160c3
Nddtfjiang <jzc001@qq.com>
2022-09-01 15:24:59 +0000

    feat: jira issue e2e test
    update jira issue e2e test for support type mappings
    
    Nddtfjiang <zhicheng.jiang@merico.dev>

--- plugins/jira/e2e/issue_test.go
+++ plugins/jira/e2e/issue_test.go
@@ -33,9 +33,28 @@ func TestIssueDataFlow(t *testing.T) {
 
 	taskData := &tasks.JiraTaskData{
 		Options: &tasks.JiraOptions{
-			ConnectionId:        2,
-			BoardId:             8,
-			TransformationRules: tasks.TransformationRules{StoryPointField: "customfield_10024"}},
+			ConnectionId: 2,
+			BoardId:      8,
+			TransformationRules: tasks.TransformationRules{
+				StoryPointField: "customfield_10024",
+				TypeMappings: map[string]tasks.TypeMapping{
+					"Sub-task": {
+						StandardType: "Sub-task",
+						StatusMappings: map[string]tasks.StatusMapping{
+							"done": {StandardStatus: "你好世界"},
+							"new":  {StandardStatus: "\u6069\u5E95\u6EF4\u68AF\u6B38\u592B\u5178\u4EA2\u59C6"},
+						},
+					},
+					"Task": {
+						StandardType: "Task",
+						StatusMappings: map[string]tasks.StatusMapping{
+							"done": {StandardStatus: "hello world"},
+							"new":  {StandardStatus: "110 100 100 116 102 46 99 111 109"},
+						},
+					},
+				},
+			},
+		},
 	}
 
 	// import raw data table
