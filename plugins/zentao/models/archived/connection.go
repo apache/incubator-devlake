e7e31a10cb87af1bfb55998367222d5d3ee67d4a
Yingchu Chen <yingchu.chen@merico.dev>
2022-09-10 15:35:59 +0800

    fix(zentao): update connection

--- plugins/zentao/models/archived/connection.go
+++ plugins/zentao/models/archived/connection.go
@@ -15,7 +15,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package models
+package archived
 
 import (
 	"github.com/apache/incubator-devlake/models/migrationscripts/archived"
