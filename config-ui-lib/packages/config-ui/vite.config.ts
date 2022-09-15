c6d93c0e45b5eaf7864bd9eeef77f95f5f01072c
mintsweet <junren.yu@merico.dev>
2022-09-15 12:06:43 +0800

    refactor(config-ui): use yarn workspaces to replace multiple directories

--- config-ui-lib/packages/config-ui/vite.config.ts
+++ config-ui-lib/packages/config-ui/vite.config.ts
@@ -1,12 +1,24 @@
 import { resolve } from 'path';
+
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
+
   build: {
     outDir: '_build',
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'config-ui',
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          vue: 'React',
+        },
+      },
     },
   },
 });
--- config-ui-lib/packages/website/vite.config.ts
+++ config-ui-lib/packages/website/vite.config.ts
@@ -1,12 +1,11 @@
 import { resolve } from 'path';
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  root: resolve(__dirname, 'website'),
 
   build: {
     outDir: resolve(__dirname, '_website'),
