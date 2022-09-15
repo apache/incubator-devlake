8b7f90abf92fd58a64f7b2980e19309cc61f433a
linyh <yanghui@meri.co>
2022-09-16 01:17:53 +0800

    feat: add webhooks cicd_task and pipeline_finish

--- plugins/webhook/api/cicd_pipeline.go
+++ plugins/webhook/api/cicd_pipeline.go
@@ -22,7 +22,9 @@ import (
 	"github.com/apache/incubator-devlake/errors"
 	"github.com/apache/incubator-devlake/models/domainlayer"
 	"github.com/apache/incubator-devlake/models/domainlayer/devops"
+	"github.com/apache/incubator-devlake/models/domainlayer/ticket"
 	"github.com/apache/incubator-devlake/plugins/core"
+	"github.com/apache/incubator-devlake/plugins/core/dal"
 	"github.com/apache/incubator-devlake/plugins/helper"
 	"github.com/apache/incubator-devlake/plugins/webhook/models"
 	"github.com/go-playground/validator/v10"
@@ -30,76 +32,180 @@ import (
 	"time"
 )
 
-type WebhookPipelineRequest struct {
-	Id           string     `validate:"required"`
-	Result       string     `validate:"oneof=SUCCESS FAILURE ABORT"`
+type WebhookTaskRequest struct {
+	// PipelineName can be filled by any string unique in one pipeline
+	PipelineName string `mapstructure:"pipeline_name" validate:"required"`
+
+	Name         string     `validate:"required"` // Name should be unique in one pipeline
+	Result       string     `validate:"oneof=SUCCESS FAILURE ABORT IN_PROGRESS"`
 	Status       string     `validate:"oneof=IN_PROGRESS DONE"`
 	Type         string     `validate:"oneof=CI CD CI/CD"`
-	CreatedDate  time.Time  `mapstructure:"created_date" validate:"required"`
+	StartedDate  time.Time  `mapstructure:"created_date" validate:"required"`
 	FinishedDate *time.Time `mapstructure:"finished_date"`
 
-	Repo      string `validate:"required"`
+	RepoId    string `mapstructure:"repo_id" validate:"required"` // RepoId should be unique string
 	Branch    string
 	CommitSha string `mapstructure:"commit_sha"`
 }
 
-// PostCicdPipeline
+// PostCicdTask
 // @Summary create pipeline by webhook
-// @Description Create pipeline by webhook, example: {"id":"A123123","result":"one of SUCCESS/FAILURE/ABORT","status":"one of IN_PROGRESS/DONE","type":"CI/CD","created_date":"2020-01-01T12:00:00+00:00","finished_date":"2020-01-01T12:59:59+00:00","repo":"devlake","branch":"main","commit_sha":"015e3d3b480e417aede5a1293bd61de9b0fd051d"}
+// @Description Create pipeline by webhook.<br/>
+// @Description example1: {"pipeline_name":"A123","name":"unit-test","result":"IN_PROGRESS","status":"IN_PROGRESS","type":"CI","created_date":"2020-01-01T12:00:00+00:00","finished_date":"2020-01-01T12:59:59+00:00","repo_id":"devlake","branch":"main","commit_sha":"015e3d3b480e417aede5a1293bd61de9b0fd051d"}<br/>
+// @Description example2: {"pipeline_name":"A123","name":"unit-test","result":"SUCCESS","status":"DONE","type":"CI/CD","created_date":"2020-01-01T12:00:00+00:00","finished_date":"2020-01-01T12:59:59+00:00","repo_id":"devlake","branch":"main","commit_sha":"015e3d3b480e417aede5a1293bd61de9b0fd051d"}<br/>
+// @Description When request webhook first time for each pipeline, it will be created.
+// @Description So we suggest request before task start and after pipeline finish.
+// @Description Remember fill all data to request after pipeline finish.
 // @Tags plugins/webhook
-// @Param body body WebhookPipelineRequest true "json body"
+// @Param body body WebhookTaskRequest true "json body"
 // @Success 200
 // @Failure 400  {string} errcode.Error "Bad Request"
+// @Failure 403  {string} errcode.Error "Forbidden"
 // @Failure 500  {string} errcode.Error "Internal Error"
-// @Router /plugins/webhook/:connectionId/cicd_pipelines [POST]
-func PostCicdPipeline(input *core.ApiResourceInput) (*core.ApiResourceOutput, errors.Error) {
+// @Router /plugins/webhook/:connectionId/cicd_tasks [POST]
+func PostCicdTask(input *core.ApiResourceInput) (*core.ApiResourceOutput, errors.Error) {
 	connection := &models.WebhookConnection{}
 	err := connectionHelper.First(connection, input.Params)
 	if err != nil {
 		return nil, err
 	}
 	// get request
-	request := &WebhookPipelineRequest{}
+	request := &WebhookTaskRequest{}
 	err = helper.DecodeMapStruct(input.Body, request)
 	if err != nil {
 		return &core.ApiResourceOutput{Body: err.Error(), Status: http.StatusBadRequest}, nil
 	}
 	// validate
 	vld = validator.New()
-	err = errors.Convert(vld.Struct(request))
+	err = errors.BadInput.Wrap(vld.Struct(request), `input json error`)
 	if err != nil {
 		return &core.ApiResourceOutput{Body: err.Error(), Status: http.StatusBadRequest}, nil
 	}
 
 	db := basicRes.GetDal()
-	domainPipeline := &devops.CICDPipeline{
+	pipelineId := fmt.Sprintf("%s:%d:%s", "webhook", connection.ID, request.PipelineName)
+	domainCicdTask := &devops.CICDTask{
 		DomainEntity: domainlayer.DomainEntity{
-			Id: fmt.Sprintf("%s:%d:%s:%s:%s:%s", "webhook", connection.ID, request.Repo, request.Branch, request.CommitSha, request.Id),
+			Id: fmt.Sprintf("%s:%d:%s:%s", "webhook", connection.ID, request.PipelineName, request.Name),
 		},
-		Name:         request.Id,
+		PipelineId:   pipelineId,
+		Name:         request.Name,
 		Result:       request.Result,
 		Status:       request.Status,
 		Type:         request.Type,
-		CreatedDate:  request.CreatedDate,
+		StartedDate:  request.StartedDate,
 		FinishedDate: request.FinishedDate,
 	}
-	if domainPipeline.FinishedDate != nil {
-		domainPipeline.DurationSec = uint64(domainPipeline.FinishedDate.Sub(domainPipeline.CreatedDate).Seconds())
+	if domainCicdTask.FinishedDate != nil {
+		domainCicdTask.DurationSec = uint64(domainCicdTask.FinishedDate.Sub(domainCicdTask.StartedDate).Seconds())
 	}
-	domainPipelineRepo := &devops.CiCDPipelineRepo{
-		DomainEntity: domainlayer.DomainEntity{
-			Id: fmt.Sprintf("%s:%d:%s:%s:%s", "webhook", connection.ID, request.Repo, request.Branch, request.CommitSha),
-		},
-		CommitSha: request.CommitSha,
-		Branch:    request.Branch,
-		Repo:      request.Repo,
+
+	domainPipeline := &devops.CICDPipeline{}
+	err = db.First(domainPipeline, dal.Where("id = ?", pipelineId))
+	if err != nil {
+		domainPipeline = &devops.CICDPipeline{
+			DomainEntity: domainlayer.DomainEntity{
+				Id: pipelineId,
+			},
+			Name:         request.PipelineName,
+			Result:       ``,
+			Status:       `IN_PROGRESS`,
+			Type:         ``,
+			CreatedDate:  request.StartedDate,
+			FinishedDate: nil,
+		}
+	} else if domainPipeline.Status == `DONE` {
+		return nil, errors.Forbidden.New(`can not receive this task because pipeline has already been done.`)
 	}
+
+	domainPipelineRepo := &devops.CiCDPipelineCommit{
+		PipelineId: pipelineId,
+		CommitSha:  request.CommitSha,
+		Branch:     request.Branch,
+		RepoId:     request.RepoId,
+	}
+
 	// save
+	err = db.CreateOrUpdate(domainCicdTask)
+	if err != nil {
+		return nil, err
+	}
 	err = db.CreateOrUpdate(domainPipeline)
 	if err != nil {
 		return nil, err
 	}
 	err = db.CreateOrUpdate(domainPipelineRepo)
+	if err != nil {
+		return nil, err
+	}
+
+	return &core.ApiResourceOutput{Body: nil, Status: http.StatusOK}, nil
+}
+
+// PostPipelineFinish
+// @Summary set pipeline's status to DONE
+// @Description set pipeline's status to DONE and cal duration
+// @Tags plugins/webhook
+// @Success 200
+// @Failure 400  {string} errcode.Error "Bad Request"
+// @Failure 500  {string} errcode.Error "Internal Error"
+// @Router /plugins/webhook/:connectionId/cicd_pipeline/:pipelineName/finish [POST]
+func PostPipelineFinish(input *core.ApiResourceInput) (*core.ApiResourceOutput, errors.Error) {
+	connection := &models.WebhookConnection{}
+	err := connectionHelper.First(connection, input.Params)
+	if err != nil {
+		return nil, err
+	}
+
+	db := basicRes.GetDal()
+	pipelineId := fmt.Sprintf("%s:%d:%s", "webhook", connection.ID, input.Params[`pipelineName`])
+	println(pipelineId)
+	domainPipeline := &devops.CICDPipeline{}
+	err = db.First(domainPipeline, dal.Where("id = ?", pipelineId))
+	if err != nil {
+		return nil, errors.NotFound.Wrap(err, `pipeline not found`)
+	}
+
+	domainTasks := []devops.CICDTask{}
+	err = db.All(&domainTasks, dal.Where("pipeline_id = ?", pipelineId))
+	if err != nil {
+		return nil, errors.NotFound.Wrap(err, `tasks not found`)
+	}
+	typeHasCi := false
+	typeHasCd := false
+	result := `SUCCESS`
+	for _, domainTask := range domainTasks {
+		if domainTask.Type == `CI/CD` {
+			typeHasCi = true
+			typeHasCd = true
+		} else if domainTask.Type == `CI` {
+			typeHasCi = true
+		} else if domainTask.Type == `CD` {
+			typeHasCd = true
+		}
+		if domainTask.Type == `ABORT` {
+			result = `ABORT`
+		} else if domainTask.Type == `FAILURE` {
+			if result == `SUCCESS` {
+				result = `FAILURE`
+			}
+		}
+	}
+	if typeHasCi && typeHasCd {
+		domainPipeline.Type = `CI/CD`
+	} else if typeHasCi {
+		domainPipeline.Type = `CI`
+	} else if typeHasCd {
+		domainPipeline.Type = `CD`
+	}
+	domainPipeline.Result = result
+	domainPipeline.Status = ticket.DONE
+	now := time.Now()
+	domainPipeline.FinishedDate = &now
+	domainPipeline.DurationSec = uint64(domainPipeline.FinishedDate.Sub(domainPipeline.CreatedDate).Seconds())
+
+	// save
+	err = db.Update(domainPipeline)
 	if err != nil {
 		return nil, err
 	}
--- plugins/webhook/api/issue.go
+++ plugins/webhook/api/issue.go
@@ -65,7 +65,7 @@ type WebhookIssueRequest struct {
 // @Summary receive a record as defined and save it
 // @Description receive a record as follow and save it, example: {"board_key":"DLK","url":"","issue_key":"DLK-1234","title":"a feature from DLK","description":"","epic_key":"","type":"BUG","status":"TODO","original_status":"created","story_point":0,"resolution_date":null,"created_date":"2020-01-01T12:00:00+00:00","updated_date":null,"lead_time_minutes":0,"parent_issue_key":"DLK-1200","priority":"","original_estimate_minutes":0,"time_spent_minutes":0,"time_remaining_minutes":0,"creator_id":"user1131","creator_name":"Nick name 1","assignee_id":"user1132","assignee_name":"Nick name 2","severity":"","component":""}
 // @Tags plugins/webhook
-// @Param body body WebhookPipelineRequest true "json body"
+// @Param body body WebhookIssueRequest true "json body"
 // @Success 200  {string} noResponse ""
 // @Failure 400  {string} errcode.Error "Bad Request"
 // @Failure 500  {string} errcode.Error "Internal Error"
@@ -170,7 +170,7 @@ func CloseIssue(input *core.ApiResourceInput) (*core.ApiResourceOutput, errors.E
 	domainIssue := &ticket.Issue{}
 	err = db.First(domainIssue, dal.Where("id = ?", fmt.Sprintf("%s:%d:%s:%s", "webhook", connection.ID, input.Params[`boardKey`], input.Params[`issueId`])))
 	if err != nil {
-		return &core.ApiResourceOutput{Body: `issue not found:` + err.Error(), Status: http.StatusNotFound}, nil
+		return nil, errors.NotFound.Wrap(err, `issue not found`)
 	}
 	domainIssue.Status = ticket.DONE
 	domainIssue.OriginalStatus = ``
