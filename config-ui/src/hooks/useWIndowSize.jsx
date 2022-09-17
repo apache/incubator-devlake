a5fee73170ecff83db42f4b008354729e1ef1b5b
Henit Chobisa <chobisa.henit@gmail.com>
2022-09-17 10:40:28 +0530

    fix: Window Size hook added for menu visibility

---
+++ config-ui/src/hooks/useWIndowSize.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+function useWindowSize() {
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            function handleResize() {
+                // Set window width/height to state
+                setWindowSize({
+                    width: window.innerWidth,
+                    height: window.innerHeight,
+                });
+            }
+
+            window.addEventListener("resize", handleResize);
+
+            handleResize();
+
+            return () => window.removeEventListener("resize", handleResize);
+        }
+    }, []);
+    return windowSize;
+}
+
+export default useWindowSize;
