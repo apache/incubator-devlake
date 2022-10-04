8b1ab945c9eb0b4702c0d31435cab92f72602ab6
Henit Chobisa <chobisa.henit@gmail.com>
2022-10-04 23:22:16 +0530

    fix: window size hooks resolved

--- config-ui/src/hooks/useWIndowSize.jsx
+++ config-ui/src/hooks/useWIndowSize.jsx
@@ -24,23 +24,21 @@ function useWindowSize() {
     height: 240
   })
 
+  function handleResize() {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight
+    })
+  }
+
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      function handleResize() {
-        // Set window width/height to state
-        setWindowSize({
-          width: window.innerWidth,
-          height: window.innerHeight
-        })
-      }
-
       window.addEventListener('resize', handleResize)
-
       handleResize()
-
-      return () => window.removeEventListener('resize', handleResize)
     }
+    return () => window.removeEventListener('resize', handleResize)
   }, [])
+
   return windowSize
 }
 
