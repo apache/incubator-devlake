7582a78ac3f0a7d07be859bf5dc08146d4758351
Julien Chinapen <julien.chinapen@merico.dev>
2022-10-20 01:28:35 -0400

    chore: cleanup unused code in connection dialog

--- config-ui/src/components/blueprints/ConnectionDialog.jsx
+++ config-ui/src/components/blueprints/ConnectionDialog.jsx
@@ -30,15 +30,6 @@ import {
   MenuItem
 } from '@blueprintjs/core'
 import { Select } from '@blueprintjs/select'
-import {
-  Providers,
-  // ProviderTypes,
-  ProviderLabels
-  // ProviderFormLabels,
-  // ProviderFormPlaceholders,
-  // ProviderConnectionLimits
-  // ProviderIcons,
-} from '@/data/Providers'
 import { NullBlueprintConnection } from '@/data/NullBlueprintConnection'
 import InputValidationError from '@/components/validation/InputValidationError'
 import ContentLoader from '@/components/loaders/ContentLoader'
@@ -48,58 +39,6 @@ const Modes = {
   CREATE: 'create',
   EDIT: 'edit'
 }
-
-// @todo: lift data sources list to configuration level, requires expansion when more providers are added..
-// const DATA_SOURCES_LIST = [
-//   {
-//     id: 1,
-//     name: Providers.JIRA,
-//     title: ProviderLabels[Providers.JIRA.toUpperCase()],
-//     value: Providers.JIRA
-//   },
-//   {
-//     id: 2,
-//     name: Providers.GITHUB,
-//     title: ProviderLabels[Providers.GITHUB.toUpperCase()],
-//     value: Providers.GITHUB
-//   },
-//   {
-//     id: 3,
-//     name: Providers.GITLAB,
-//     title: ProviderLabels[Providers.GITLAB.toUpperCase()],
-//     value: Providers.GITLAB
-//   },
-//   {
-//     id: 4,
-//     name: Providers.JENKINS,
-//     title: ProviderLabels[Providers.JENKINS.toUpperCase()],
-//     value: Providers.JENKINS
-//   },
-//   {
-//     id: 5,
-//     name: Providers.TAPD,
-//     title: ProviderLabels[Providers.TAPD.toUpperCase()],
-//     value: Providers.TAPD
-//   },
-//   {
-//     id: 6,
-//     name: Providers.AZURE,
-//     title: ProviderLabels[Providers.AZURE.toUpperCase()],
-//     value: Providers.AZURE
-//   },
-//   {
-//     id: 7,
-//     name: Providers.BITBUCKET,
-//     title: ProviderLabels[Providers.BITBUCKET.toUpperCase()],
-//     value: Providers.BITBUCKET
-//   },
-//   {
-//     id: 8,
-//     name: Providers.GITEE,
-//     title: ProviderLabels[Providers.GITEE.toUpperCase()],
-//     value: Providers.GITEE
-//   }
-// ]
 
 const ConnectionDialog = (props) => {
   const {
