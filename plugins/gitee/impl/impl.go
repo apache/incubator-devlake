7620d8bb292f723faad573cccc368d58890fff26
Nddtfjiang <jzc001@qq.com>
2022-10-20 02:08:03 +0000

    refactor: refactor gitee migration scripts
    Refactor Gitee MigrationScripts
    
    Nddtfjiang <zhicheng.jiang@merico.dev>

--- plugins/gitee/impl/impl.go
+++ plugins/gitee/impl/impl.go
@@ -19,9 +19,9 @@ package impl
 
 import (
 	"fmt"
+
 	"github.com/apache/incubator-devlake/errors"
 
-	"github.com/apache/incubator-devlake/migration"
 	"github.com/apache/incubator-devlake/plugins/core"
 	"github.com/apache/incubator-devlake/plugins/gitee/api"
 	"github.com/apache/incubator-devlake/plugins/gitee/models"
@@ -36,7 +36,7 @@ var _ core.PluginMeta = (*Gitee)(nil)
 var _ core.PluginInit = (*Gitee)(nil)
 var _ core.PluginTask = (*Gitee)(nil)
 var _ core.PluginApi = (*Gitee)(nil)
-var _ core.Migratable = (*Gitee)(nil)
+var _ core.PluginMigration = (*Gitee)(nil)
 var _ core.CloseablePluginTask = (*Gitee)(nil)
 
 type Gitee string
@@ -188,7 +188,7 @@ func (plugin Gitee) RootPkgPath() string {
 	return "github.com/apache/incubator-devlake/plugins/gitee"
 }
 
-func (plugin Gitee) MigrationScripts() []migration.Script {
+func (plugin Gitee) MigrationScripts() []core.MigrationScript {
 	return migrationscripts.All()
 }
 
