da46a405539b722afdb5f7e3ccddeb03704a0e96
zhangliang <liang.zhang@merico.dev>
2022-11-15 12:03:53 +0800

    fix: fix lint error

--- runner/run_pipeline.go
+++ runner/run_pipeline.go
@@ -42,11 +42,25 @@ func RunPipeline(
 	if err != nil {
 		return errors.Convert(err)
 	}
-	taskIds := Get2DTaskIds(tasks)
-	return RunPipelineTasks(log, db, pipelineId, taskIds, runTasks)
+	sort.Slice(tasks, func(i, j int) bool {
+		if tasks[i].PipelineRow < tasks[j].PipelineRow {
+			return true
+		} else if tasks[i].PipelineRow == tasks[j].PipelineRow {
+			return tasks[i].PipelineCol < tasks[j].PipelineCol
+		}
+		return true
+	})
+	taskIds := make([][]uint64, 0)
+	for _, task := range tasks {
+		for len(taskIds) < task.PipelineRow {
+			taskIds = append(taskIds, make([]uint64, 0))
+		}
+		taskIds[task.PipelineRow-1] = append(taskIds[task.PipelineRow-1], task.ID)
+	}
+	return runPipelineTasks(log, db, pipelineId, taskIds, runTasks)
 }
 
-func RunPipelineTasks(
+func runPipelineTasks(
 	log core.Logger,
 	db *gorm.DB,
 	pipelineId uint64,
@@ -90,23 +104,4 @@ func RunPipelineTasks(
 	}
 	log.Info("pipeline finished in %d ms: %v", time.Now().UnixMilli()-dbPipeline.BeganAt.UnixMilli(), err)
 	return errors.Convert(err)
-}
-
-func Get2DTaskIds(tasks []models.Task) [][]uint64 {
-	sort.Slice(tasks, func(i, j int) bool {
-		if tasks[i].PipelineRow < tasks[j].PipelineRow {
-			return true
-		} else if tasks[i].PipelineRow == tasks[j].PipelineRow {
-			return tasks[i].PipelineCol < tasks[j].PipelineCol
-		}
-		return true
-	})
-	taskIds := make([][]uint64, 0)
-	for _, task := range tasks {
-		for len(taskIds) < task.PipelineRow {
-			taskIds = append(taskIds, make([]uint64, 0))
-		}
-		taskIds[task.PipelineRow-1] = append(taskIds[task.PipelineRow-1], task.ID)
-	}
-	return taskIds
 }
