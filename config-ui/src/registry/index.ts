96fec5aa07066756d7eaeb8014a1f4154b6f56dc
mintsweet <0x1304570@gmail.com>
2022-11-21 17:28:00 +0800

    feat(config-ui): encapsulate base layout component

---
+++ config-ui/src/registry/index.ts
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import { default as AE } from './plugins/ae.json'
+import { default as Azure } from './plugins/azure.json'
+import { default as BitBucket } from './plugins/bitbucket.json'
+import { default as DBT } from './plugins/dbt.json'
+import { default as DORA } from './plugins/dora.json'
+import { default as Feishu } from './plugins/feishu.json'
+import { default as Gitee } from './plugins/gitee.json'
+import { default as GitExtractor } from './plugins/gitextractor.json'
+import { default as GitHub } from './plugins/github.json'
+import { default as GitLab } from './plugins/gitlab.json'
+import { default as Jenkins } from './plugins/jenkins.json'
+import { default as JIRA } from './plugins/jira.json'
+import { default as Refdiff } from './plugins/refdiff.json'
+import { default as Starrocks } from './plugins/starrocks.json'
+import { default as TAPD } from './plugins/tapd.json'
+
+export const Plugins = [
+  AE,
+  Azure,
+  BitBucket,
+  DBT,
+  DORA,
+  Feishu,
+  Gitee,
+  GitExtractor,
+  GitHub,
+  GitLab,
+  Jenkins,
+  JIRA,
+  Refdiff,
+  Starrocks,
+  TAPD
+]
