dd6d4e3c54da1c8a530af1301ccdc9a3c123fbe2
mappjzc <zhicheng.jiang@merico.dev>
2022-12-19 18:49:00 +0800

    fix: support create bp without project (#3970)
    support create bp without project
    
    Nddtfjiang <zhicheng.jiang@merico.dev>

--- services/blueprint_makeplan_v200.go
+++ services/blueprint_makeplan_v200.go
@@ -41,12 +41,14 @@ func GeneratePlanJsonV200(
 	}
 	// refresh project_mapping table to reflect project/scopes relationship
 	if len(scopes) > 0 {
-		e := db.Where("project_name = ?", projectName).Delete(&crossdomain.ProjectMapping{}).Error
-		if e != nil {
-			return nil, errors.Default.Wrap(e, fmt.Sprintf("projectName:[%s]", projectName))
+		if projectName != "" {
+			e := db.Where("project_name = ?", projectName).Delete(&crossdomain.ProjectMapping{}).Error
+			if e != nil {
+				return nil, errors.Default.Wrap(e, fmt.Sprintf("projectName:[%s]", projectName))
+			}
 		}
 		for _, scope := range scopes {
-			e = basicRes.GetDal().CreateOrUpdate(scope)
+			e := basicRes.GetDal().CreateOrUpdate(scope)
 			if e != nil {
 				scopeInfo := fmt.Sprintf("[Id:%s][Name:%s][TableName:%s]", scope.ScopeId(), scope.ScopeName(), scope.TableName())
 				return nil, errors.Default.Wrap(e, fmt.Sprintf("failed to create scopes:[%s]", scopeInfo))
