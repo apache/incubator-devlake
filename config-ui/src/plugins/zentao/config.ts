b910ce5e25bafd70dd7299639b4678fec92471d8
Warren Chen <yingchu.chen@merico.dev>
2023-01-05 18:55:48 +0800

    feat(config-ui): turn on zentao and tapd (#4111)

--- config-ui/src/plugins/zentao/config.ts
+++ config-ui/src/plugins/zentao/config.ts
@@ -24,6 +24,52 @@ import Icon from './assets/icon.svg';
 export const ZenTaoConfig: PluginConfigType = {
   plugin: Plugins.ZenTao,
   name: 'ZenTao',
-  type: PluginType.Pipeline,
+  type: PluginType.Connection,
   icon: Icon,
+  connection: {
+    fields: [
+      {
+        key: 'name',
+        label: 'Connection Name',
+        type: 'text',
+        required: true,
+        placeholder: 'eg. ZenTao',
+      },
+      {
+        key: 'endpoint',
+        label: 'Endpoint URL',
+        type: 'text',
+        required: true,
+        placeholder: 'eg. https://your-domain:port/api.php/v1/',
+      },
+      {
+        key: 'username',
+        label: 'Username',
+        type: 'text',
+        required: true,
+        placeholder: 'eg. admin',
+      },
+      {
+        key: 'password',
+        label: 'Password',
+        type: 'password',
+        required: true,
+        placeholder: 'eg. ************',
+      },
+      {
+        key: 'proxy',
+        label: 'Proxy URL',
+        type: 'text',
+        placeholder: 'eg. http://proxy.localhost:8080',
+      },
+      {
+        key: 'rateLimitPerHour',
+        label: 'Rate Limit (per hour)',
+        type: 'numeric',
+        tooltip: 'Rate Limit requests per hour,\nEnter a numeric value > 0 to enable.',
+      },
+    ],
+  },
+  entities: ['TICKET'],
+  transformation: {},
 };
