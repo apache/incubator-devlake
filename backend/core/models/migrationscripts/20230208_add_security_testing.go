672e05d98e6032910bba2de0e2c54c5951a89e6b
Yingchu Chen <yingchu.chen@merico.dev>
2023-02-08 15:22:23 +0800

    feat(sonarqube): add issue convertor

--- backend/core/models/migrationscripts/20230208_add_security_testing.go
+++ backend/core/models/migrationscripts/20230208_add_security_testing.go
@@ -30,11 +30,12 @@ func (u *addSecurityTesting) Up(basicRes context.BasicRes) errors.Error {
 	return migrationhelper.AutoMigrateTables(
 		basicRes,
 		&archived.StProject{},
+		&archived.StIssue{},
 	)
 }
 
 func (*addSecurityTesting) Version() uint64 {
-	return 20230208000001
+	return 20230208000002
 }
 
 func (*addSecurityTesting) Name() string {
