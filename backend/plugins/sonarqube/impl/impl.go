7fff78f1ed3fe02850f46b43bf1d5e37306b474a
abeizn <zikuan.an@merico.dev>
2023-02-09 17:16:38 +0800

    feat: add sonarqube scope and fix some models

--- backend/plugins/sonarqube/impl/impl.go
+++ backend/plugins/sonarqube/impl/impl.go
@@ -165,7 +165,6 @@ func (p Sonarqube) ApiResources() map[string]map[string]plugin.ApiResourceHandle
 		"connections/:connectionId/scopes/:projectKey": {
 			"GET":   api.GetScope,
 			"PATCH": api.UpdateScope,
-			
 		},
 		"connections/:connectionId/scopes": {
 			"GET": api.GetScopeList,
