1407baaed50baa276961b59635719c126228cebf
Camille Teruel <camille.teruel@meri.co>
2023-02-21 15:51:18 +0100

    refactor: Use same struct for all plugin API areas

--- backend/server/services/remote/plugin/connection_api.go
+++ backend/server/services/remote/plugin/connection_api.go
@@ -18,31 +18,24 @@ limitations under the License.
 package plugin
 
 import (
-	"github.com/apache/incubator-devlake/core/errors"
-	"github.com/apache/incubator-devlake/core/models"
-	"github.com/apache/incubator-devlake/core/plugin"
-	"github.com/apache/incubator-devlake/helpers/pluginhelper/api"
-	"github.com/apache/incubator-devlake/server/services/remote/bridge"
 	"net/http"
+
+	"github.com/apache/incubator-devlake/core/errors"
+	"github.com/apache/incubator-devlake/core/plugin"
+	"github.com/apache/incubator-devlake/server/services/remote/bridge"
 )
 
-type ConnectionAPI struct {
-	invoker  bridge.Invoker
-	connType *models.DynamicTabler
-	helper   *api.ConnectionApiHelper
-}
-
-func (c *ConnectionAPI) TestConnection(input *plugin.ApiResourceInput) (*plugin.ApiResourceOutput, errors.Error) {
-	err := c.invoker.Call("test-connection", bridge.DefaultContext, input.Body).Get()
+func (pa *pluginAPI) TestConnection(input *plugin.ApiResourceInput) (*plugin.ApiResourceOutput, errors.Error) {
+	err := pa.invoker.Call("test-connection", bridge.DefaultContext, input.Body).Get()
 	if err != nil {
 		return nil, err
 	}
 	return nil, nil
 }
 
-func (c *ConnectionAPI) PostConnections(input *plugin.ApiResourceInput) (*plugin.ApiResourceOutput, errors.Error) {
-	connection := c.connType.New()
-	err := c.helper.Create(connection, input)
+func (pa *pluginAPI) PostConnections(input *plugin.ApiResourceInput) (*plugin.ApiResourceOutput, errors.Error) {
+	connection := pa.connType.New()
+	err := pa.helper.Create(connection, input)
 	if err != nil {
 		return nil, err
 	}
@@ -50,9 +43,9 @@ func (c *ConnectionAPI) PostConnections(input *plugin.ApiResourceInput) (*plugin
 	return &plugin.ApiResourceOutput{Body: conn, Status: http.StatusOK}, nil
 }
 
-func (c *ConnectionAPI) ListConnections(input *plugin.ApiResourceInput) (*plugin.ApiResourceOutput, errors.Error) {
-	connections := c.connType.NewSlice()
-	err := c.helper.List(connections)
+func (pa *pluginAPI) ListConnections(input *plugin.ApiResourceInput) (*plugin.ApiResourceOutput, errors.Error) {
+	connections := pa.connType.NewSlice()
+	err := pa.helper.List(connections)
 	if err != nil {
 		return nil, err
 	}
@@ -60,9 +53,9 @@ func (c *ConnectionAPI) ListConnections(input *plugin.ApiResourceInput) (*plugin
 	return &plugin.ApiResourceOutput{Body: conns}, nil
 }
 
-func (c *ConnectionAPI) GetConnection(input *plugin.ApiResourceInput) (*plugin.ApiResourceOutput, errors.Error) {
-	connection := c.connType.New()
-	err := c.helper.First(connection, input.Params)
+func (pa *pluginAPI) GetConnection(input *plugin.ApiResourceInput) (*plugin.ApiResourceOutput, errors.Error) {
+	connection := pa.connType.New()
+	err := pa.helper.First(connection, input.Params)
 	if err != nil {
 		return nil, err
 	}
@@ -70,9 +63,9 @@ func (c *ConnectionAPI) GetConnection(input *plugin.ApiResourceInput) (*plugin.A
 	return &plugin.ApiResourceOutput{Body: conn}, nil
 }
 
-func (c *ConnectionAPI) PatchConnection(input *plugin.ApiResourceInput) (*plugin.ApiResourceOutput, errors.Error) {
-	connection := c.connType.New()
-	err := c.helper.Patch(connection, input)
+func (pa *pluginAPI) PatchConnection(input *plugin.ApiResourceInput) (*plugin.ApiResourceOutput, errors.Error) {
+	connection := pa.connType.New()
+	err := pa.helper.Patch(connection, input)
 	if err != nil {
 		return nil, err
 	}
@@ -80,13 +73,13 @@ func (c *ConnectionAPI) PatchConnection(input *plugin.ApiResourceInput) (*plugin
 	return &plugin.ApiResourceOutput{Body: conn, Status: http.StatusOK}, nil
 }
 
-func (c *ConnectionAPI) DeleteConnection(input *plugin.ApiResourceInput) (*plugin.ApiResourceOutput, errors.Error) {
-	connection := c.connType.New()
-	err := c.helper.First(connection, input.Params)
+func (pa *pluginAPI) DeleteConnection(input *plugin.ApiResourceInput) (*plugin.ApiResourceOutput, errors.Error) {
+	connection := pa.connType.New()
+	err := pa.helper.First(connection, input.Params)
 	if err != nil {
 		return nil, err
 	}
-	err = c.helper.Delete(connection)
+	err = pa.helper.Delete(connection)
 	conn := connection.Unwrap()
 	return &plugin.ApiResourceOutput{Body: conn}, err
 }
