ae57df9a15103871477e1002e97beefbc6edefb0
abeizn <zikuan.an@merico.dev>
2023-02-22 16:57:40 +0800

    fix: largest files with lowest number of authors dashboard can not show (#4483)
    * fix: largest files with lowest number of authors dashboard can not show
    
    * fix: add commit sha index by gorm
    
    * fix: add commit sha index by gorm

---
+++ backend/core/models/migrationscripts/20230222_add_commit_sha_index.go
@@ -0,0 +1,53 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package migrationscripts
+
+import (
+	"github.com/apache/incubator-devlake/core/context"
+	"github.com/apache/incubator-devlake/core/errors"
+	"github.com/apache/incubator-devlake/core/plugin"
+	"github.com/apache/incubator-devlake/helpers/migrationhelper"
+)
+
+var _ plugin.MigrationScript = (*addCommitShaIndex)(nil)
+
+type commitFile20230222 struct {
+	CommitSha string `gorm:"index;type:varchar(40)"`
+}
+
+func (commitFile20230222) TableName() string {
+	return "commit_files"
+}
+
+type addCommitShaIndex struct{}
+
+func (script *addCommitShaIndex) Up(basicRes context.BasicRes) errors.Error {
+
+	return migrationhelper.AutoMigrateTables(
+		basicRes,
+		&commitFile20230222{},
+	)
+}
+
+func (*addCommitShaIndex) Version() uint64 {
+	return 20230222145127
+}
+
+func (*addCommitShaIndex) Name() string {
+	return "add commit_sha index for commit_files"
+}
