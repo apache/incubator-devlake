d66f67573cefe055cc8e73ee2df47dea39c6bb25
mintsweet <0x1304570@gmail.com>
2023-02-23 11:16:04 +0800

    feat(config-ui): use icon-button replace button in connection

--- config-ui/src/pages/connection/list/connection.tsx
+++ config-ui/src/pages/connection/list/connection.tsx
@@ -21,7 +21,7 @@ import { useHistory } from 'react-router-dom';
 import { ButtonGroup, Button, Intent, Position } from '@blueprintjs/core';
 import { Popover2 } from '@blueprintjs/popover2';
 
-import { Table, ColumnType } from '@/components';
+import { Table, ColumnType, IconButton } from '@/components';
 import type { ConnectionItemType } from '@/store';
 import { useConnection, ConnectionStatus } from '@/store';
 import { operator } from '@/utils';
@@ -91,7 +91,7 @@ export const Connection = ({ plugin }: Props) => {
           key: 'action',
           render: (id) => (
             <ButtonGroup>
-              <Button minimal intent={Intent.PRIMARY} icon="edit" onClick={() => handleUpdate(id)} />
+              <IconButton icon="edit" tooltip="Edit" onClick={() => handleUpdate(id)} />
               <Popover2
                 position={Position.TOP}
                 content={
@@ -109,7 +109,7 @@ export const Connection = ({ plugin }: Props) => {
                   </S.DeleteConfirm>
                 }
               >
-                <Button minimal intent={Intent.PRIMARY} icon="delete" />
+                <IconButton icon="delete" tooltip="Delete" />
               </Popover2>
             </ButtonGroup>
           ),
