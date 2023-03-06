ca801329379395b5ab92f21754f459ee850ede9a
Camille Teruel <camille.teruel@meri.co>
2023-03-06 16:08:32 +0100

    fix: Fix base_url and improve APIBase.get

--- backend/python/plugins/azure/azure/api.py
+++ backend/python/plugins/azure/azure/api.py
@@ -1,8 +1,7 @@
 from typing import Optional
 import base64
-import typing
 
-from pydevlake.api import API, request_hook, Paginator, response_hook, Response, Request
+from pydevlake.api import API, request_hook, Paginator, Request
 
 
 class AzurePaginator(Paginator):
@@ -23,66 +22,55 @@ class AzureDevOpsAPI(API):
         self.base_url = base_url
         self.pat = pat
 
-    def base_url(self):
-        return self.base_url
-
-    @response_hook  # how to use this?
-    def on_response(self, response: Response):
-        return response
-
-    @response_hook  # how to use this?
-    def rate_limiter(self, response: Response):
-        # TODO
-        return response
-
     @request_hook
     def authenticate(self, request: Request):
         pat_b64 = base64.b64encode((':' + self.pat).encode()).decode()
         if self.pat:
             request.headers['Authorization'] = 'Basic ' + pat_b64
 
-    def accounts(self) -> Response:
-        return self.get('https://app.vssps.visualstudio.com', '_apis/accounts')
+    def accounts(self):
+        req = Request('https://app.vssps.visualstudio.com/_apis/accounts')
+        return self.send(req)
 
     def orgs(self) -> list[str]:
         response = self.accounts()
         return [acct["AccountName"] for acct in response.json]
 
-    def projects(self, org: str) -> Response:
-        return self.get(f'{org}/_apis/projects')
+    def projects(self, org: str):
+        return self.get(org, '_apis/projects')
 
     # Get a project
-    def project(self, org: str, project: str) -> Response:
-        return self.get(f'{org}/_apis/projects/{project}')
+    def project(self, org: str, project: str):
+        return self.get(org, '_apis/projects', project)
 
     # List repos under an org
-    def git_repos(self, org: str, project: str) -> Response:
-        return self.get(f'{org}/{project}/_apis/git/repositories')
+    def git_repos(self, org: str, project: str):
+        return self.get(org, project, '_apis/git/repositories')
 
-    def git_repo_pull_requests(self, org: str, project: str, repo_id: str) -> Response:
+    def git_repo_pull_requests(self, org: str, project: str, repo_id: str):
         # see https://learn.microsoft.com/en-us/rest/api/azure/devops/git/pull-requests/get-pull-requests?view=azure-devops-rest-7.1&tabs=HTTP
-        return self.get(f'{org}/{project}/_apis/git/repositories/{repo_id}/pullrequests')
+        return self.get(org, project, '_apis/git/repositories', repo_id, 'pullrequests')
 
-    def git_repo_pull_request_commits(self, org: str, project: str, repo_id: str, pull_request_id: int) -> Response:
-        return self.get(f'{org}/{project}/_apis/git/repositories/{repo_id}/pullRequests/{pull_request_id}/commits')
+    def git_repo_pull_request_commits(self, org: str, project: str, repo_id: str, pull_request_id: int):
+        return self.get(org, project, '_apis/git/repositories', repo_id, 'pullRequests', pull_request_id, 'commits')
 
-    def git_repo_pull_request_comments(self, org: str, project: str, repo_id: str, pull_request_id: int) -> Response:
-        return self.get(f'{org}/{project}/_apis/git/repositories/{repo_id}/pullRequests/{pull_request_id}/threads')
+    def git_repo_pull_request_comments(self, org: str, project: str, repo_id: str, pull_request_id: int):
+        return self.get(org, project, '_apis/git/repositories', repo_id, 'pullRequests', pull_request_id, 'threads')
 
     # not needed
-    def commits(self, org: str, project: str, repo_id: str) -> Response:
-        return self.get(f'{org}/{project}/_apis/git/repositories/{repo_id}/commits')
+    def commits(self, org: str, project: str, repo_id: str):
+        return self.get(org, project, '_apis/git/repositories', repo_id, 'commits')
 
-    def builds(self, org: str, project: str) -> Response:
-        return self.get(f'{org}/{project}/_apis/build/builds')
+    def builds(self, org: str, project: str):
+        return self.get(org, project, '_apis/build/builds')
 
-    def jobs(self, org: str, project: str, build_id: int) -> Response:
-        return self.get(f'{org}/{project}/_apis/build/builds/{build_id}/timeline')
-
-    # unused
-    def deployments(self, org: str, project: str) -> Response:
-        return self.get(f'{org}/{project}/_apis/release/deployments')
+    def jobs(self, org: str, project: str, build_id: int):
+        return self.get(org, project, '_apis/build/builds', build_id, 'timeline')
 
     # unused
-    def releases(self, org: str, project: str) -> Response:
-        return self.get(f'{org}/{project}/_apis/release/releases')
+    def deployments(self, org: str, project: str):
+        return self.get(org, project, '_apis/release/deployments')
+
+    # unused
+    def releases(self, org: str, project: str):
+        return self.get(org, project, '_apis/release/releases')
