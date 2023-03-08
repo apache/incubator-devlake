580df2852ba420f027115619c07dc9be381c812e
Camille Teruel <camille.teruel@meri.co>
2023-03-08 03:28:31 +0100

    fix: Add missing Licences

--- backend/python/plugins/azure/azure/streams/pull_request_commits.py
+++ backend/python/plugins/azure/azure/streams/pull_request_commits.py
@@ -1,3 +1,18 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from typing import Iterable
 
 from azure.api import AzureDevOpsAPI
@@ -6,7 +21,6 @@ from azure.streams.commits import extract_raw_commit
 from azure.streams.pull_requests import GitPullRequests
 from pydevlake import Substream, DomainType
 from pydevlake.domain_layer.code import PullRequestCommit as DomainPullRequestCommit
-
 
 
 class GitPullRequestCommits(Substream):
