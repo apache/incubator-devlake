4b0554122210069aa077e5ae16dc2ae6ed7003a9
abeizn <zikuan.an@merico.dev>
2023-03-15 20:11:53 +0800

    feat: enhance timeFilter/diffSync support for jenkins and github plugin and format

--- backend/plugins/github/tasks/cicd_run_collector.go
+++ backend/plugins/github/tasks/cicd_run_collector.go
@@ -40,6 +40,16 @@ const RAW_RUN_TABLE = "github_api_runs"
 // https://github.com/apache/incubator-devlake/issues/3199
 const PAGE_SIZE = 30
 
+type GithubRawRunsResult struct {
+	TotalCount         int64             `json:"total_count"`
+	GithubWorkflowRuns []json.RawMessage `json:"workflow_runs"`
+}
+
+type SimpleGithubApiJob struct {
+	ID        int64
+	CreatedAt helper.Iso8601Time `json:"created_at"`
+}
+
 var CollectRunsMeta = plugin.SubTaskMeta{
 	Name:             "collectRuns",
 	EntryPoint:       CollectRuns,
@@ -50,124 +60,8 @@ var CollectRunsMeta = plugin.SubTaskMeta{
 
 func CollectRuns(taskCtx plugin.SubTaskContext) errors.Error {
 	data := taskCtx.GetData().(*GithubTaskData)
-	collectorWithState, err := helper.NewStatefulApiCollector(helper.RawDataSubTaskArgs{
-		Ctx: taskCtx,
-		Params: GithubApiParams{
-			ConnectionId: data.Options.ConnectionId,
-			Name:         data.Options.Name,
-		},
-		Table: RAW_RUN_TABLE,
-	}, data.TimeAfter)
-	if err != nil {
-		return err
-	}
-	incremental := collectorWithState.IsIncremental()
-	// step 1: fetch records created after createdAfter
-	var createdAfter *time.Time
-	if incremental {
-		createdAfter = collectorWithState.LatestState.LatestSuccessStart
-	} else {
-		createdAfter = data.TimeAfter
-	}
-	err = collectorWithState.InitCollector(helper.ApiCollectorArgs{
-		ApiClient:   data.ApiClient,
-		PageSize:    PAGE_SIZE,
-		Incremental: incremental,
-		UrlTemplate: "repos/{{ .Params.Name }}/actions/runs",
-		Query: func(reqData *helper.RequestData) (url.Values, errors.Error) {
-			query := url.Values{}
-			query.Set("page", fmt.Sprintf("%v", reqData.Pager.Page))
-			query.Set("per_page", fmt.Sprintf("%v", reqData.Pager.Size))
-			return query, nil
-		},
-		// use Undetermined strategy so we can stop fetching further pages by using
-		// ErrFinishCollect
-		Concurrency: 10,
-		ResponseParser: func(res *http.Response) ([]json.RawMessage, errors.Error) {
-			body := &GithubRawRunsResult{}
-			err := helper.UnmarshalResponse(res, body)
-			if err != nil {
-				return nil, err
-			}
-
-			if len(body.GithubWorkflowRuns) == 0 {
-				return nil, nil
-			}
-
-			// time filter or diff sync
-			if createdAfter != nil {
-				// if the first record of the page was created before minCreated, return emtpy set and stop
-				firstRun := &models.GithubRun{}
-				if e := json.Unmarshal(body.GithubWorkflowRuns[0], firstRun); e != nil {
-					return nil, errors.Default.Wrap(e, "failed to unmarshal first run")
-				}
-				if firstRun.GithubCreatedAt.Before(*createdAfter) {
-					return nil, helper.ErrFinishCollect
-				}
-				// if the last record was created before minCreated, return records and stop
-				lastRun := &models.GithubRun{}
-				if e := json.Unmarshal(body.GithubWorkflowRuns[len(body.GithubWorkflowRuns)-1], lastRun); e != nil {
-					return nil, errors.Default.Wrap(e, "failed to unmarshal last run")
-				}
-				if lastRun.GithubCreatedAt.Before(*createdAfter) {
-					err = helper.ErrFinishCollect
-				}
-			}
-
-			return body.GithubWorkflowRuns, err
-		},
-	})
-
-	if err != nil {
-		return err
-	}
-
-	err = collectorWithState.Execute()
-	if err != nil {
-		return err
-	}
-
-	// step 2: for incremental collection, we have to update previous collected data which status is unfinished
-	if incremental {
-		// update existing data by collecting unfinished runs prior to LatestState.LatestSuccessStart
-		return collectUnfinishedRuns(taskCtx)
-	}
-	return nil
-}
-
-type GithubRawRunsResult struct {
-	TotalCount         int64             `json:"total_count"`
-	GithubWorkflowRuns []json.RawMessage `json:"workflow_runs"`
-}
-
-type SimpleGithubApiJob struct {
-	GithubId  int
-	CreatedAt helper.Iso8601Time `json:"created_at"`
-}
-
-func collectUnfinishedRuns(taskCtx plugin.SubTaskContext) errors.Error {
-	data := taskCtx.GetData().(*GithubTaskData)
 	db := taskCtx.GetDal()
-
-	// load unfinished runs from the database
-	cursor, err := db.Cursor(
-		dal.Select("id"),
-		dal.From(&models.GithubRun{}),
-		dal.Where(
-			"repo_id = ? AND connection_id = ? AND status IN ('ACTION_REQUIRED', 'STALE', 'IN_PROGRESS', 'QUEUED', 'REQUESTED', 'WAITING', 'PENDING')",
-			data.Options.GithubId, data.Options.ConnectionId,
-		),
-	)
-	if err != nil {
-		return err
-	}
-	iterator, err := helper.NewDalCursorIterator(db, cursor, reflect.TypeOf(SimpleGithubRun{}))
-	if err != nil {
-		return err
-	}
-
-	// collect details from api
-	collector, err := helper.NewApiCollector(helper.ApiCollectorArgs{
+	collector, err := helper.NewStatefulApiCollectorForFinalizableEntity(helper.FinalizableApiCollectorArgs{
 		RawDataSubTaskArgs: helper.RawDataSubTaskArgs{
 			Ctx: taskCtx,
 			Params: GithubApiParams{
@@ -176,23 +70,76 @@ func collectUnfinishedRuns(taskCtx plugin.SubTaskContext) errors.Error {
 			},
 			Table: RAW_RUN_TABLE,
 		},
-		ApiClient:   data.ApiClient,
-		Input:       iterator,
-		Incremental: true,
-		UrlTemplate: "repos/{{ .Params.Name }}/actions/runs/{{ .Input.ID }}",
-		ResponseParser: func(res *http.Response) ([]json.RawMessage, errors.Error) {
-			body, err := io.ReadAll(res.Body)
-			if err != nil {
-				return nil, errors.Convert(err)
-			}
-			res.Body.Close()
-			return []json.RawMessage{body}, nil
+		ApiClient: data.ApiClient,
+		TimeAfter: data.TimeAfter,
+		CollectNewRecordsByList: helper.FinalizableApiCollectorListArgs{
+			PageSize:    PAGE_SIZE,
+			Concurrency: 10,
+			FinalizableApiCollectorCommonArgs: helper.FinalizableApiCollectorCommonArgs{
+				UrlTemplate: "repos/{{ .Params.Name }}/actions/runs",
+				Query: func(reqData *helper.RequestData, createdAfter *time.Time) (url.Values, errors.Error) {
+					query := url.Values{}
+					query.Set("page", fmt.Sprintf("%v", reqData.Pager.Page))
+					query.Set("per_page", fmt.Sprintf("%v", reqData.Pager.Size))
+					return query, nil
+				},
+				ResponseParser: func(res *http.Response) ([]json.RawMessage, errors.Error) {
+					body := &GithubRawRunsResult{}
+					err := helper.UnmarshalResponse(res, body)
+					if err != nil {
+						return nil, err
+					}
+					if len(body.GithubWorkflowRuns) == 0 {
+						return nil, nil
+					}
+					return body.GithubWorkflowRuns, nil
+				},
+			},
+			GetCreated: func(item json.RawMessage) (time.Time, errors.Error) {
+				pj := &SimpleGithubApiJob{}
+				err := json.Unmarshal(item, pj)
+				if err != nil {
+					return time.Time{}, errors.BadInput.Wrap(err, "failed to unmarshal github run")
+				}
+				return pj.CreatedAt.ToTime(), nil
+			},
 		},
-		AfterResponse: ignoreHTTPStatus404,
+		CollectUnfinishedDetails: helper.FinalizableApiCollectorDetailArgs{
+			BuildInputIterator: func() (helper.Iterator, errors.Error) {
+				// load unfinished runs from the database
+				cursor, err := db.Cursor(
+					dal.Select("id"),
+					dal.From(&models.GithubRun{}),
+					dal.Where(
+						"repo_id = ? AND connection_id = ? AND status IN ('ACTION_REQUIRED', 'STALE', 'IN_PROGRESS', 'QUEUED', 'REQUESTED', 'WAITING', 'PENDING')",
+						data.Options.GithubId, data.Options.ConnectionId,
+					),
+				)
+				if err != nil {
+					return nil, err
+				}
+				return helper.NewDalCursorIterator(db, cursor, reflect.TypeOf(SimpleGithubApiJob{}))
+			},
+
+			FinalizableApiCollectorCommonArgs: helper.FinalizableApiCollectorCommonArgs{
+				UrlTemplate: "repos/{{ .Params.Name }}/actions/runs/{{ .Input.ID }}",
+				ResponseParser: func(res *http.Response) ([]json.RawMessage, errors.Error) {
+					body, err := io.ReadAll(res.Body)
+					if err != nil {
+						return nil, errors.Convert(err)
+					}
+					res.Body.Close()
+					return []json.RawMessage{body}, nil
+				},
+				AfterResponse: ignoreHTTPStatus404,
+			},
+		},
 	})
 
 	if err != nil {
 		return err
 	}
+
 	return collector.Execute()
+
 }
