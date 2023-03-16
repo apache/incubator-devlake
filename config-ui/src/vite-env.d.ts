ec9d0ce490b029d4ea4c252cea173775e47d6074
mintsweet <0x1304570@gmail.com>
2023-03-16 09:33:40 +0800

    fix(config-ui): some missed issues

--- config-ui/src/vite-env.d.ts
+++ config-ui/src/vite-env.d.ts
@@ -16,28 +16,4 @@
  *
  */
 
-import path from 'path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-
-  server: {
-    port: 4000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-
-  resolve: {
-    alias: {
-      '@': path.join(__dirname, './src'),
-    },
-  },
-});
+/// <reference types="vite/client" />
