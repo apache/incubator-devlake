9f5642c15ee7dcf717ff67cc92d2540ca475dbe4
Camille Teruel <camille.teruel@meri.co>
2023-03-18 03:22:15 +0100

    fix: Include closed PRs
    Azure devops API excludes closed PRs by default.
    Also merge commit is always presents if PR can be merged even if it is not.

--- backend/python/plugins/azuredevops/azuredevops/streams/pull_requests.py
+++ backend/python/plugins/azuredevops/azuredevops/streams/pull_requests.py
@@ -22,7 +22,7 @@ from azuredevops.api import AzureDevOpsAPI
 from azuredevops.helper import db
 from azuredevops.models import GitRepository, GitPullRequest, GitCommit
 from pydevlake import Stream, Context, DomainType
-from pydevlake.domain_layer.code import PullRequest as DomainPullRequest
+import pydevlake.domain_layer.code as code
 
 
 class GitPullRequests(Stream):
@@ -30,7 +30,6 @@ class GitPullRequests(Stream):
     domain_types = [DomainType.CODE]
 
     def collect(self, state, context) -> Iterable[tuple[object, dict]]:
-        connection = context.connection
         api = AzureDevOpsAPI(context.connection)
         repo: GitRepository = context.scope
         response = api.git_repo_pull_requests(repo.org_id, repo.project_id, repo.id)
@@ -47,15 +46,12 @@ class GitPullRequests(Stream):
         pr.creation_date = iso8601.parse_date(raw_data["creationDate"])
         pr.code_review_id = raw_data["codeReviewId"]
         pr.repo_id = raw_data["repository"]["id"]
-        pr.title = raw_data["title"]
         pr.description = raw_data["description"]
         pr.source_commit_sha = raw_data["lastMergeSourceCommit"]["commitId"]
         pr.target_commit_sha = raw_data["lastMergeTargetCommit"]["commitId"]
         pr.merge_commit_sha = raw_data["lastMergeCommit"]["commitId"]
         pr.source_ref_name = raw_data["sourceRefName"]
         pr.target_ref_name = raw_data["targetRefName"]
-        pr.status = raw_data["status"]
-        pr.url = raw_data["url"]
         if "labels" in raw_data:
             # TODO get this off transformation rules regex
             pr.type = raw_data["labels"][0]["name"]
@@ -63,13 +59,8 @@ class GitPullRequests(Stream):
             pr.fork_repo_id = raw_data["forkSource"]["repository"]["id"]
         return pr
 
-    def convert(self, pr: GitPullRequest, context: Context) -> Iterable[DomainPullRequest]:
-        merged_date: datetime = None
-        if pr.status == GitPullRequest.Status.Completed:
-            # query from commits
-            merge_commit: GitCommit = db.get(context, GitCommit, GitCommit.commit_sha == pr.merge_commit_sha)
-            merged_date = merge_commit.commit_date
-        yield DomainPullRequest(
+    def convert(self, pr: GitPullRequest, ctx):
+        yield code.PullRequest(
             base_repo_id=(pr.fork_repo_id if pr.fork_repo_id is not None else pr.repo_id),
             head_repo_id=pr.repo_id,
             status=pr.status.value,
@@ -80,7 +71,7 @@ class GitPullRequests(Stream):
             author_id=pr.created_by_id,
             pull_request_key=pr.id,
             created_date=pr.creation_date,
-            merged_date=merged_date,
+            merged_date=pr.closed_date,
             closed_date=pr.closed_date,
             type=pr.type,
             component="", # not supported
