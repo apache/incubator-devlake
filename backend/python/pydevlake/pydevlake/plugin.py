54d3badf166940668d28e551cc92118a66e0a014
Camille Teruel <camille.teruel@meri.co>
2023-03-29 19:02:27 +0200

    test: Use pydevlake testing facilities to test AzureDevOps plugin

--- backend/python/pydevlake/pydevlake/plugin.py
+++ backend/python/pydevlake/pydevlake/plugin.py
@@ -108,19 +108,20 @@ class Plugin(ABC):
 
     def make_remote_scopes(self, connection: Connection, group_id: Optional[str] = None) -> msg.RemoteScopes:
         if group_id:
-            scopes = [
-                msg.RemoteScope(
-                    id=tool_scope.id,
-                    parent_id=group_id,
-                    name=tool_scope.name,
-                    scope=tool_scope
+            remote_scopes = []
+            for tool_scope in self.remote_scopes(connection, group_id):
+                tool_scope.connection_id = connection.id
+                remote_scopes.append(
+                    msg.RemoteScope(
+                        id=tool_scope.id,
+                        parent_id=group_id,
+                        name=tool_scope.name,
+                        scope=tool_scope
+                    )
                 )
-                for tool_scope
-                in self.remote_scopes(connection, group_id)
-            ]
         else:
-            scopes = self.remote_scope_groups(connection)
-        return msg.RemoteScopes(__root__=scopes)
+            remote_scopes = self.remote_scope_groups(connection)
+        return msg.RemoteScopes(__root__=remote_scopes)
 
     def make_pipeline(self, scope_tx_rule_pairs: list[ScopeTxRulePair],
                       entity_types: list[str], connection: Connection):
