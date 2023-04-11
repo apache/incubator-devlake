922e81f7cdb89980028b2fb037cc8cc6d04f45c4
Yingchu Chen <yingchu.chen@merico.dev>
2023-04-11 20:28:54 +0800

    fix(configui): delete cookie storage

--- config-ui/src/aws-exports.ts
+++ config-ui/src/aws-exports.ts
@@ -19,12 +19,5 @@ export const awsExports = {
   region: import.meta.env.VITE_AUTH_REGION,
   userPoolId: import.meta.env.VITE_AUTH_USER_POOL_ID,
   userPoolWebClientId: import.meta.env.VITE_AUTH_USER_POOL_WEB_CLIENT_ID,
-  cookieStorage: {
-    domain: import.meta.env.VITE_AUTH_COOKIE_STORAGE_DOMAIN,
-    path: '/',
-    expires: 365,
-    sameSite: 'strict',
-    secure: true,
-  },
   authenticationFlowType: 'USER_SRP_AUTH',
 };
