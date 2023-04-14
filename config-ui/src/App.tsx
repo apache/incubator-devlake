bae5319b8cb012f9fd06b3b563110cb670dd9186
青湛 <0x1304570@gmail.com>
2023-04-14 17:41:33 +0800

    feat(config-ui): adjust the config-ui about cognito (#4933)

--- config-ui/src/App.tsx
+++ config-ui/src/App.tsx
@@ -37,30 +37,45 @@ import {
 function App() {
   return (
     <Router history={history}>
-      <BaseLayout>
-        <Switch>
-          <Route path="/" exact component={() => <Redirect to="/projects" />} />
-          <Route exact path="/login" component={() => <LoginPage />} />
-          <Route exact path="/projects" component={() => <ProjectHomePage />} />
-          <Route exact path="/projects/:pname" component={() => <ProjectDetailPage />} />
-          <Route exact path="/projects/:pname/:bid/connection-add" component={() => <BlueprintConnectioAddPage />} />
-          <Route exact path="/projects/:pname/:bid/:unique" component={() => <BlueprintConnectionDetailPage />} />
-          <Route
-            exact
-            path="/projects/:pname/create-blueprint"
-            component={() => <BlueprintCreatePage from={FromEnum.project} />}
-          />
-          <Route exact path="/connections" component={() => <ConnectionHomePage />} />
-          <Route exact path="/connections/:plugin" component={() => <ConnectionListPage />} />
-          <Route exact path="/connections/:plugin/create" component={() => <ConnectionFormPage />} />
-          <Route exact path="/connections/:plugin/:cid" component={() => <ConnectionFormPage />} />
-          <Route exact path="/blueprints" component={() => <BlueprintHomePage />} />
-          <Route exact path="/blueprints/create" component={() => <BlueprintCreatePage from={FromEnum.blueprint} />} />
-          <Route exact path="/blueprints/:id" component={() => <BlueprintDetailPage />} />
-          <Route exact path="/blueprints/:bid/connection-add" component={() => <BlueprintConnectioAddPage />} />
-          <Route exact path="/blueprints/:bid/:unique" component={() => <BlueprintConnectionDetailPage />} />
-        </Switch>
-      </BaseLayout>
+      <Switch>
+        <Route exact path="/login" component={() => <LoginPage />} />
+        <Route
+          path="/"
+          component={() => (
+            <BaseLayout>
+              <Switch>
+                <Route exact path="/" component={() => <Redirect to="/projects" />} />
+                <Route exact path="/projects" component={() => <ProjectHomePage />} />
+                <Route exact path="/projects/:pname" component={() => <ProjectDetailPage />} />
+                <Route
+                  exact
+                  path="/projects/:pname/:bid/connection-add"
+                  component={() => <BlueprintConnectioAddPage />}
+                />
+                <Route exact path="/projects/:pname/:bid/:unique" component={() => <BlueprintConnectionDetailPage />} />
+                <Route
+                  exact
+                  path="/projects/:pname/create-blueprint"
+                  component={() => <BlueprintCreatePage from={FromEnum.project} />}
+                />
+                <Route exact path="/connections" component={() => <ConnectionHomePage />} />
+                <Route exact path="/connections/:plugin" component={() => <ConnectionListPage />} />
+                <Route exact path="/connections/:plugin/create" component={() => <ConnectionFormPage />} />
+                <Route exact path="/connections/:plugin/:cid" component={() => <ConnectionFormPage />} />
+                <Route exact path="/blueprints" component={() => <BlueprintHomePage />} />
+                <Route
+                  exact
+                  path="/blueprints/create"
+                  component={() => <BlueprintCreatePage from={FromEnum.blueprint} />}
+                />
+                <Route exact path="/blueprints/:id" component={() => <BlueprintDetailPage />} />
+                <Route exact path="/blueprints/:bid/connection-add" component={() => <BlueprintConnectioAddPage />} />
+                <Route exact path="/blueprints/:bid/:unique" component={() => <BlueprintConnectionDetailPage />} />
+              </Switch>
+            </BaseLayout>
+          )}
+        />
+      </Switch>
     </Router>
   );
 }
