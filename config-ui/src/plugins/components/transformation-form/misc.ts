24abc34e0b5573997f8bb4c15b70ef1a29ed1327
青湛 <0x1304570@gmail.com>
2023-05-05 09:36:18 +0800

    fix(config-ui): update doc link for transformation (#5090)

--- config-ui/src/plugins/components/transformation-form/misc.ts
+++ config-ui/src/plugins/components/transformation-form/misc.ts
@@ -19,10 +19,30 @@
 export const TIPS_MAP: Record<string, { name: string; link: string }> = {
   github: {
     name: 'GitHub',
-    link: 'https://devlake.apache.org/docs/UserManuals/ConfigUI/GitHub#step-3---adding-transformation-rules-optional',
+    link: 'https://devlake.apache.org/docs/Configuration/GitHub#step-3---adding-transformation-rules-optional',
+  },
+  gitlab: {
+    name: 'GitLab',
+    link: 'https://devlake.apache.org/docs/Configuration/GitLab#step-3---adding-transformation-rules-optional'
   },
   jira: {
     name: 'Jira',
-    link: 'https://devlake.apache.org/docs/UserManuals/ConfigUI/Jira#step-3---adding-transformation-rules-optional',
+    link: 'https://devlake.apache.org/docs/Configuration/Jira#step-3---adding-transformation-rules-optional',
+  },
+  jenkins: {
+    name: 'Jenkins',
+    link: 'https://devlake.apache.org/docs/Configuration/Jenkins#step-3---adding-transformation-rules-optional'
+  },
+  bitbucket: {
+    name: 'BitBucket',
+    link: 'https://devlake.apache.org/docs/Configuration/BitBucket#step-3---adding-transformation-rules-optional'
+  },
+  azuredevops: {
+    name: 'Azure DevOps',
+    link: 'https://devlake.apache.org/docs/Configuration/Jenkins#step-3---adding-transformation-rules-optional'
+  },
+  tapd: {
+    name:'TAPD',
+    link: 'https://devlake.apache.org/docs/Configuration/Tapd#step-3---adding-transformation-rules-optional'
   },
 };
