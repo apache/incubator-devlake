81b208af9e4b1a15921e7b39290d25eeb67927bb
mappjzc <zhicheng.jiang@merico.dev>
2023-05-06 16:24:40 +0800

    fix: fix zentao ignore half (#5116)
    Cherry pick for #5092
    
    Nddtfjiang <zhicheng.jiang@merico.dev>

--- backend/plugins/zentao/models/project.go
+++ backend/plugins/zentao/models/project.go
@@ -19,6 +19,7 @@ package models
 
 import (
 	"fmt"
+
 	"github.com/apache/incubator-devlake/core/models/common"
 	"github.com/apache/incubator-devlake/core/plugin"
 	helper "github.com/apache/incubator-devlake/helpers/pluginhelper/api"
@@ -26,88 +27,89 @@ import (
 
 type ZentaoProject struct {
 	common.NoPKModel `json:"-"`
-	ConnectionId     uint64              `json:"connectionid" gorm:"primaryKey;type:BIGINT  NOT NULL"`
-	Id               int64               `json:"id" gorm:"primaryKey;type:BIGINT  NOT NULL"`
-	Project          int64               `json:"project"`
-	Model            string              `json:"model"`
-	Type             string              `json:"type"`
-	Lifetime         string              `json:"lifetime"`
-	Budget           string              `json:"budget"`
-	BudgetUnit       string              `json:"budgetUnit"`
-	Attribute        string              `json:"attribute"`
-	Percent          int                 `json:"percent"`
-	Milestone        string              `json:"milestone"`
-	Output           string              `json:"output"`
-	Auth             string              `json:"auth"`
-	Parent           int64               `json:"parent"`
-	Path             string              `json:"path"`
-	Grade            int                 `json:"grade"`
-	Name             string              `json:"name"`
-	Code             string              `json:"code"`
-	PlanBegin        *helper.Iso8601Time `json:"begin"`
-	PlanEnd          *helper.Iso8601Time `json:"end"`
-	RealBegan        *helper.Iso8601Time `json:"realBegan"`
-	RealEnd          *helper.Iso8601Time `json:"realEnd"`
-	Days             int                 `json:"days"`
-	Status           string              `json:"status"`
-	SubStatus        string              `json:"subStatus"`
-	Pri              string              `json:"pri"`
-	Description      string              `json:"desc"`
-	Version          int                 `json:"version"`
-	ParentVersion    int                 `json:"parentVersion"`
-	PlanDuration     int                 `json:"planDuration"`
-	RealDuration     int                 `json:"realDuration"`
-	//OpenedBy       string    `json:"openedBy"`
-	OpenedDate    *helper.Iso8601Time `json:"openedDate"`
-	OpenedVersion string              `json:"openedVersion"`
-	//LastEditedBy   string              `json:"lastEditedBy"`
-	LastEditedDate *helper.Iso8601Time `json:"lastEditedDate"`
-	ClosedBy       string              `json:"closedBy"`
-	ClosedDate     *helper.Iso8601Time `json:"closedDate"`
-	CanceledBy     string              `json:"canceledBy"`
-	CanceledDate   *helper.Iso8601Time `json:"canceledDate"`
-	SuspendedDate  *helper.Iso8601Time `json:"suspendedDate"`
-	PO             string              `json:"PO"`
-	PM             `json:"PM"`
-	QD             string `json:"QD"`
-	RD             string `json:"RD"`
-	Team           string `json:"team"`
-	Acl            string `json:"acl"`
-	Whitelist      `json:"whitelist" gorm:"-"`
-	OrderIn        int    `json:"order"`
-	Vision         string `json:"vision"`
-	DisplayCards   int    `json:"displayCards"`
-	FluidBoard     string `json:"fluidBoard"`
-	Deleted        bool   `json:"deleted"`
-	Delay          int    `json:"delay"`
-	Hours          `json:"hours"`
-	TeamCount      int    `json:"teamCount"`
-	LeftTasks      string `json:"leftTasks"`
+	ConnectionId     uint64              `json:"connectionid" mapstructure:"connectionid" gorm:"primaryKey;type:BIGINT  NOT NULL"`
+	Id               int64               `json:"id" mapstructure:"id" gorm:"primaryKey;type:BIGINT  NOT NULL"`
+	Project          int64               `json:"project" mapstructure:"project"`
+	Model            string              `json:"model" mapstructure:"model"`
+	Type             string              `json:"type" mapstructure:"type"`
+	ProjectType      string              `json:"projectType" mapstructure:"projectType"`
+	Lifetime         string              `json:"lifetime" mapstructure:"lifetime"`
+	Budget           string              `json:"budget" mapstructure:"budget"`
+	BudgetUnit       string              `json:"budgetUnit" mapstructure:"budgetUnit"`
+	Attribute        string              `json:"attribute" mapstructure:"attribute"`
+	Percent          int                 `json:"percent" mapstructure:"percent"`
+	Milestone        string              `json:"milestone" mapstructure:"milestone"`
+	Output           string              `json:"output" mapstructure:"output"`
+	Auth             string              `json:"auth" mapstructure:"auth"`
+	Parent           int64               `json:"parent" mapstructure:"parent"`
+	Path             string              `json:"path" mapstructure:"path"`
+	Grade            int                 `json:"grade" mapstructure:"grade"`
+	Name             string              `json:"name" mapstructure:"name"`
+	Code             string              `json:"code" mapstructure:"code"`
+	PlanBegin        *helper.Iso8601Time `json:"begin" mapstructure:"begin"`
+	PlanEnd          *helper.Iso8601Time `json:"end" mapstructure:"end"`
+	RealBegan        *helper.Iso8601Time `json:"realBegan" mapstructure:"realBegan"`
+	RealEnd          *helper.Iso8601Time `json:"realEnd" mapstructure:"realEnd"`
+	Days             int                 `json:"days" mapstructure:"days"`
+	Status           string              `json:"status" mapstructure:"status"`
+	SubStatus        string              `json:"subStatus" mapstructure:"subStatus"`
+	Pri              string              `json:"pri" mapstructure:"pri"`
+	Description      string              `json:"desc" mapstructure:"desc"`
+	Version          int                 `json:"version" mapstructure:"version"`
+	ParentVersion    int                 `json:"parentVersion" mapstructure:"parentVersion"`
+	PlanDuration     int                 `json:"planDuration" mapstructure:"planDuration"`
+	RealDuration     int                 `json:"realDuration" mapstructure:"realDuration"`
+	//OpenedBy       string    `json:"openedBy" mapstructure:"openedBy"`
+	OpenedDate    *helper.Iso8601Time `json:"openedDate" mapstructure:"openedDate"`
+	OpenedVersion string              `json:"openedVersion" mapstructure:"openedVersion"`
+	//LastEditedBy   string              `json:"lastEditedBy" mapstructure:"lastEditedBy"`
+	LastEditedDate *helper.Iso8601Time `json:"lastEditedDate" mapstructure:"lastEditedDate"`
+	ClosedBy       string              `json:"closedBy" mapstructure:"closedBy"`
+	ClosedDate     *helper.Iso8601Time `json:"closedDate" mapstructure:"closedDate"`
+	CanceledBy     string              `json:"canceledBy" mapstructure:"canceledBy"`
+	CanceledDate   *helper.Iso8601Time `json:"canceledDate" mapstructure:"canceledDate"`
+	SuspendedDate  *helper.Iso8601Time `json:"suspendedDate" mapstructure:"suspendedDate"`
+	PO             string              `json:"po" mapstructure:"po"`
+	PM             `json:"pm" mapstructure:"pm"`
+	QD             string `json:"qd" mapstructure:"qd"`
+	RD             string `json:"rd" mapstructure:"rd"`
+	Team           string `json:"team" mapstructure:"team"`
+	Acl            string `json:"acl" mapstructure:"acl"`
+	Whitelist      `json:"whitelist" mapstructure:"" gorm:"-"`
+	OrderIn        int    `json:"order" mapstructure:"order"`
+	Vision         string `json:"vision" mapstructure:"vision"`
+	DisplayCards   int    `json:"displayCards" mapstructure:"displayCards"`
+	FluidBoard     string `json:"fluidBoard" mapstructure:"fluidBoard"`
+	Deleted        bool   `json:"deleted" mapstructure:"deleted"`
+	Delay          int    `json:"delay" mapstructure:"delay"`
+	Hours          `json:"hours" mapstructure:"hours"`
+	TeamCount      int    `json:"teamCount" mapstructure:"teamCount"`
+	LeftTasks      string `json:"leftTasks" mapstructure:"leftTasks"`
 	//TeamMembers   []interface{} `json:"teamMembers" gorm:"-"`
-	TotalEstimate float64 `json:"totalEstimate"`
-	TotalConsumed float64 `json:"totalConsumed"`
-	TotalLeft     float64 `json:"totalLeft"`
-	Progress      float64 `json:"progress"`
-	TotalReal     int     `json:"totalReal"`
+	TotalEstimate float64 `json:"totalEstimate" mapstructure:"totalEstimate"`
+	TotalConsumed float64 `json:"totalConsumed" mapstructure:"totalConsumed"`
+	TotalLeft     float64 `json:"totalLeft" mapstructure:"totalLeft"`
+	Progress      float64 `json:"progress" mapstructure:"progress"`
+	TotalReal     int     `json:"totalReal" mapstructure:"totalReal"`
 }
 type PM struct {
-	PmId       int64  `json:"id"`
-	PmAccount  string `json:"account"`
-	PmAvatar   string `json:"avatar"`
-	PmRealname string `json:"realname"`
+	PmId       int64  `json:"id" mapstructure:"id"`
+	PmAccount  string `json:"account" mapstructure:"account"`
+	PmAvatar   string `json:"avatar" mapstructure:"avatar"`
+	PmRealname string `json:"realname" mapstructure:"realname"`
 }
 type Whitelist []struct {
-	WhitelistID       int64  `json:"id"`
-	WhitelistAccount  string `json:"account"`
-	WhitelistAvatar   string `json:"avatar"`
-	WhitelistRealname string `json:"realname"`
+	WhitelistID       int64  `json:"id" mapstructure:"id"`
+	WhitelistAccount  string `json:"account" mapstructure:"account"`
+	WhitelistAvatar   string `json:"avatar" mapstructure:"avatar"`
+	WhitelistRealname string `json:"realname" mapstructure:"realname"`
 }
 type Hours struct {
-	HoursTotalEstimate float64 `json:"totalEstimate"`
-	HoursTotalConsumed float64 `json:"totalConsumed"`
-	HoursTotalLeft     float64 `json:"totalLeft"`
-	HoursProgress      float64 `json:"progress"`
-	HoursTotalReal     float64 `json:"totalReal"`
+	HoursTotalEstimate float64 `json:"totalEstimate" mapstructure:"totalEstimate"`
+	HoursTotalConsumed float64 `json:"totalConsumed" mapstructure:"totalConsumed"`
+	HoursTotalLeft     float64 `json:"totalLeft" mapstructure:"totalLeft"`
+	HoursProgress      float64 `json:"progress" mapstructure:"progress"`
+	HoursTotalReal     float64 `json:"totalReal" mapstructure:"totalReal"`
 }
 
 func (ZentaoProject) TableName() string {
@@ -123,5 +125,9 @@ func (p ZentaoProject) ScopeName() string {
 }
 
 func (p ZentaoProject) ConvertApiScope() plugin.ToolLayerScope {
+	if p.ProjectType == "" {
+		p.ProjectType = p.Type
+		p.Type = "project"
+	}
 	return p
 }
