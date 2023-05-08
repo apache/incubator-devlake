44c30255b73d389f51681d6f1b5f73326fcb50f3
mintsweet <0x1304570@gmail.com>
2023-05-08 09:22:11 +0800

    feat(config-ui): add tips for project incoming webhook

--- config-ui/src/pages/project/detail/panel/incoming-webhooks.tsx
+++ config-ui/src/pages/project/detail/panel/incoming-webhooks.tsx
@@ -16,10 +16,10 @@
  *
  */
 
-import React, { useState, useMemo } from 'react';
+import { useState, useMemo } from 'react';
 import { Button, Intent } from '@blueprintjs/core';
 
-import { NoData } from '@/components';
+import { Alert, NoData } from '@/components';
 import type { WebhookItemType } from '@/plugins/register/webook';
 import { WebhookCreateDialog, WebhookSelectorDialog, WebHookConnection } from '@/plugins/register/webook';
 
@@ -56,31 +56,44 @@ export const IncomingWebhooksPanel = ({
     setType(undefined);
   };
 
-  if (!webhookIds.length) {
-    return (
-      <>
-        <NoData
-          text="Push `incidents` or `deployments` from your tools by incoming webhooks."
-          action={
-            <>
-              <Button intent={Intent.PRIMARY} icon="plus" text="Add a Webhook" onClick={() => setType('create')} />
-              <div style={{ margin: '8px 0' }}>or</div>
-              <Button
-                outlined
-                intent={Intent.PRIMARY}
-                text="Select Existing Webhooks"
-                onClick={() => setType('selectExist')}
-              />
-            </>
-          }
-        />
-        {type === 'create' && <WebhookCreateDialog isOpen onCancel={handleCancel} onSubmitAfter={onCreateWebhook} />}
-        {type === 'selectExist' && (
-          <WebhookSelectorDialog isOpen saving={saving} onCancel={handleCancel} onSubmit={onSelectWebhook} />
-        )}
-      </>
-    );
-  }
-
-  return <WebHookConnection filterIds={webhookIds} onCreateAfter={onCreateWebhook} onDeleteAfter={onDeleteWebhook} />;
+  return (
+    <>
+      <Alert style={{ marginBottom: 24, color: '#3C5088' }}>
+        <div>
+          The data pushed by Webhooks will only be calculated for DORA in the next run of the Blueprint of this project
+          because DORA relies on the post-processing of "deployments," "incidents," and "pull requests" triggered by
+          running the blueprint.
+        </div>
+        <div style={{ marginTop: 16 }}>
+          To calculate DORA after receiving Webhook data immediately, you can visit the{' '}
+          <b style={{ textDecoration: 'underline' }}>Status tab</b> of the Blueprint page and click on Run Now.
+        </div>
+      </Alert>
+      {!webhookIds.length ? (
+        <>
+          <NoData
+            text="Push `incidents` or `deployments` from your tools by incoming webhooks."
+            action={
+              <>
+                <Button intent={Intent.PRIMARY} icon="plus" text="Add a Webhook" onClick={() => setType('create')} />
+                <div style={{ margin: '8px 0' }}>or</div>
+                <Button
+                  outlined
+                  intent={Intent.PRIMARY}
+                  text="Select Existing Webhooks"
+                  onClick={() => setType('selectExist')}
+                />
+              </>
+            }
+          />
+          {type === 'create' && <WebhookCreateDialog isOpen onCancel={handleCancel} onSubmitAfter={onCreateWebhook} />}
+          {type === 'selectExist' && (
+            <WebhookSelectorDialog isOpen saving={saving} onCancel={handleCancel} onSubmit={onSelectWebhook} />
+          )}
+        </>
+      ) : (
+        <WebHookConnection filterIds={webhookIds} onCreateAfter={onCreateWebhook} onDeleteAfter={onDeleteWebhook} />
+      )}
+    </>
+  );
 };
