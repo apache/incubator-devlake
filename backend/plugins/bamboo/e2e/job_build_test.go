0b7258e65f0280f38f70cbe0d54a6ca8b995655c
Klesh Wong <zhenmian.huang@merico.dev>
2023-05-15 13:02:33 +0800

    fix: bamboo builds should be marked as PRODUCTION when corresponding regex is omitted

--- backend/plugins/bamboo/e2e/job_build_test.go
+++ backend/plugins/bamboo/e2e/job_build_test.go
@@ -45,12 +45,24 @@ func TestBambooJobBuildDataFlow(t *testing.T) {
 		RegexEnricher: helper.NewRegexEnricher(),
 	}
 	taskData.RegexEnricher.TryAdd(devops.DEPLOYMENT, taskData.Options.DeploymentPattern)
-	taskData.RegexEnricher.TryAdd(devops.PRODUCTION, taskData.Options.ProductionPattern)
 	// import raw data table
 	// SELECT * FROM _raw_bamboo_api_job_build INTO OUTFILE "/tmp/_raw_bamboo_api_job_build.csv" FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\r\n';
 	dataflowTester.ImportCsvIntoRawTable("./raw_tables/_raw_bamboo_api_job_build.csv", "_raw_bamboo_api_job_build")
 
+	// verify env when production regex is not set
+	dataflowTester.FlushTabler(&models.BambooJobBuild{})
+	dataflowTester.Subtask(tasks.ExtractJobBuildMeta, taskData)
+	dataflowTester.Subtask(tasks.ExtractJobBuildMeta, taskData)
+	dataflowTester.VerifyTable(
+		models.BambooJobBuild{},
+		"./snapshot_tables/_tool_bamboo_job_builds_no_prod_env.csv",
+		e2ehelper.ColumnWithRawData(
+			"environment",
+		),
+	)
+
 	// verify extraction
+	taskData.RegexEnricher.TryAdd(devops.PRODUCTION, taskData.Options.ProductionPattern)
 	dataflowTester.FlushTabler(&models.BambooJobBuild{})
 	dataflowTester.Subtask(tasks.ExtractJobBuildMeta, taskData)
 	dataflowTester.VerifyTable(
