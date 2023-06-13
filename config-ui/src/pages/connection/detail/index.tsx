7ccfa3e53a66aa0b65abc0d2c1fe2c15e6216af5
青湛 <0x1304570@gmail.com>
2023-06-13 20:40:19 +0800

    fix(config-ui): replace data scope clear icon (#5450)
    * feat(config-ui): support params image in icon-button component
    
    * fix(config-ui): replace data scope clear icon

--- config-ui/src/pages/connection/detail/index.tsx
+++ config-ui/src/pages/connection/detail/index.tsx
@@ -22,6 +22,7 @@ import { Button, Icon, Intent } from '@blueprintjs/core';
 
 import { PageHeader, Buttons, Dialog, IconButton, Table, Message } from '@/components';
 import { useTips, useConnections, useRefreshData } from '@/hooks';
+import ClearImg from '@/images/icons/clear.svg';
 import {
   ConnectionForm,
   ConnectionStatus,
@@ -260,7 +261,7 @@ const ConnectionDetail = ({ plugin, connectionId }: Props) => {
               render: (id) => (
                 <>
                   <IconButton
-                    icon="unarchive"
+                    image={<img src={ClearImg} alt="clear" />}
                     tooltip="Clear historical data"
                     onClick={() => handleShowClearDataScopeDialog(id)}
                   />
