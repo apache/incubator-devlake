754135dee02b6b9992da91e17a9de748e00415ba
abeizn <zikuan.an@merico.dev>
2023-06-13 17:20:52 +0800

    fix: gitlab/bitbucket generate the complete repo name path when creat… (#5463)
    * fix: gitlab/bitbucket generate the complete repo name path when creating blueprint
    
    * fix: unit test
    
    * fix: unit test

--- backend/plugins/gitlab/api/blueprint_V200_test.go
+++ backend/plugins/gitlab/api/blueprint_V200_test.go
@@ -47,6 +47,7 @@ func TestMakeDataSourcePipelinePlanV200(t *testing.T) {
 	const testHttpUrlToRepo string = "https://this_is_cloneUrl"
 	const testToken string = "nddtf"
 	const testName string = "gitlab-test"
+	const pathWithNamespace string = "nddtf/gitlab-test"
 	const testScopeConfigName string = "gitlab scope config"
 	const testProxy string = ""
 
@@ -59,13 +60,13 @@ func TestMakeDataSourcePipelinePlanV200(t *testing.T) {
 	}
 
 	var testGitlabProject = &models.GitlabProject{
-		ConnectionId: testConnectionID,
-		GitlabId:     testID,
-		Name:         testName,
-
-		ScopeConfigId: testScopeConfigId,
-		CreatedDate:   &time.Time{},
-		HttpUrlToRepo: testHttpUrlToRepo,
+		ConnectionId:      testConnectionID,
+		GitlabId:          testID,
+		Name:              testName,
+		PathWithNamespace: pathWithNamespace,
+		ScopeConfigId:     testScopeConfigId,
+		CreatedDate:       &time.Time{},
+		HttpUrlToRepo:     testHttpUrlToRepo,
 	}
 
 	var testScopeConfig = &models.GitlabScopeConfig{
@@ -160,14 +161,14 @@ func TestMakeDataSourcePipelinePlanV200(t *testing.T) {
 		},
 	}
 
-	expectRepo := code.NewRepo(expectRepoId, testName)
+	expectRepo := code.NewRepo(expectRepoId, pathWithNamespace)
 	expectRepo.ForkedFrom = testGitlabProject.ForkedFromProjectWebUrl
 
-	expectCicdScope := devops.NewCicdScope(expectRepoId, testName)
+	expectCicdScope := devops.NewCicdScope(expectRepoId, pathWithNamespace)
 	expectCicdScope.Description = ""
 	expectCicdScope.Url = ""
 
-	expectBoard := ticket.NewBoard(expectRepoId, testName)
+	expectBoard := ticket.NewBoard(expectRepoId, pathWithNamespace)
 	expectBoard.Description = ""
 	expectBoard.Url = ""
 	expectBoard.Type = ""
