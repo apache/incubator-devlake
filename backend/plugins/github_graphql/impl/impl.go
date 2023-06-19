7842e85c53642872948a3c8c9103e8641f54a953
Albin Vass <albin.vass@teliacompany.com>
2023-06-19 13:15:54 +0200

    Use proxy settings for graphql connections

--- backend/plugins/github_graphql/impl/impl.go
+++ backend/plugins/github_graphql/impl/impl.go
@@ -20,6 +20,7 @@ package impl
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"net/url"
 	"reflect"
 	"time"
@@ -157,7 +158,30 @@ func (p GithubGraphql) PrepareTaskData(taskCtx plugin.TaskContext, options map[s
 	src := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: connection.GetToken()},
 	)
-	httpClient := oauth2.NewClient(taskCtx.GetContext(), src)
+	oauthContext := taskCtx.GetContext()
+	proxy := connection.GetProxy()
+	if proxy != "" {
+		pu, err := url.Parse(proxy)
+		if err != nil {
+			return nil, errors.Convert(err)
+		}
+		if pu.Scheme == "http" || pu.Scheme == "socks5" {
+			proxyClient := &http.Client{
+				Transport: &http.Transport{Proxy: http.ProxyURL(pu)},
+			}
+			oauthContext = context.WithValue(
+				taskCtx.GetContext(),
+				oauth2.HTTPClient,
+				proxyClient,
+			)
+			logger.Debug("Proxy set in oauthContext to %s", proxy)
+		} else {
+			return nil, errors.BadInput.New("Unsupported scheme set in proxy")
+		}
+	}
+
+
+	httpClient := oauth2.NewClient(oauthContext, src)
 	endpoint, err := errors.Convert01(url.JoinPath(connection.Endpoint, `graphql`))
 	if err != nil {
 		return nil, errors.BadInput.Wrap(err, fmt.Sprintf("malformed connection endpoint supplied: %s", connection.Endpoint))
