e6fa869239b4b232877710280e0fbd3219d8100f
青湛 <0x1304570@gmail.com>
2023-07-20 15:43:12 +1200

    fix(config-ui): use new error boundary in new react-router-dom (#5700)

--- config-ui/src/main.tsx
+++ config-ui/src/main.tsx
@@ -16,21 +16,9 @@
  *
  */
 
-import React from 'react';
 import ReactDOM from 'react-dom';
-import { RouterProvider } from 'react-router-dom';
 
-import { ErrorBoundary } from '@/components';
-
-import { router } from './router';
-
+import { App } from './App';
 import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <RouterProvider router={router} />
-    </ErrorBoundary>
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
