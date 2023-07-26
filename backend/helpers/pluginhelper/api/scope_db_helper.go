59da82888789a293522e243a00605848583777f4
abeizn <zikuan.an@merico.dev>
2023-07-26 14:25:02 +0800

    fix: support gitlab data scope search (#5699)
    * fix: gitlab allow Data Scope search
    
    * fix: support gitlab data scope search
    
    * fix: add SearchScopeParamName field for data scope search
    
    * fix: add comment for the SearchScopeParamName field
    
    * fix: lint

--- backend/helpers/pluginhelper/api/scope_db_helper.go
+++ backend/helpers/pluginhelper/api/scope_db_helper.go
@@ -116,8 +116,11 @@ func (s *ScopeDatabaseHelperImpl[Conn, Scope, Tr]) ListScopes(input *plugin.ApiR
 	searchTerm := input.Query.Get("searchTerm")
 	query := dal.Where("connection_id = ?", connectionId)
 	if searchTerm != "" {
-		query = dal.Where(fmt.Sprintf("connection_id = ? AND %s LIKE ?", s.params.RawScopeParamName), connectionId, "%"+searchTerm+"%")
-
+		if s.params.SearchScopeParamName != "" {
+			query = dal.Where(fmt.Sprintf("connection_id = ? AND %s LIKE ?", s.params.SearchScopeParamName), connectionId, "%"+searchTerm+"%")
+		} else {
+			query = dal.Where(fmt.Sprintf("connection_id = ? AND %s LIKE ?", s.params.RawScopeParamName), connectionId, "%"+searchTerm+"%")
+		}
 	}
 	limit, offset := GetLimitOffset(input.Query, "pageSize", "page")
 	var scopes []*Scope
