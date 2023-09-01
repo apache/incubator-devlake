68baa7d45e4e6ac94d14c75f05fdf921591b23f0
Lynwee <linwei.hou@merico.dev>
2023-09-01 14:15:21 +0800

    fix(zentao): get all tasks ignore task's status (#6003)

--- backend/plugins/zentao/tasks/task_collector.go
+++ backend/plugins/zentao/tasks/task_collector.go
@@ -67,6 +67,7 @@ func CollectTask(taskCtx plugin.SubTaskContext) errors.Error {
 			query := url.Values{}
 			query.Set("page", fmt.Sprintf("%v", reqData.Pager.Page))
 			query.Set("limit", fmt.Sprintf("%v", reqData.Pager.Size))
+			query.Set("status", "all")
 			return query, nil
 		},
 		GetTotalPages: GetTotalPagesFromResponse,
