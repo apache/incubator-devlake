af03b7562c082e1f3e495f84bc52567dbb4e2deb
abeizn <zikuan.an@merico.dev>
2023-09-07 17:27:03 +0800

    fix: update completed_date and started_date logic (#6031)

--- backend/plugins/zentao/tasks/execution_convertor.go
+++ backend/plugins/zentao/tasks/execution_convertor.go
@@ -93,10 +93,15 @@ func ConvertExecutions(taskCtx plugin.SubTaskContext) errors.Error {
 				Name:            toolExecution.Name,
 				Url:             fmt.Sprintf("%s/execution-view-%d.html", homePage, toolExecution.Id),
 				Status:          domainStatus,
-				StartedDate:     toolExecution.RealBegan.ToNullableTime(),
 				EndedDate:       toolExecution.PlanEnd.ToNullableTime(),
-				CompletedDate:   toolExecution.RealEnd.ToNullableTime(),
+				CompletedDate:   toolExecution.ClosedDate.ToNullableTime(),
 				OriginalBoardID: projectIdGen.Generate(toolExecution.ConnectionId, data.Options.ProjectId),
+			}
+
+			if domainStatus == `FUTURE` {
+				sprint.StartedDate = toolExecution.PlanBegin.ToNullableTime()
+			} else {
+				sprint.StartedDate = toolExecution.RealBegan.ToNullableTime()
 			}
 
 			boardSprint := &ticket.BoardSprint{
