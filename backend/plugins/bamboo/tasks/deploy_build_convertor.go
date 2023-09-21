f1ea68323a6528547eda6c12ce806d4293eea27c
Lynwee <linwei.hou@merico.dev>
2023-09-21 15:03:31 +0800

    fix(bamboo): update cicd_pipelines and cicd_deployment_commit tables' name field

--- backend/plugins/bamboo/tasks/deploy_build_convertor.go
+++ backend/plugins/bamboo/tasks/deploy_build_convertor.go
@@ -18,6 +18,7 @@ limitations under the License.
 package tasks
 
 import (
+	"fmt"
 	"reflect"
 	"strconv"
 	"time"
@@ -42,18 +43,25 @@ var ConvertDeployBuildsMeta = plugin.SubTaskMeta{
 
 type deployBuildWithVcsRevision struct {
 	models.BambooDeployBuild
-	RepositoryId   int
-	RepositoryName string
-	VcsRevisionKey string
+	RepositoryId    int
+	RepositoryName  string
+	VcsRevisionKey  string
+	ProjectPlanName string
+	ProjectName     string
+}
+
+func (deployBuildWithVcsRevision deployBuildWithVcsRevision) GenerateCICDDeploymentCommitName() string {
+	return fmt.Sprintf("%s/%s", deployBuildWithVcsRevision.ProjectPlanName, deployBuildWithVcsRevision.DeploymentVersionName)
 }
 
 func ConvertDeployBuilds(taskCtx plugin.SubTaskContext) errors.Error {
 	db := taskCtx.GetDal()
 	rawDataSubTaskArgs, data := CreateRawDataSubTaskArgs(taskCtx, RAW_JOB_BUILD_TABLE)
 	cursor, err := db.Cursor(
-		dal.Select("db.*, pbc.repository_id, pbc.repository_name, pbc.vcs_revision_key"),
+		dal.Select("db.*, pbc.repository_id, pbc.repository_name, pbc.vcs_revision_key, p.name as project_plan_name, p.project_name"),
 		dal.From("_tool_bamboo_deploy_builds AS db"),
 		dal.Join("INNER JOIN _tool_bamboo_plan_build_commits AS pbc ON db.connection_id = pbc.connection_id AND db.plan_result_key = pbc.plan_result_key"),
+		dal.Join("LEFT JOIN _tool_bamboo_plans as p pm ON db.plan_key = p.plan_key"),
 		dal.Where("db.connection_id = ? and db.plan_key = ?", data.Options.ConnectionId, data.Options.PlanKey))
 	if err != nil {
 		return err
@@ -78,7 +86,7 @@ func ConvertDeployBuilds(taskCtx plugin.SubTaskContext) errors.Error {
 				},
 				CicdScopeId:      planIdGen.Generate(data.Options.ConnectionId, data.Options.PlanKey),
 				CicdDeploymentId: deployBuildIdGen.Generate(data.Options.ConnectionId, input.DeployBuildId),
-				Name:             input.DeploymentVersionName,
+				Name:             input.GenerateCICDDeploymentCommitName(),
 				Result: devops.GetResult(&devops.ResultRule{
 					Failed:  []string{"Failed", "FAILED"},
 					Success: []string{"Successful", "SUCCESSFUL"},
