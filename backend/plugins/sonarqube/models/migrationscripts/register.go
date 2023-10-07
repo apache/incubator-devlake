f98072cd904d81b695ed6d9ab8387db96da0a3e0
abeizn <zikuan.an@merico.dev>
2023-10-07 16:20:11 +0800

    fix: sonarqube and gitee scope config id (#6186)
    * fix: sonarqube and gitee scope config id
    
    * fix: ci lint
    
    * fix: typo

--- backend/plugins/sonarqube/models/migrationscripts/register.go
+++ backend/plugins/sonarqube/models/migrationscripts/register.go
@@ -26,6 +26,7 @@ func All() []plugin.MigrationScript {
 		new(modifyCharacterSet),
 		new(expandProjectKey20230206),
 		new(addRawParamTableForScope),
+		new(addScopeConfigIdToProject),
 		new(modifyFileMetricsKeyLength),
 	}
 }
