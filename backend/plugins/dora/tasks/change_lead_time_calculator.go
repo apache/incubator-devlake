310e43cbbe3022a4f64736cac613e1781ef76852
Klesh Wong <zhenmian.huang@merico.dev>
2023-10-18 13:48:12 +0800

    fix: pr should be connected to failed deployment commit (#6270)

--- backend/plugins/dora/tasks/change_lead_time_calculator.go
+++ backend/plugins/dora/tasks/change_lead_time_calculator.go
@@ -206,7 +206,7 @@ func getDeploymentCommit(mergeSha string, projectName string, db dal.Dal) (*devo
 		dal.Join("LEFT JOIN project_mapping pm ON (pm.table = 'cicd_scopes' AND pm.row_id = dc.cicd_scope_id)"),
 		dal.Join("INNER JOIN commits_diffs cd ON (cd.new_commit_sha = dc.commit_sha AND cd.old_commit_sha = COALESCE (p.commit_sha, ''))"),
 		dal.Where("dc.environment = 'PRODUCTION'"), // TODO: remove this when multi-environment is supported
-		dal.Where("pm.project_name = ? AND cd.commit_sha = ?", projectName, mergeSha),
+		dal.Where("pm.project_name = ? AND cd.commit_sha = ? AND dc.RESULT = ?", projectName, mergeSha, devops.RESULT_SUCCESS),
 		dal.Orderby("dc.started_date, dc.id ASC"),
 		dal.Limit(1),
 	)
