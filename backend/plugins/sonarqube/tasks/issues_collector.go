bc4f603774745b7e17268717ef78d3a2b1a54a09
abeizn <zikuan.an@merico.dev>
2023-10-24 18:07:06 +0800

    fix: add time filter

--- backend/plugins/sonarqube/tasks/issues_collector.go
+++ backend/plugins/sonarqube/tasks/issues_collector.go
@@ -22,6 +22,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"time"
 
 	"github.com/apache/incubator-devlake/core/errors"
 	"github.com/apache/incubator-devlake/core/models/common"
@@ -34,9 +35,11 @@ const RAW_ISSUES_TABLE = "sonarqube_api_issues"
 var _ plugin.SubTaskEntryPoint = CollectIssues
 
 type SonarqubeIssueIteratorNode struct {
-	Severity string
-	Status   string
-	Type     string
+	Severity      string
+	Status        string
+	Type          string
+	CreatedAfter  *time.Time
+	CreatedBefore *time.Time
 }
 
 func CollectIssues(taskCtx plugin.SubTaskContext) (err errors.Error) {
@@ -52,9 +55,11 @@ func CollectIssues(taskCtx plugin.SubTaskContext) (err errors.Error) {
 			for _, typ := range types {
 				iterator.Push(
 					&SonarqubeIssueIteratorNode{
-						Severity: severity,
-						Status:   status,
-						Type:     typ,
+						Severity:      severity,
+						Status:        status,
+						Type:          typ,
+						CreatedAfter:  nil,
+						CreatedBefore: nil,
 					},
 				)
 			}
@@ -70,15 +75,95 @@ func CollectIssues(taskCtx plugin.SubTaskContext) (err errors.Error) {
 		Query: func(reqData *helper.RequestData) (url.Values, errors.Error) {
 			query := url.Values{}
 			query.Set("componentKeys", fmt.Sprintf("%v", data.Options.ProjectKey))
+			input, ok := reqData.Input.(*SonarqubeIssueIteratorNode)
+			if !ok {
+				return nil, errors.Default.New(fmt.Sprintf("Input to SonarqubeIssueIteratorNode failed:%+v", reqData.Input))
+			}
 			query.Set("severities", reqData.Input.(*SonarqubeIssueIteratorNode).Severity)
 			query.Set("statuses", reqData.Input.(*SonarqubeIssueIteratorNode).Status)
 			query.Set("types", reqData.Input.(*SonarqubeIssueIteratorNode).Type)
+			if input.CreatedAfter != nil {
+				query.Set("createdAfter", GetFormatTime(input.CreatedAfter))
+			}
+			if input.CreatedBefore != nil {
+				query.Set("createdBefore", GetFormatTime(input.CreatedBefore))
+			}
 			query.Set("p", fmt.Sprintf("%v", reqData.Pager.Page))
 			query.Set("ps", fmt.Sprintf("%v", reqData.Pager.Size))
 			query.Encode()
 			return query, nil
 		},
-		GetTotalPages: GetTotalPagesFromResponse,
+		GetTotalPages: func(res *http.Response, args *helper.ApiCollectorArgs) (int, errors.Error) {
+			body := &SonarqubePagination{}
+			err := helper.UnmarshalResponse(res, body)
+			if err != nil {
+				return 0, err
+			}
+
+			pages := body.Paging.Total / args.PageSize
+			if body.Paging.Total%args.PageSize > 0 {
+				pages++
+			}
+
+			query := res.Request.URL.Query()
+
+			// if get more than 10000 data, that need split it
+			if pages > 100 {
+				var createdAfterUnix int64
+				var createdBeforeUnix int64
+
+				createdAfter, err := getTimeFromFormatTime(query.Get("createdAfter"))
+				if err != nil {
+					return 0, err
+				}
+				createdBefore, err := getTimeFromFormatTime(query.Get("createdBefore"))
+				if err != nil {
+					return 0, err
+				}
+
+				if createdAfter == nil {
+					createdAfterUnix = 0
+				} else {
+					createdAfterUnix = createdAfter.Unix()
+				}
+
+				if createdBefore == nil {
+					createdBeforeUnix = time.Now().Unix()
+				} else {
+					createdBeforeUnix = createdBefore.Unix()
+				}
+
+				// can not split it any more
+				if createdBeforeUnix-createdAfterUnix <= 10 {
+					return 100, nil
+				}
+
+				// split it
+				MidTime := time.Unix((createdAfterUnix+createdBeforeUnix)/2+1, 0)
+
+				// left part
+				iterator.Push(&SonarqubeIssueIteratorNode{
+					CreatedAfter:  createdAfter,
+					CreatedBefore: &MidTime,
+				})
+
+				// right part
+				iterator.Push(&SonarqubeIssueIteratorNode{
+					CreatedAfter:  &MidTime,
+					CreatedBefore: createdBefore,
+				})
+
+				logger.Info("split [%s][%s] by mid [%s] for it has pages:[%d] and total:[%d]",
+					query.Get("createdAfter"), query.Get("createdBefore"), GetFormatTime(&MidTime), pages, body.Paging.Total)
+
+				return 0, nil
+			} else {
+				logger.Info("[%s][%s] has pages:[%d] and total:[%d]",
+					query.Get("createdAfter"), query.Get("createdBefore"), pages, body.Paging.Total)
+
+				return pages, nil
+			}
+		},
 		ResponseParser: func(res *http.Response) ([]json.RawMessage, errors.Error) {
 			var resData struct {
 				Data []json.RawMessage `json:"issues"`
@@ -119,4 +204,29 @@ var CollectIssuesMeta = plugin.SubTaskMeta{
 	EnabledByDefault: true,
 	Description:      "Collect issues data from Sonarqube api",
 	DomainTypes:      []string{plugin.DOMAIN_TYPE_CODE_QUALITY},
+}
+
+func GetFormatTime(t *time.Time) string {
+	if t == nil {
+		return ""
+	}
+	return t.Format("2006-01-02T15:04:05-0700")
+}
+
+func getTimeFromFormatTime(formatTime string) (*time.Time, errors.Error) {
+	if formatTime == "" {
+		return nil, nil
+	}
+
+	if len(formatTime) < 20 {
+		return nil, errors.Default.New(fmt.Sprintf("formatTime [%s] is too short ", formatTime))
+	}
+
+	t, err := time.Parse("2006-01-02T15:04:05-0700", formatTime)
+
+	if err != nil {
+		return nil, errors.Default.New(fmt.Sprintf("Failed to Parse the time from [%s]:%s", formatTime, err.Error()))
+	}
+
+	return &t, nil
 }
