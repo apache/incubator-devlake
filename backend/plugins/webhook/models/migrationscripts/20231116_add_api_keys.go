b46971c853cacfb7f5b15cce8e205608a1bbdfcb
Lynwee Hou <linwei.hou@merico.dev>
2023-11-16 11:12:46 +0800

    refactor(webhook): fix lint errors

--- backend/plugins/webhook/models/migrationscripts/20231116_add_api_keys.go
+++ backend/plugins/webhook/models/migrationscripts/20231116_add_api_keys.go
@@ -22,10 +22,13 @@ import (
 	"github.com/apache/incubator-devlake/core/context"
 	"github.com/apache/incubator-devlake/core/dal"
 	"github.com/apache/incubator-devlake/core/errors"
+	"github.com/apache/incubator-devlake/core/log"
+	"github.com/apache/incubator-devlake/core/models"
 	common "github.com/apache/incubator-devlake/core/models/common"
 	"github.com/apache/incubator-devlake/core/plugin"
-	"github.com/apache/incubator-devlake/helpers/apikeyhelper"
 	"github.com/apache/incubator-devlake/plugins/webhook/models/migrationscripts/archived"
+	"regexp"
+	"time"
 )
 
 var _ plugin.MigrationScript = (*addApiKeys)(nil)
@@ -37,6 +40,47 @@ const (
 
 type addApiKeys struct{}
 
+func createForPlugin(db dal.Dal, logger log.Logger, user string, name string, pluginName string, allowedPath string, extra string) (*models.ApiKey, errors.Error) {
+	if _, err := regexp.Compile(allowedPath); err != nil {
+		logger.Error(err, "Compile allowed path")
+		return nil, errors.Default.Wrap(err, fmt.Sprintf("compile allowed path: %s", allowedPath))
+	}
+
+	apiKey, hashedApiKey := "auto-generated-key", "auto-generated-key"
+	now := time.Now()
+	apiKeyRecord := &models.ApiKey{
+		Model: common.Model{
+			CreatedAt: now,
+			UpdatedAt: now,
+		},
+		Name:        name,
+		ApiKey:      hashedApiKey,
+		ExpiredAt:   nil,
+		AllowedPath: allowedPath,
+		Type:        fmt.Sprintf("plugin:%s", pluginName),
+		Extra:       extra,
+	}
+	if user != "" {
+		apiKeyRecord.Creator = common.Creator{
+			Creator:      user,
+			CreatorEmail: "",
+		}
+		apiKeyRecord.Updater = common.Updater{
+			Updater:      user,
+			UpdaterEmail: "",
+		}
+	}
+	if err := db.Create(apiKeyRecord); err != nil {
+		logger.Error(err, "create api key record")
+		if db.IsDuplicationError(err) {
+			return nil, errors.BadInput.New(fmt.Sprintf("An api key with name [%s] has already exists", name))
+		}
+		return nil, errors.Default.Wrap(err, "error creating DB api key")
+	}
+	apiKeyRecord.ApiKey = apiKey
+	return apiKeyRecord, nil
+}
+
 func (u *addApiKeys) Up(baseRes context.BasicRes) errors.Error {
 	db := baseRes.GetDal()
 	if !db.HasTable(archived.WebhookConnection{}.TableName()) {
@@ -46,43 +90,26 @@ func (u *addApiKeys) Up(baseRes context.BasicRes) errors.Error {
 	if err := db.All(&webhooks); err != nil {
 		return err
 	}
-	user := &common.User{
-		Name:  creator,
-		Email: "",
-	}
 	logger := baseRes.GetLogger()
-	apiKeyHelper := apikeyhelper.NewApiKeyHelper(baseRes, logger)
 	tx := db.Begin()
 	for _, webhook := range webhooks {
-		name := apiKeyHelper.GenApiKeyNameForPlugin(pluginName, webhook.ID)
-		_, err := apiKeyHelper.GetApiKey(db, dal.Where("name = ?", name))
-		if err != nil {
+		name := fmt.Sprintf("%s-%d", pluginName, webhook.ID)
+		apiKey := &models.ApiKey{}
+		if err := tx.First(apiKey, dal.Where("name = ?", name)); err != nil {
 			if db.IsErrorNotFound(err) {
 				logger.Info("api key with name: %s not found in db", name)
 				allowedPath := fmt.Sprintf("/plugins/%s/connections/%d/.*", pluginName, webhook.ID)
 				extra := fmt.Sprintf("connectionId:%d", webhook.ID)
-				_, err := apiKeyHelper.CreateForPlugin(tx, user, name, pluginName, allowedPath, extra)
+				_, err := createForPlugin(db, logger, creator, name, pluginName, allowedPath, extra)
 				if err != nil {
 					logger.Error(err, "CreateForPlugin")
-					if err := tx.Rollback(); err != nil {
-						logger.Error(err, "rollback transaction")
-						return err
-					}
 					return err
 				}
 			} else {
 				logger.Error(err, "query api key from db, name: %s", name)
-				if err := tx.Rollback(); err != nil {
-					logger.Error(err, "rollback transaction")
-					return err
-				}
 				return err
 			}
 		}
-	}
-	if err := tx.Commit(); err != nil {
-		logger.Error(err, "commit transaction")
-		return err
 	}
 	return nil
 }
