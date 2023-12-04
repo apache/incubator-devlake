5581670a17466f0ae198a4c62fd9fb6f0066913d
Klesh Wong <zhenmian.huang@merico.dev>
2023-12-04 14:22:53 +0800

    refactor: jenkins adopts new dshelpers (#6554)

--- backend/plugins/jenkins/api/remote.go
+++ backend/plugins/jenkins/api/remote.go
@@ -18,128 +18,98 @@ limitations under the License.
 package api
 
 import (
-	gocontext "context"
-	"net/http"
-	"strings"
+	"fmt"
 
-	"github.com/apache/incubator-devlake/core/context"
 	"github.com/apache/incubator-devlake/core/errors"
 	"github.com/apache/incubator-devlake/core/plugin"
 	"github.com/apache/incubator-devlake/helpers/pluginhelper/api"
+	dsmodels "github.com/apache/incubator-devlake/helpers/pluginhelper/api/models"
 	"github.com/apache/incubator-devlake/plugins/jenkins/models"
 )
 
-// RemoteScopes list all available scope for users
-// @Summary list all available scope for users
-// @Description list all available scope for users
-// @Tags plugins/jenkins
+type JenkinsRemotePagination struct {
+	Page    int `json:"page"`
+	PerPage int `json:"per_page"`
+}
+
+func listJenkinsRemoteScopes(
+	connection *models.JenkinsConnection,
+	apiClient plugin.ApiClient,
+	groupId string,
+	page JenkinsRemotePagination,
+) (
+	children []dsmodels.DsRemoteApiScopeListEntry[models.JenkinsJob],
+	nextPage *JenkinsRemotePagination,
+	err errors.Error,
+) {
+	if page.Page == 0 {
+		page.Page = 1
+	}
+	if page.PerPage == 0 {
+		page.PerPage = 100
+	}
+	var parentId *string
+	if groupId != "" {
+		parentId = &groupId
+	}
+	getJobsPageCallBack := func(job *models.Job) errors.Error {
+		if job.Jobs != nil {
+			// this is a group
+			job.Path = groupId
+			children = append(children, dsmodels.DsRemoteApiScopeListEntry[models.JenkinsJob]{
+				Type:     api.RAS_ENTRY_TYPE_GROUP,
+				Id:       fmt.Sprintf("%s/job/%s", job.Path, job.Name),
+				Name:     job.Name,
+				ParentId: parentId,
+			})
+		} else {
+			// this is a scope
+			jenkinsJob := job.ToJenkinsJob()
+			children = append(children, dsmodels.DsRemoteApiScopeListEntry[models.JenkinsJob]{
+				Type:     api.RAS_ENTRY_TYPE_SCOPE,
+				Id:       jenkinsJob.ScopeId(),
+				Name:     jenkinsJob.ScopeName(),
+				FullName: jenkinsJob.ScopeFullName(),
+				Data:     jenkinsJob,
+				ParentId: parentId,
+			})
+		}
+		return nil
+	}
+	_, err = GetJobsPage(apiClient, groupId, page.Page-1, page.PerPage, getJobsPageCallBack)
+	if err != nil {
+		return
+	}
+	if len(children) == page.PerPage {
+		nextPage = &JenkinsRemotePagination{
+			Page:    page.Page + 1,
+			PerPage: page.PerPage,
+		}
+	}
+	return
+}
+
+// RemoteScopes list all available scopes on the remote server
+// @Summary list all available scopes on the remote server
+// @Description list all available scopes on the remote server
 // @Accept application/json
 // @Param connectionId path int false "connection ID"
 // @Param groupId query string false "group ID"
 // @Param pageToken query string false "page Token"
-// @Success 200  {object} api.RemoteScopesOutput
 // @Failure 400  {object} shared.ApiBody "Bad Request"
 // @Failure 500  {object} shared.ApiBody "Internal Error"
+// @Success 200  {object} dsmodels.DsRemoteApiScopeList[models.JenkinsJob]
+// @Tags plugins/jenkins
 // @Router /plugins/jenkins/connections/{connectionId}/remote-scopes [GET]
 func RemoteScopes(input *plugin.ApiResourceInput) (*plugin.ApiResourceOutput, errors.Error) {
-	getter := func(basicRes context.BasicRes, groupId string, queryData *api.RemoteQueryData, connection models.JenkinsConnection) (*api.RemoteScopesOutput, errors.Error) {
-		var resBody []api.RemoteScopesChild
-		apiClient, err := api.NewApiClientFromConnection(gocontext.TODO(), basicRes, &connection)
-		if err != nil {
-			return nil, errors.BadInput.Wrap(err, "failed to get create apiClient")
-		}
-		getJobsPageCallBack := func(job *models.Job) errors.Error {
-			if job.Jobs != nil {
-				// this is a group
-				job.Path = groupId
-				groupChild := api.RemoteScopesChild{
-					Type: api.TypeGroup,
-					Id:   job.GroupId(),
-					Name: job.GroupName(),
-					// don't need to save group into data
-					Data: nil,
-				}
-				groupChild.ParentId = &groupId
-				if *groupChild.ParentId == "" {
-					groupChild.ParentId = nil
-				}
-				resBody = append(resBody, groupChild)
-			} else {
-				// this is a scope
-				scope := job.ConvertApiScope()
-				scopeChild := api.RemoteScopesChild{
-					Type:     api.TypeScope,
-					Id:       scope.ScopeId(),
-					Name:     scope.ScopeName(),
-					FullName: scope.ScopeFullName(),
-					Data:     &scope,
-				}
-				scopeChild.ParentId = &groupId
-				if *scopeChild.ParentId == "" {
-					scopeChild.ParentId = nil
-				}
-				resBody = append(resBody, scopeChild)
-			}
-			return nil
-		}
-		_, err = GetJobsPage(apiClient, groupId, queryData.Page-1, queryData.PerPage, getJobsPageCallBack)
-		if err != nil {
-			return nil, errors.BadInput.Wrap(err, "failed to GetJobsPage")
-		}
-		return &api.RemoteScopesOutput{
-			Children: resBody,
-		}, nil
-	}
-	remoteScopesOutput, err := remoteHelper.GetRemoteScopesOutput(input, getter)
-	if err != nil {
-		return nil, err
-	}
-	return &plugin.ApiResourceOutput{Body: remoteScopesOutput, Status: http.StatusOK}, nil
+	return raScopeList.Get(input)
 }
 
-// SearchRemoteScopes use the Search API and only return project
-// @Summary use the Search API and only return project
-// @Description use the Search API and only return project
-// @Tags plugins/jenkins
-// @Accept application/json
-// @Param connectionId path int false "connection ID"
-// @Param search query string false "search"
-// @Param page query int false "page number"
-// @Param pageSize query int false "page size per page"
-// @Success 200  {object} api.SearchRemoteScopesOutput
-// @Failure 400  {object} shared.ApiBody "Bad Request"
-// @Failure 500  {object} shared.ApiBody "Internal Error"
-// @Router /plugins/jenkins/connections/{connectionId}/search-remote-scopes [GET]
-func SearchRemoteScopes(input *plugin.ApiResourceInput) (*plugin.ApiResourceOutput, errors.Error) {
-	return remoteHelper.SearchRemoteScopes(input,
-		func(basicRes context.BasicRes, queryData *api.RemoteQueryData, connection models.JenkinsConnection) ([]models.Job, errors.Error) {
-			apiClient, err := api.NewApiClientFromConnection(gocontext.TODO(), basicRes, &connection)
-			if err != nil {
-				return nil, errors.BadInput.Wrap(err, "failed to get create apiClient")
-			}
-
-			var resBody []models.Job
-			breakError := errors.Default.New("we need break from get all jobs for page full")
-			count := 0
-			pageoOffset := (queryData.Page - 1) * queryData.PerPage
-			err = GetAllJobs(apiClient, "", "", queryData.PerPage, func(job *models.Job, isPath bool) errors.Error {
-				if job.Jobs == nil {
-					if strings.Contains(job.FullName, queryData.Search[0]) {
-						if count >= pageoOffset {
-							resBody = append(resBody, *job)
-						}
-						count++
-					}
-					if len(resBody) > queryData.PerPage {
-						return breakError
-					}
-				}
-				return nil
-			})
-			if err != nil && err != breakError {
-				return nil, errors.BadInput.Wrap(err, "failed to get create apiClient")
-			}
-
-			return resBody, nil
-		})
+// @Summary Remote server API proxy
+// @Description Forward API requests to the specified remote server
+// @Param connectionId path int true "connection ID"
+// @Param path path string true "path to a API endpoint"
+// @Router /plugins/github/connections/{connectionId}/proxy/{path} [GET]
+func Proxy(input *plugin.ApiResourceInput) (*plugin.ApiResourceOutput, errors.Error) {
+	return raProxy.Proxy(input)
 }
