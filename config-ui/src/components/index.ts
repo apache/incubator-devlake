92d211e13008743aff5d726d2779302bc05fa741
mintsweet <0x1304570@gmail.com>
2023-12-11 14:53:10 +1300

    refactor(config-ui): remove unuse component pagination

--- config-ui/src/components/index.ts
+++ config-ui/src/components/index.ts
@@ -25,6 +25,5 @@ export * from './logo';
 export * from './message';
 export * from './no-data';
 export * from './page-header';
-export * from './pagination';
 export * from './selector';
 export * from './tooltip';
