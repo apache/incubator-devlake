44fa3f23e352bacd5d7482ac807996e2e03636eb
abeizn <zikuan.an@merico.dev>
2024-01-02 18:01:11 +0800

    fix: jira data scope not trans (#6715)
    * fix: jira data scope not trans
    
    * fix: get scope id from impl
    
    * fix: remove log
    
    * fix: remove unuseless code

--- backend/plugins/jira/impl/impl.go
+++ backend/plugins/jira/impl/impl.go
@@ -209,6 +209,9 @@ func (p Jira) PrepareTaskData(taskCtx plugin.TaskContext, options map[string]int
 		if err != nil {
 			return nil, errors.Default.Wrap(err, fmt.Sprintf("fail to find board: %d", op.BoardId))
 		}
+		if op.ScopeConfigId == 0 && scope.ScopeConfigId != 0 {
+			op.ScopeConfigId = scope.ScopeConfigId
+		}
 	}
 	if op.ScopeConfig == nil && op.ScopeConfigId != 0 {
 		var scopeConfig models.JiraScopeConfig
@@ -220,6 +223,9 @@ func (p Jira) PrepareTaskData(taskCtx plugin.TaskContext, options map[string]int
 		if err != nil {
 			return nil, errors.BadInput.Wrap(err, "fail to make scopeConfig")
 		}
+	}
+	if op.ScopeConfig == nil && op.ScopeConfigId == 0 {
+		op.ScopeConfig = new(models.JiraScopeConfig)
 	}
 
 	// set default page size
