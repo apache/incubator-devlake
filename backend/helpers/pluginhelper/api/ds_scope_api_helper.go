b4e2b5f58db29ad60d3069e5a7a55acbce69cb76
Klesh Wong <zhenmian.huang@merico.dev>
2024-02-08 12:30:37 +0800

    fix: new dshelper does not set raw data origin properly

--- backend/helpers/pluginhelper/api/ds_scope_api_helper.go
+++ backend/helpers/pluginhelper/api/ds_scope_api_helper.go
@@ -18,8 +18,11 @@ limitations under the License.
 package api
 
 import (
+	"fmt"
+
 	"github.com/apache/incubator-devlake/core/context"
 	"github.com/apache/incubator-devlake/core/errors"
+	"github.com/apache/incubator-devlake/core/models/common"
 	"github.com/apache/incubator-devlake/core/plugin"
 	"github.com/apache/incubator-devlake/helpers/srvhelper"
 	"github.com/apache/incubator-devlake/server/api/shared"
@@ -112,7 +115,16 @@ func (scopeApi *DsScopeApiHelper[C, S, SC]) PutMultiple(input *plugin.ApiResourc
 		}
 		dict["connectionId"] = connectionId
 	}
-	return scopeApi.ModelApiHelper.PutMultiple(input)
+	return scopeApi.ModelApiHelper.PutMultipleCb(input, func(m *S) errors.Error {
+		ok := setRawDataOrigin(m, common.RawDataOrigin{
+			RawDataTable:  fmt.Sprintf("_raw_%s_scopes", scopeApi.GetPluginName()),
+			RawDataParams: plugin.MarshalScopeParams((*m).ScopeParams()),
+		})
+		if !ok {
+			panic("set raw data origin failed")
+		}
+		return nil
+	})
 }
 
 func (scopeApi *DsScopeApiHelper[C, S, SC]) Delete(input *plugin.ApiResourceInput) (*plugin.ApiResourceOutput, errors.Error) {
