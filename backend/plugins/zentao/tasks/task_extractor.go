cadcc18e3720290934c0c091341b32ca379886f7
Lynwee <linwei.hou@merico.dev>
2024-02-21 17:56:09 +0800

    fix(zentao): update the type of `PriOrder` (#6986)

--- backend/plugins/zentao/tasks/task_extractor.go
+++ backend/plugins/zentao/tasks/task_extractor.go
@@ -146,7 +146,7 @@ func (c *taskExtractor) toZentaoTasks(accountCache *AccountCache, res *models.Ze
 		StoryTitle:         res.StoryTitle,
 		LatestStoryVersion: 0,
 		AssignedToRealName: res.AssignedToRealName,
-		PriOrder:           res.PriOrder,
+		PriOrder:           res.PriOrder.String(),
 		NeedConfirm:        res.NeedConfirm,
 		Progress:           res.Progress,
 		Url:                url,
