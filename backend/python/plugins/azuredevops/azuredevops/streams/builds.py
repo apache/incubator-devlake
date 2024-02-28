57efdc02733e271718b88f543577849fdebb8053
Lynwee <linwei.hou@merico.dev>
2024-02-28 11:35:39 +0800

    fix `updated_at` field in cicd_tasks and cicd_pipelines (#7032)
    * fix(gitextractor): cover errors that may leak sensitive information
    
    * fix(azuredevops): fix updated_at field in cicd_tasks and cicd_pipelines
    
    * fix(azuredevops): fix test errors
    
    * fix(azuredevops): remove domain layer tables when collecting data fully
    
    * fix(azuredevops): remove debug codes
    
    * fix(azuredevops): fix testing errors
    
    * fix(azuredevops): fix test errors
    
    * fix(azuredevops): fix comments(most of them are about format)

--- backend/python/plugins/azuredevops/azuredevops/streams/builds.py
+++ backend/python/plugins/azuredevops/azuredevops/streams/builds.py
@@ -25,6 +25,7 @@ from pydevlake import Context, DomainType, Stream
 class Builds(Stream):
     tool_model = Build
     domain_types = [DomainType.CICD]
+    domain_models = [devops.CiCDPipelineCommit, devops.CICDPipeline]
 
     def collect(self, state, context) -> Iterable[tuple[object, dict]]:
         repo: GitRepository = context.scope
