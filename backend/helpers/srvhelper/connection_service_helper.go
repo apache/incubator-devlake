c5ad3a0d1855efbe9f326e1d54aa23c126b7c61c
abeizn <zikuan.an@merico.dev>
2024-03-04 15:11:14 +0800

    fix: delete some code

--- backend/helpers/srvhelper/connection_service_helper.go
+++ backend/helpers/srvhelper/connection_service_helper.go
@@ -18,7 +18,6 @@ limitations under the License.
 package srvhelper
 
 import (
-	"fmt"
 	"reflect"
 
 	"github.com/apache/incubator-devlake/core/context"
@@ -62,10 +61,7 @@ func (connSrv *ConnectionSrvHelper[C, S, SC]) DeleteConnection(connection *C) (r
 			return errors.Conflict.New("Please delete all data scope(s) before you delete this Data Connection.")
 		}
 		errors.Must(tx.Delete(connection))
-		scType := reflect.TypeOf(new(SC))
-		_, ok := scType.Elem().FieldByName("ConnectionId")
-		fmt.Println("scType---------:", scType, ok)
-		if scType != reflect.TypeOf(new(NoScopeConfig)) && ok {
+		if reflect.TypeOf(new(SC)) != reflect.TypeOf(new(NoScopeConfig)) {
 			errors.Must(connSrv.db.Delete(new(SC), dal.Where("connection_id = ?", connectionId)))
 		}
 		return nil
