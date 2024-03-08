7ec58111290e8f2b036ccdcb733b764249638459
abeizn <zikuan.an@merico.dev>
2024-03-08 15:35:09 +0800

    fix: add flag for data convert to dora (#7143)
    * fix: add flag for data convert to dora
    
    * fix: some minor optimizations
    
    * fix: add subtask_name filter

--- backend/plugins/dora/tasks/deployment_commits_generator.go
+++ backend/plugins/dora/tasks/deployment_commits_generator.go
@@ -30,6 +30,8 @@ import (
 	"github.com/apache/incubator-devlake/helpers/pluginhelper/api"
 )
 
+const DORAGenerateDeploymentCommits = "dora.generateDeploymentCommits"
+
 var DeploymentCommitsGeneratorMeta = plugin.SubTaskMeta{
 	Name:             "generateDeploymentCommits",
 	EntryPoint:       GenerateDeploymentCommits,
@@ -103,32 +105,32 @@ func GenerateDeploymentCommits(taskCtx plugin.SubTaskContext) errors.Error {
 	}
 	if data.Options.ScopeId != nil {
 		clauses = append(clauses, dal.Where(`p.cicd_scope_id = ?`, data.Options.ScopeId))
-		// // Clear previous results from the project
-		// deleteSql := `DELETE FROM cicd_deployment_commits WHERE cicd_scope_id = ? ;`
-		// err := db.Exec(deleteSql, data.Options.ScopeId)
-		// if err != nil {
-		// 	return errors.Default.Wrap(err, "error deleting previous cicd_deployment_commits")
-		// }
+		// Clear previous results from the project
+		deleteSql := `DELETE FROM cicd_deployment_commits WHERE cicd_scope_id = ? and subtask_name = ?;`
+		err := db.Exec(deleteSql, data.Options.ScopeId, DORAGenerateDeploymentCommits)
+		if err != nil {
+			return errors.Default.Wrap(err, "error deleting previous cicd_deployment_commits")
+		}
 	} else {
 		clauses = append(clauses,
 			dal.Join("LEFT JOIN project_mapping pm ON (pm.table = 'cicd_scopes' AND pm.row_id = p.cicd_scope_id)"),
 			dal.Where(`pm.project_name = ?`, data.Options.ProjectName),
 		)
-		// // Clear previous results from the project
-		// deleteSql := `DELETE FROM cicd_deployment_commits
-		// 	WHERE cicd_scope_id IN (
-		// 	SELECT cicd_scope_id
-		// 	FROM (
-		// 		SELECT cdc.cicd_scope_id
-		// 		FROM cicd_deployment_commits cdc
-		// 		LEFT JOIN project_mapping pm ON (pm.table = 'cicd_scopes' AND pm.row_id = cdc.cicd_scope_id)
-		// 		WHERE pm.project_name = ?
-		// 	) AS subquery
-		// 	);`
-		// err := db.Exec(deleteSql, data.Options.ProjectName)
-		// if err != nil {
-		// 	return errors.Default.Wrap(err, "error deleting previous cicd_deployment_commits")
-		// }
+		// Clear previous results from the project
+		deleteSql := `DELETE FROM cicd_deployment_commits
+			WHERE cicd_scope_id IN (
+			SELECT cicd_scope_id
+			FROM (
+				SELECT cdc.cicd_scope_id
+				FROM cicd_deployment_commits cdc
+				LEFT JOIN project_mapping pm ON (pm.table = 'cicd_scopes' AND pm.row_id = cdc.cicd_scope_id)
+				WHERE pm.project_name = ?
+			) AS subquery
+			) AND subtask_name = ?;`
+		err := db.Exec(deleteSql, data.Options.ProjectName, DORAGenerateDeploymentCommits)
+		if err != nil {
+			return errors.Default.Wrap(err, "error deleting previous cicd_deployment_commits")
+		}
 	}
 	cursor, err := db.Cursor(clauses...)
 	if err != nil {
@@ -198,6 +200,7 @@ func GenerateDeploymentCommits(taskCtx plugin.SubTaskContext) errors.Error {
 					domainDeployCommit.Environment = devops.TESTING
 				}
 			}
+			domainDeployCommit.SubtaskName = DORAGenerateDeploymentCommits
 			return []interface{}{domainDeployCommit}, nil
 		},
 	})
