3ad9aaff2ad7afc097f41edef616304691d94997
Klesh Wong <zhenmian.huang@merico.dev>
2024-03-08 16:36:10 +0800

    fix: blueprint gets disabled when disabling dora for project (#7146)

--- backend/server/services/project.go
+++ backend/server/services/project.go
@@ -224,14 +224,14 @@ func PatchProject(name string, body map[string]interface{}) (*models.ApiOutputPr
 	}
 
 	// Blueprint
-	err = tx.UpdateColumn(
-		&models.Blueprint{},
-		"enable", projectInput.Enable,
-		dal.Where("project_name = ?", name),
-	)
-	if err != nil {
-		return nil, err
-	}
+	// err = tx.UpdateColumn(
+	// 	&models.Blueprint{},
+	// 	"enable", projectInput.Enable,
+	// 	dal.Where("project_name = ?", name),
+	// )
+	// if err != nil {
+	// 	return nil, err
+	// }
 
 	// refresh project metrics if needed
 	if len(projectInput.Metrics) > 0 {
@@ -377,7 +377,7 @@ func makeProjectOutput(project *models.Project, withLastPipeline bool) (*models.
 	}
 	if withLastPipeline {
 		if projectOutput.Blueprint == nil {
-			logger.Warn(fmt.Errorf("Blueprint is nil"), "want to get latest pipeline, but blueprint is nil")
+			logger.Warn(fmt.Errorf("blueprint is nil"), "want to get latest pipeline, but blueprint is nil")
 		} else {
 			pipelines, pipelinesCount, err := GetPipelines(&PipelineQuery{
 				BlueprintId: projectOutput.Blueprint.ID,
