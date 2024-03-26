7b5b1fc97ff4c0c82ba6f8257e31bd1c7f391172
abeizn <zikuan.an@merico.dev>
2024-03-26 09:58:42 +0800

    fix: jira jql checker add debug log (#7215)
    * fix: jira jql checker add debug log
    
    * fix: jira jql checker add debug log
    
    * fix: jira jql checker add debug log

--- backend/plugins/jira/tasks/board_filter_begin_collector.go
+++ backend/plugins/jira/tasks/board_filter_begin_collector.go
@@ -84,7 +84,7 @@ func CollectBoardFilterBegin(taskCtx plugin.SubTaskContext) errors.Error {
 	}
 	// change
 	if record.Jql != jql {
-		return errors.Default.New(fmt.Sprintf("board filter jql has changed for connection_id:%d board_id:%d, please use fullSync mode!!!", data.Options.ConnectionId, data.Options.BoardId))
+		return errors.Default.New(fmt.Sprintf("connection_id:%d board_id:%d filter jql has changed, please use fullSync mode. And the previous jql is %s, now jql is %s", data.Options.ConnectionId, data.Options.BoardId, record.Jql, jql))
 	}
 	// no change
 	return nil
