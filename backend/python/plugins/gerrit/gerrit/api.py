bce10d017fb35495b9ddb9684f7533c441e4a0c7
Ji Bin <matrixji@live.com>
2024-03-30 11:56:53 +0000

    feat: gerrit plugin support incremental sync
    Signed-off-by: Ji Bin <matrixji@live.com>

--- backend/python/plugins/gerrit/gerrit/api.py
+++ backend/python/plugins/gerrit/gerrit/api.py
@@ -14,8 +14,13 @@
 # limitations under the License.
 
 from base64 import b64encode
+from os import environ
 from typing import Optional
+from urllib.parse import urlparse
+from datetime import datetime, timedelta
+from MySQLdb import connect as mysql_connect, Error as MySQLError
 from pydevlake.api import API, Request, Response, request_hook, response_hook, Paginator
+from gerrit.models import GerritChange
 
 
 # TODO: implement pagination
@@ -33,6 +38,33 @@ class GerritPaginator(Paginator):
 class GerritApi(API):
     # paginator = GerritPaginator()
 
+    def __init__(self, connection=None):
+        super().__init__(connection)
+        self.db_conn = None
+
+    def auto_connect(self):
+        if self.db_conn:
+            try:
+                self.db_conn.ping()
+                return
+            except MySQLError as e:
+                self.db_conn.close()
+        self.db_conn = None
+        if 'DB_URL' in environ:
+            parsed_url = urlparse((environ['DB_URL']))
+            connection_args = {
+                'user': parsed_url.username,
+                'password': parsed_url.password,
+                'host': parsed_url.hostname,
+                'port': parsed_url.port or 3306,  # Default MySQL port
+                # Remove leading slash from path
+                'database': parsed_url.path[1:]
+            }
+            try:
+                self.db_conn = mysql_connect(**connection_args)
+            except MySQLError as e:
+                print(f"Error connecting to MySQL: {e}")
+
     @property
     def base_url(self):
         return self.connection.url
@@ -41,8 +73,8 @@ class GerritApi(API):
     def authenticate(self, request: Request):
         conn = self.connection
         if conn.username and conn.password:
-            user_and_pass = f"{conn.username}:{conn.password.get_secret_value()}".encode()
-            basic_auth = b64encode(user_and_pass).decode()
+            user_pass = f"{conn.username}:{conn.password.get_secret_value()}".encode()
+            basic_auth = b64encode(user_pass).decode()
             request.headers["Authorization"] = f"Basic {basic_auth}"
 
     @response_hook
@@ -63,6 +95,22 @@ class GerritApi(API):
 
     def changes(self, project_name: str):
         # TODO: use pagination
+        self.auto_connect()
+        start_date = None
+        if self.db_conn:
+            cursor = self.db_conn.cursor()
+            try:
+                cursor.execute(
+                    f"SELECT updated_at FROM _tool_gerrit_gerritchanges WHERE id like '{project_name}~%' ORDER BY updated_at desc limit 1")
+                last_updated = cursor.fetchone()
+                if last_updated and len(last_updated) > 0:
+                    last_updated = last_updated[0] - timedelta(days=1)
+                    start_date = datetime.strftime(last_updated, "%Y-%m-%d")
+            except MySQLError as e:
+                print(f"Error fetching last updated date: {e}")
+            cursor.close()
+        if start_date:
+            return self.get(f"changes/?q=p:{project_name}+after:{start_date}&o=CURRENT_REVISION&o=ALL_COMMITS&o=DETAILED_ACCOUNTS&no-limit")
         return self.get(f"changes/?q=p:{project_name}&o=CURRENT_REVISION&o=ALL_COMMITS&o=DETAILED_ACCOUNTS&no-limit")
 
     def change_detail(self, change_id: str):
