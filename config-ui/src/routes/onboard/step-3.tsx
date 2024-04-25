fa6c3fb4677c1d6469738e806b01280c03a2aa6d
mintsweet <0x1304570@gmail.com>
2024-04-25 14:13:58 +0800

    feat: enable dora when onboarding progress

--- config-ui/src/routes/onboard/step-3.tsx
+++ config-ui/src/routes/onboard/step-3.tsx
@@ -64,7 +64,7 @@ export const Step3 = () => {
             {
               pluginName: 'dora',
               pluginOption: '',
-              enable: false,
+              enable: true,
             },
           ],
         });
