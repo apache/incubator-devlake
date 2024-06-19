e2f889192c54e8bfd14760a56439c215f04edaba
d4x1 <1507509064@qq.com>
2024-06-19 18:26:21 +0800

    fix(migration): fix unexpected `defer unlock`

--- backend/server/services/init.go
+++ backend/server/services/init.go
@@ -123,11 +123,12 @@ var statusLock sync.Mutex
 // This might be called concurrently across multiple API requests
 func ExecuteMigration() errors.Error {
 	statusLock.Lock()
-	defer statusLock.Unlock()
 	if serviceStatus == SERVICE_STATUS_MIGRATING {
+		statusLock.Unlock()
 		return errors.BadInput.New("already migrating")
 	}
 	if serviceStatus == SERVICE_STATUS_READY {
+		statusLock.Unlock()
 		return nil
 	}
 	serviceStatus = SERVICE_STATUS_MIGRATING
@@ -146,6 +147,7 @@ func ExecuteMigration() errors.Error {
 	pipelineServiceInit()
 	statusLock.Lock()
 	serviceStatus = SERVICE_STATUS_READY
+	statusLock.Unlock()
 	return nil
 }
 
