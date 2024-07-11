eaa7c45ee76a2b3552fca0a8453eaae861c6fe50
xiaomingzhang12 <142298906+xiaomingzhang12@users.noreply.github.com>
2024-07-11 18:16:31 +0800

    Create 711_main_1.py
    this is a test ,add=27

---
+++ 711_main_1.py
@@ -0,0 +1,26 @@
+import os
+from git import Repo
+
+# 设置本地仓库路径
+repo_path = '/path/to/your/local/repo'
+file_path = os.path.join(repo_path, 'test.txt')
+
+# 写入文件内容
+with open(file_path, 'w') as file:
+    file.write('hello\nhelloworld\n')
+
+# 打开本地仓库
+repo = Repo(repo_path)
+
+# 添加文件到 Git
+repo.index.add([file_path])
+
+# 提交更改
+repo.index.commit('Add test file with hello, helloworld content')
+
+# 推送到远程仓库
+origin = repo.remote(name='origin')
+origin.push()
+
+print('File committed and pushed to GitHub.')
+print('That is all!')
