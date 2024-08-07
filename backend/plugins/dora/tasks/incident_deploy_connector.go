c7377a6d3e6797835505e39142d84c1dace11b52
Lynwee <1507509064@qq.com>
2024-08-07 13:53:35 +0800

    Add more log for dora task ConnectIncidentToDeployment (#7856)
    * fix(dora): add more log in task ConnectIncidentToDeployment
    
    * fix(dora): add select clause in tasks
    
    * fix(dora): add more log

--- backend/plugins/dora/tasks/incident_deploy_connector.go
+++ backend/plugins/dora/tasks/incident_deploy_connector.go
@@ -57,10 +57,19 @@ func ConnectIncidentToDeployment(taskCtx plugin.SubTaskContext) errors.Error {
 	logger.Info("delete previous project_incident_deployment_relationships")
 	// select all issues belongs to the board
 	clauses := []dal.Clause{
+		dal.Select(`i.*`),
 		dal.From(`incidents i`),
 		dal.Join(`left join project_mapping pm on pm.row_id = i.scope_id and pm.table = i.table`),
 		dal.Where("pm.project_name = ?", data.Options.ProjectName),
 	}
+
+	count, err := db.Count(clauses...)
+	if err != nil {
+		logger.Error(err, "count incidents")
+	} else {
+		logger.Info("incident count is %d", count)
+	}
+
 	cursor, err := db.Cursor(clauses...)
 	if err != nil {
 		logger.Error(err, "db.cursor error")
