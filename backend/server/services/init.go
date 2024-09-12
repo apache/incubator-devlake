b7a5bc146490c4c7a8c721daa48c1df0ee07cd19
Lynwee <1507509064@qq.com>
2024-09-12 15:01:13 +0800

    feat(framwork): add custom project service (#8041)

--- backend/server/services/init.go
+++ backend/server/services/init.go
@@ -125,11 +125,13 @@ func Init() {
 	registerPluginsMigrationScripts()
 }
 
-func InjectCustomService(pipelineNotifier PipelineNotificationService) errors.Error {
-	if pipelineNotifier == nil {
-		return errors.Default.New("pipeline notifier is nil")
+func InjectCustomService(customPipelineNotifier PipelineNotificationService, customProjectService ProjectService) errors.Error {
+	if customPipelineNotifier != nil {
+		customPipelineNotificationService = customPipelineNotifier
 	}
-	customPipelineNotificationService = pipelineNotifier
+	if customProjectService != nil {
+		projectService = customProjectService
+	}
 	return nil
 }
 
