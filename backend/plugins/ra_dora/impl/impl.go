5240827f5166bd64ef5ed17f2e6906f8a049eb81
Diego Nunes <diego.silveira@reclameaqui.com.br>
2025-02-20 17:23:04 -0300

    feat: add ra dora metrics plugin.

---
+++ backend/plugins/ra_dora/impl/impl.go
@@ -0,0 +1,104 @@
+package impl
+
+import (
+	"github.com/apache/incubator-devlake/helpers/pluginhelper/subtaskmeta/sorter"
+
+	"github.com/apache/incubator-devlake/core/context"
+	"github.com/apache/incubator-devlake/core/dal"
+	"github.com/apache/incubator-devlake/core/errors"
+	coreModels "github.com/apache/incubator-devlake/core/models"
+	"github.com/apache/incubator-devlake/core/plugin"
+	"github.com/apache/incubator-devlake/plugins/ae/models/migrationscripts"
+	"github.com/apache/incubator-devlake/plugins/gitlab/api"
+	"github.com/apache/incubator-devlake/plugins/gitlab/tasks"
+)
+
+var _ interface {
+	plugin.PluginMeta
+	plugin.PluginInit
+	plugin.PluginTask
+	plugin.PluginModel
+	plugin.PluginMigration
+	plugin.PluginSource
+	plugin.DataSourcePluginBlueprintV200
+	plugin.CloseablePluginTask
+} = (*RaDoraMetrics)(nil)
+
+type RaDoraMetrics struct{}
+
+func (r RaDoraMetrics) MakeDataSourcePipelinePlanV200(
+	connectionId uint64,
+	scopes []*coreModels.BlueprintScope,
+	skipCollectors bool,
+) (pp coreModels.PipelinePlan, sc []plugin.Scope, err errors.Error) {
+	return nil, nil, nil
+}
+
+func init() {
+	// check subtask meta loop when init subtask meta
+	if _, err := sorter.NewDependencySorter(tasks.SubTaskMetaList).Sort(); err != nil {
+		panic(err)
+	}
+}
+
+func (r RaDoraMetrics) Init(basicRes context.BasicRes) errors.Error {
+	api.Init(basicRes, r)
+
+	return nil
+}
+
+func (r RaDoraMetrics) Description() string {
+	return "Description"
+}
+
+func (r RaDoraMetrics) Name() string {
+	return "ra_dora"
+}
+
+func (r RaDoraMetrics) RootPkgPath() string {
+	return "github.com/apache/incubator-devlake/plugins/ra_dora"
+}
+
+func (r RaDoraMetrics) Connection() dal.Tabler {
+	return nil
+}
+
+func (r RaDoraMetrics) Scope() plugin.ToolLayerScope {
+	return nil
+}
+
+func (r RaDoraMetrics) ScopeConfig() dal.Tabler {
+	return nil
+}
+
+func (r RaDoraMetrics) GetTablesInfo() []dal.Tabler {
+	return []dal.Tabler{}
+}
+
+func (r RaDoraMetrics) SubTaskMetas() []plugin.SubTaskMeta {
+	list, err := sorter.NewDependencySorter(tasks.SubTaskMetaList).Sort()
+	if err != nil {
+		panic(err)
+	}
+	return list
+}
+
+func (r RaDoraMetrics) PrepareTaskData(taskCtx plugin.TaskContext, options map[string]interface{}) (interface{}, errors.Error) {
+	return nil, nil
+}
+
+func (r RaDoraMetrics) MigrationScripts() []plugin.MigrationScript {
+	return migrationscripts.All()
+}
+
+func (r RaDoraMetrics) TestConnection(id uint64) errors.Error {
+	return nil
+}
+
+func (r RaDoraMetrics) ApiResources() map[string]map[string]plugin.ApiResourceHandler {
+	return map[string]map[string]plugin.ApiResourceHandler{}
+}
+
+func (r RaDoraMetrics) Close(taskCtx plugin.TaskContext) errors.Error {
+	return nil
+}
---
+++ backend/plugins/ra_dora/ra_dora.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/apache/incubator-devlake/plugins/ra_dora/impl"
+)
+
+// PluginEntry is a variable exported for Framework to search and load
+var PluginEntry impl.RaDoraMetrics //nolint
+
+// standalone mode for debugging
+func main() {
+}
