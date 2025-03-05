a44715dae0413f90efe112673a34cd4d2d9b9135
Diego Nunes <diego.silveira@reclameaqui.com.br>
2025-03-05 10:24:26 -0300

    ajustes

--- backend/plugins/ra_dora/tasks/deployment_convertor.go
+++ backend/plugins/ra_dora/tasks/deployment_convertor.go
@@ -1,7 +1,6 @@
 package tasks
 
 import (
-	"encoding/json"
 	"log"
 
 	"github.com/apache/incubator-devlake/core/errors"
@@ -23,35 +22,17 @@ var ConvertDeploymentsMeta = plugin.SubTaskMeta{
 	EnabledByDefault: true,
 	Description:      "Converte deployments do DevLake para análise de métricas DORA",
 	DomainTypes:      []string{plugin.DOMAIN_TYPE_CICD},
-	DependencyTables: []string{RAW_DEPLOYMENT_TABLE},
-	ProductTables:    []string{"dora_deployments"},
 }
 
 func ConvertDeployments(taskCtx plugin.SubTaskContext) errors.Error {
-	extractor, err := api.NewApiExtractor(api.ApiExtractorArgs{
-		RawDataSubTaskArgs: api.RawDataSubTaskArgs{
-			Ctx:    taskCtx,
+
+	converter, err := api.NewStatefulDataConverter(&api.StatefulDataConverterArgs[models.Deployments]{
+		SubtaskCommonArgs: &api.SubtaskCommonArgs{
 			Params: taskCtx.GetData(),
-			Table:  RAW_DEPLOYMENT_TABLE,
+			Table:  "argo_api_deployments",
 		},
-		Extract: func(row *api.RawData) ([]interface{}, errors.Error) {
-			var rawDeployment models.Deployments
-
-			err := errors.Convert(json.Unmarshal(row.Data, &rawDeployment))
-			if err != nil {
-				return nil, err
-			}
-
-			deployment := &models.Deployment{
-				ID:      rawDeployment.ID,
-				ScopeID: rawDeployment.ScopeID,
-			}
-
-			results := make([]interface{}, 0, 1)
-			results = append(results, deployment)
-
-			return results, nil
-		},
+		Input:   nil,
+		Convert: nil,
 	})
 
 	if err != nil {
@@ -59,5 +40,5 @@ func ConvertDeployments(taskCtx plugin.SubTaskContext) errors.Error {
 	}
 
 	log.Println("Conversão de deployments concluída com sucesso!")
-	return extractor.Execute()
+	return converter.Execute()
 }
