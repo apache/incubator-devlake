017c918bb4c477ce6fc886f70684b9401ca71c50
warren <warren.chen830@gmail.com>
2026-08-27 11:00:18 +0800

    fix: add Apache license headers
    Signed-off-by: warren <warren.chen830@gmail.com>

--- tools/kiro-attribution/install.sh
+++ tools/kiro-attribution/install.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # Installs Kiro commit attribution into a repository.
 #
 # Usage:
@@ -181,7 +196,7 @@ fi
   # Inlined rather than sourced: a developer's checkout of this repository can
   # move or be deleted, and a hook pointing at a missing file would break every
   # commit in that repo.
-  sed '1d' "$SCRIPT_DIR/prepare-commit-msg"
+  sed '1d' "$SCRIPT_DIR/prepare-commit-msg.sh"
 } > "$HOOK_FILE"
 
 chmod +x "$HOOK_FILE"
